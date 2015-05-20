4a2ad5f8a0e860e07331fdfb2c23e61fadf3e8ea
BoXz_modder <asgernohns@gmail.com>
2015-05-20 11:53:13 +0200

    Added position to getNearbyVehicles()

--- source/World.hpp
+++ source/World.hpp
@@ -69,6 +69,7 @@ namespace GTA
                 static array<Ped ^> ^GetNearbyPeds(Math::Vector3 position, float radius);
 		static array<Ped ^> ^GetNearbyPeds(Ped ^ped, float radius);
 		static array<Ped ^> ^GetNearbyPeds(Ped ^ped, float radius, int maxAmount);
+		static array<Vehicle ^> ^GetNearbyVehicles(Math::Vector3 position, float radius);
 		static array<Vehicle ^> ^GetNearbyVehicles(Ped ^ped, float radius);
 		static array<Vehicle ^> ^GetNearbyVehicles(Ped ^ped, float radius, int maxAmount);
 		static Ped ^GetClosestPed(Math::Vector3 position, float radius);
