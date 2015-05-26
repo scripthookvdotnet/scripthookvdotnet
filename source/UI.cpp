8ffd3da7b4d22e7e3b578918fc3601b43694df20
Johnny Dellinger @PC <johnny@johnnycrazy.de>
2015-05-26 03:25:03 +0200

    Changed PointF to Point

--- source/UI.cpp
+++ source/UI.cpp
@@ -19,11 +19,11 @@ namespace GTA
 		Native::Function::Call(Native::Hash::_ADD_TEXT_COMPONENT_STRING, msg);
 		Native::Function::Call(Native::Hash::_0x9D77056A530643F6, duration, 1);
 	}
-	System::Drawing::PointF UI::WorldToScreen(Math::Vector3 position)
+	System::Drawing::Point UI::WorldToScreen(Math::Vector3 position)
 	{
 		float pointX, pointY;
 		if (!Native::Function::Call<bool>(Native::Hash::_0x34E82F05DF2974F5, position.X, position.Y, position.Z, &pointX, &pointY))
-			return System::Drawing::PointF(0, 0);
-		return System::Drawing::PointF(pointX, pointY);
+			return System::Drawing::Point();
+		return System::Drawing::Point(pointX * UI::WIDTH, pointY * UI::HEIGHT);
 	}
 }
--- source/UI.hpp
+++ source/UI.hpp
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "Vector3.hpp"
+
 namespace GTA
 {
 	public ref class UI sealed abstract
@@ -13,6 +15,6 @@ namespace GTA
 		static void ShowSubtitle(System::String ^msg);
 		static void ShowSubtitle(System::String ^msg, int duration);
 
-		static System::Drawing::PointF WorldToScreen(Math::Vector3 position);
+		static System::Drawing::Point WorldToScreen(Math::Vector3 position);
 	};
 }
