fde34941dfd3fadc44955016eccfd3ce27826837
zorg93 <nayjames@hotmail.com>
2015-05-29 16:30:54 +0100

    Update UI.hpp

--- source/UI.hpp
+++ source/UI.hpp
@@ -8,8 +8,8 @@ namespace GTA
 	{
 	public:
 
-		const int WIDTH = 1280;
-		const int HEIGHT = 720;
+		static const int WIDTH = 1280;
+		static const int HEIGHT = 720;
 
 		static int Notify(System::String ^msg);
 		static void RemoveNotification(int notification);
