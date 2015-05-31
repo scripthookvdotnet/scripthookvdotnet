d4d4020c389a268fd26450594a525282a24fd32a
Nathan James <n.james93@hotmail.co.uk>
2015-05-31 18:06:36 +0100

    Reverted Notifications, While a better solution is found

--- source/UI.hpp
+++ source/UI.hpp
@@ -4,23 +4,12 @@
 
 namespace GTA
 {
-	public ref class Notification
-	{
-	public:
-		static Notification ^Show(System::String ^Message){ return Show(Message, false); }
-		static Notification ^Show(System::String ^Message, bool Blinking);
-		void Hide();
-	private:
-		Notification(int Handle);
-		int mHandle;
-	};
 	public ref class UI sealed abstract
 	{
 	public:
 		static const int WIDTH = 1280;
 		static const int HEIGHT = 720;
 
-		[System::ObsoleteAttribute("UI.Notify is obsolete, please use Notification.Show instead.")]
 		static void Notify(System::String ^msg);
 
 		static void ShowSubtitle(System::String ^msg);
