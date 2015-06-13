d463ca7d6bfa9ea3e374a9dab6ab0cafc16592f6
Crosire <crosiredev@gmail.com>
2015-06-13 11:33:56 +0200

    Fixed control names in example scripts

--- example/ExitVehicle.cs
+++ example/ExitVehicle.cs
@@ -1,44 +1,43 @@
 using System;
-using System.Windows.Forms;
 using GTA;
 
 public class ExitVehicle : Script
 {
-    public ExitVehicle()
-    {
-        Tick += OnTick;
-    }
+	public ExitVehicle()
+	{
+		Tick += OnTick;
+	}
 
-    DateTime mLastExit;
+	DateTime mLastExit;
 
-    void OnTick(object sender, EventArgs e)
-    {
-        Ped player = Game.Player.Character;
+	void OnTick(object sender, EventArgs e)
+	{
+		Ped player = Game.Player.Character;
 
-        if (Game.IsControlPressed(2, GTA.Control.INPUT_VEH_EXIT) && DateTime.Now > this.mLastExit && player.IsInVehicle())
-        {
-            Wait(250);
+		if (Game.IsControlPressed(2, Control.VehicleExit) && DateTime.Now > this.mLastExit && player.IsInVehicle())
+		{
+			Wait(250);
 
-            Vehicle vehicle = player.CurrentVehicle;
-            bool isDriver = vehicle.GetPedOnSeat(VehicleSeat.Driver) == player;
+			Vehicle vehicle = player.CurrentVehicle;
+			bool isDriver = vehicle.GetPedOnSeat(VehicleSeat.Driver) == player;
 
-            if (Game.IsControlPressed(2, GTA.Control.INPUT_VEH_EXIT))
-            {
-                player.Task.LeaveVehicle(vehicle, true);
-            }
-            else
-            {
-                player.Task.LeaveVehicle(vehicle, false);
+			if (Game.IsControlPressed(2, Control.VehicleExit))
+			{
+				player.Task.LeaveVehicle(vehicle, true);
+			}
+			else
+			{
+				player.Task.LeaveVehicle(vehicle, false);
 
-                Wait(0);
+				Wait(0);
 
-                if (isDriver)
-                {
-                    vehicle.EngineRunning = true;
-                }
-            }
+				if (isDriver)
+				{
+					vehicle.EngineRunning = true;
+				}
+			}
 
-            this.mLastExit = DateTime.Now + TimeSpan.FromMilliseconds(2000);
-        }
-    }
+			this.mLastExit = DateTime.Now + TimeSpan.FromMilliseconds(2000);
+		}
+	}
 }
