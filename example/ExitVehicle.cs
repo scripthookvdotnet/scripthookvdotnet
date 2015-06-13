7ac31385c3d4b14dbd9fc8220f00907411f079be
Wenchao Wang <unlock.liberty@gmail.com>
2015-06-12 21:45:03 -0400

    More readable ExitVehicle

--- example/ExitVehicle.cs
+++ example/ExitVehicle.cs
@@ -4,47 +4,41 @@ using GTA;
 
 public class ExitVehicle : Script
 {
-	public ExitVehicle()
-	{
-		Tick += OnTick;
-	}
+    public ExitVehicle()
+    {
+        Tick += OnTick;
+    }
 
-	const float _HOLD_THRESHOLD = 0.25f;
-	float holdTime = 0;
+    DateTime mLastExit;
 
-	void OnTick(object sender, EventArgs e)
-	{
-		Ped player = Game.Player.Character;
+    void OnTick(object sender, EventArgs e)
+    {
+        Ped player = Game.Player.Character;
 
-		if (player.IsAlive && player.IsInVehicle())
-		{
-			if (Game.IsControlPressed(2, GTA.Control.INPUT_VEH_EXIT))
-			{
-				holdTime += Game.LastFrameTime;
-			}
-			else if (holdTime > 0)
-			{
-				Vehicle vehicle = player.CurrentVehicle;
-				if (holdTime >= _HOLD_THRESHOLD)
-				{
-					// Exit vehicle, turn off engine
-					player.Task.LeaveVehicle(vehicle, true);
-				}
-				else
-				{
-					// Exit vehicle, leave engine running (if player is driver)
-					player.Task.LeaveVehicle(vehicle, false);
-					bool isDriver = vehicle.GetPedOnSeat(VehicleSeat.Driver) == player;
-					if (isDriver)
-					{
-						vehicle.EngineRunning = true;
-					}
-				}
-			}
-		}
-		else
-		{
-			holdTime = 0;
-		}
-	}
+        if (Game.IsControlPressed(2, GTA.Control.INPUT_VEH_EXIT) && DateTime.Now > this.mLastExit && player.IsInVehicle())
+        {
+            Wait(250);
+
+            Vehicle vehicle = player.CurrentVehicle;
+            bool isDriver = vehicle.GetPedOnSeat(VehicleSeat.Driver) == player;
+
+            if (Game.IsControlPressed(2, GTA.Control.INPUT_VEH_EXIT))
+            {
+                player.Task.LeaveVehicle(vehicle, true);
+            }
+            else
+            {
+                player.Task.LeaveVehicle(vehicle, false);
+
+                Wait(0);
+
+                if (isDriver)
+                {
+                    vehicle.EngineRunning = true;
+                }
+            }
+
+            this.mLastExit = DateTime.Now + TimeSpan.FromMilliseconds(2000);
+        }
+    }
 }
