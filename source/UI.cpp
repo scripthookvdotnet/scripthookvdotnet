64362d7e288779b0a1d1f1c13b4cb0194b4b79e0
Crosire <crosiredev@gmail.com>
2015-09-07 20:38:06 +0200

    Changed "GTA::UI::DrawTexture" behaviour back to use top-left corner as texture origin by default

--- source/UI.cpp
+++ source/UI.cpp
@@ -6,6 +6,9 @@
 
 namespace GTA
 {
+	using namespace System;
+	using namespace System::Drawing;
+
 	Notification::Notification(int Handle) : mHandle(Handle)
 	{
 	}
@@ -15,11 +18,11 @@ namespace GTA
 		Native::Function::Call(Native::Hash::_REMOVE_NOTIFICATION, this->mHandle);
 	}
 
-	Notification ^UI::Notify(System::String ^msg)
+	Notification ^UI::Notify(String ^msg)
 	{
 		return Notify(msg, false);
 	}
-	Notification ^UI::Notify(System::String ^msg, bool blinking)
+	Notification ^UI::Notify(String ^msg, bool blinking)
 	{
 		Native::Function::Call(Native::Hash::_SET_NOTIFICATION_TEXT_ENTRY, "STRING");
 		Native::Function::Call(Native::Hash::_ADD_TEXT_COMPONENT_STRING, msg);
@@ -27,38 +30,38 @@ namespace GTA
 		return gcnew Notification(Native::Function::Call<int>(Native::Hash::_DRAW_NOTIFICATION, blinking, 1));
 	}
 
-	void UI::ShowSubtitle(System::String ^msg)
+	void UI::ShowSubtitle(String ^msg)
 	{
 		ShowSubtitle(msg, 2500);
 	}
-	void UI::ShowSubtitle(System::String ^msg, int duration)
+	void UI::ShowSubtitle(String ^msg, int duration)
 	{
 		Native::Function::Call(Native::Hash::_SET_TEXT_ENTRY_2, "STRING");
 		Native::Function::Call(Native::Hash::_ADD_TEXT_COMPONENT_STRING, msg);
 		Native::Function::Call(Native::Hash::_DRAW_SUBTITLE_TIMED, duration, 1);
 	}
 
-	System::Drawing::Point UI::WorldToScreen(Math::Vector3 position)
+	Point UI::WorldToScreen(Math::Vector3 position)
 	{
 		float pointX, pointY;
 
 		if (!Native::Function::Call<bool>(Native::Hash::_WORLD3D_TO_SCREEN2D, position.X, position.Y, position.Z, &pointX, &pointY))
 		{
-			return System::Drawing::Point();
+			return Point();
 		}
 
-		return System::Drawing::Point((int)(pointX * UI::WIDTH), (int)(pointY * UI::HEIGHT));
+		return Point(static_cast<int>(pointX * UI::WIDTH), static_cast<int>(pointY * UI::HEIGHT));
 	}
 
-	void UI::DrawTexture(System::String ^filename, int index, int level, int time, System::Drawing::Point pos, System::Drawing::Size size)
+	void UI::DrawTexture(String ^filename, int index, int level, int time, Point pos, Size size)
 	{
-		DrawTexture(filename, index, level, time, pos, System::Drawing::PointF(0.5f, 0.5f), size, 0.0f, System::Drawing::Color::White);
+		DrawTexture(filename, index, level, time, pos, PointF(0.0f, 0.0f), size, 0.0f, Color::White);
 	}
-	void UI::DrawTexture(System::String ^filename, int index, int level, int time, System::Drawing::Point pos, System::Drawing::Size size, float rotation, System::Drawing::Color color)
+	void UI::DrawTexture(String ^filename, int index, int level, int time, Point pos, Size size, float rotation, Color color)
 	{
-		DrawTexture(filename, index, level, time, pos, System::Drawing::PointF(0.5f, 0.5f), size, rotation, color);
+		DrawTexture(filename, index, level, time, pos, PointF(0.0f, 0.0f), size, rotation, color);
 	}
-	void UI::DrawTexture(System::String ^filename, int index, int level, int time, System::Drawing::Point pos, System::Drawing::PointF center, System::Drawing::Size size, float rotation, System::Drawing::Color color)
+	void UI::DrawTexture(String ^filename, int index, int level, int time, Point pos, PointF center, Size size, float rotation, Color color)
 	{
 		int id;
 
@@ -80,6 +83,7 @@ namespace GTA
 
 		drawTexture(id, index, level, time, w, h, center.X, center.Y, x, y, rotation, 1.0f, color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
 	}
+
 	void UI::HideHudComponentThisFrame(HudComponent component)
 	{
 		Native::Function::Call(Native::Hash::HIDE_HUD_COMPONENT_THIS_FRAME, static_cast<int>(component));
