b72c05456c3a96eb627ab825749b2de4bf537b12
kagikn <kagikn.3939@gmail.com>
2016-02-02 22:13:05 +0900

    Add Obsolete attribute with fixing message again

--- source/scripting/Game.hpp
+++ source/scripting/Game.hpp
@@ -225,7 +225,9 @@ namespace GTA
 		static bool IsDisabledControlJustPressed(int index, Control control);
 		static bool IsDisabledControlJustReleased(int index, Control control);
 		static bool IsControlEnabled(int index, Control control);
+		[System::ObsoleteAttribute("The Game.EnableControl is obsolete, use Game.EnableControlThisFrame instead.")]
 		static void EnableControl(int index, Control control);
+		[System::ObsoleteAttribute("The Game.DisableControl is obsolete, use Game.DisableControlThisFrame instead.")]
 		static void DisableControl(int index, Control control);
 		static void EnableControlThisFrame(int index, Control control);
 		static void DisableControlThisFrame(int index, Control control);
