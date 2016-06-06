fe124f33a217014cddfa207d8d5355dffc74d306
kagikn <kagikn.3939@gmail.com>
2016-06-07 00:07:37 +0900

    Handle wheel indexes of wheel_lm2 and wheel_rm2

--- source/scripting/VehicleWheel.cs
+++ source/scripting/VehicleWheel.cs
@@ -14,16 +14,38 @@ namespace GTA
 
 		internal VehicleWheel(Vehicle owner, int index)
 		{
-            _owner = owner;
-            Index = index;
+			_owner = owner;
+			Index = index;
 
-            int wheelsPtrOffset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0xAA0 : 0xA80;
-            MemoryAddress = MemoryAccess.ReadPtr(MemoryAccess.ReadPtr(owner.MemoryAddress + wheelsPtrOffset) + (index * 8));
-        }
+			int wheelsPtrOffset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0xAA0 : 0xA80;
+			int wheelOffset = (index * 8);
+			if (index == 45 && _owner.HasBone("wheel_lm2")) //wheel_lm2 is assigned to 45 on the native functions, but it's index is 4 in the wheel collection struct.
+			{
+				index = 4;
+			}
+			if (index == 47 && _owner.HasBone("wheel_rm2")) //wheel_lm2 is assigned to 47 on the native functions, but it's index is 5 in the wheel collection struct.
+			{
+				index = 5;
+			}
+
+			if (index < 0 || 19 < index)
+			{
+				new ArgumentOutOfRangeException("index", "Index values must be between 0 and 19 except for 45 of wheel_lm2 or 47 of wheel_lm2, inclusive.");
+			}
+
+			MemoryAddress = MemoryAccess.ReadPtr(MemoryAccess.ReadPtr(owner.MemoryAddress + wheelsPtrOffset) + (index * 8));
+		}
 
 		public int Index { get; private set; }
 		public IntPtr MemoryAddress { get; private set; }
 
+		public bool Exists
+		{
+			get
+			{
+				return Vehicle.Exists() && MemoryAddress != IntPtr.Zero;
+			}
+		}
 		public bool IsBurst
 		{
 			get
