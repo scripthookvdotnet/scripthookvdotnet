f10de968d5991ff17a576c9b29b3fc6fc9f396c4
kagikn <kagikn.3939@gmail.com>
2016-06-06 21:56:09 +0900

    Convert spaces to tabs

--- source/scripting/VehicleMod.cs
+++ source/scripting/VehicleMod.cs
@@ -16,7 +16,7 @@ namespace GTA
 		internal VehicleMod(Vehicle owner, VehicleModType modType)
 		{
 			_owner = owner;
-            ModType = modType;
+			ModType = modType;
 		}
 
 		public VehicleModType ModType { get; private set; }
@@ -25,43 +25,43 @@ namespace GTA
 		{
 			get
 			{
-                return Function.Call<int>(Hash.GET_VEHICLE_MOD, _owner.Handle, (int)ModType);
-            }
-            set
-            {
-                Function.Call(Hash.SET_VEHICLE_MOD, _owner.Handle, (int)ModType, (int)ModType, true);
-            }
-        }
+				return Function.Call<int>(Hash.GET_VEHICLE_MOD, _owner.Handle, (int)ModType);
+			}
+			set
+			{
+				Function.Call(Hash.SET_VEHICLE_MOD, _owner.Handle, (int)ModType, (int)ModType, true);
+			}
+		}
 
 		public string LocalizedModTypeName
 		{
 			get
 			{
 				return Function.Call<string>(Hash.GET_MOD_SLOT_NAME, _owner.Handle, (int)ModType);
-            }
-        }
-        public string LocalizedModName
-        {
-            get
-            {
-                return Function.Call<string>(Hash.GET_MOD_TEXT_LABEL, _owner.Handle, (int)ModType, Index);
-            }
-        }
+			}
+		}
+		public string LocalizedModName
+		{
+			get
+			{
+				return Function.Call<string>(Hash.GET_MOD_TEXT_LABEL, _owner.Handle, (int)ModType, Index);
+			}
+		}
 		public int ModCount
 		{
 			get
 			{
-                return Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, _owner.Handle, (int)ModType);
-            }
+				return Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, _owner.Handle, (int)ModType);
+			}
 		}
-        public Vehicle Vehicle
-        {
-            get { return _owner; }
-        }
+		public Vehicle Vehicle
+		{
+			get { return _owner; }
+		}
 
-        public void Remove()
-        {
-            Function.Call(Hash.REMOVE_VEHICLE_MOD, _owner.Handle, (int)ModType);
-        }
-    }
+		public void Remove()
+		{
+			Function.Call(Hash.REMOVE_VEHICLE_MOD, _owner.Handle, (int)ModType);
+		}
+	}
 }
--- source/scripting/VehicleWheel.cs
+++ source/scripting/VehicleWheel.cs
@@ -10,97 +10,97 @@ namespace GTA
 	{
 		#region Fields
 		Vehicle _owner;
-        #endregion
+		#endregion
 
-        internal VehicleWheel(Vehicle owner, int index)
+		internal VehicleWheel(Vehicle owner, int index)
 		{
 			_owner = owner;
 
-            int wheelsPtrOffset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0xAA0 : 0xA80;
-            MemoryAddress = MemoryAccess.ReadPtr(owner.MemoryAddress + wheelsPtrOffset) + index * 8;
+			int wheelsPtrOffset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0xAA0 : 0xA80;
+			MemoryAddress = MemoryAccess.ReadPtr(owner.MemoryAddress + wheelsPtrOffset) + index * 8;
 		}
 
 		public int Index { get; private set; }
-        public IntPtr MemoryAddress { get; private set; }
+		public IntPtr MemoryAddress { get; private set; }
 
-        public bool IsBurst
-        {
-            get
-            {
-                return Function.Call<bool>(Hash.IS_VEHICLE_TYRE_BURST, _owner.Handle, Index, false);
-            }
-        }
-        public bool IsBurstCompletely
-        {
-            get
-            {
-                return Function.Call<bool>(Hash.IS_VEHICLE_TYRE_BURST, _owner.Handle, Index, true);
-            }
-        }
-        public bool IsTireOnFire
-        {
-            get
-            {
-                if (MemoryAddress != IntPtr.Zero)
-                {
-                    int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1EC : 0x1DC;
-                    return (MemoryAccess.ReadByte(MemoryAddress + offset) & (1 << 3)) != 0;
-                }
+		public bool IsBurst
+		{
+			get
+			{
+				return Function.Call<bool>(Hash.IS_VEHICLE_TYRE_BURST, _owner.Handle, Index, false);
+			}
+		}
+		public bool IsBurstCompletely
+		{
+			get
+			{
+				return Function.Call<bool>(Hash.IS_VEHICLE_TYRE_BURST, _owner.Handle, Index, true);
+			}
+		}
+		public bool IsTireOnFire
+		{
+			get
+			{
+				if (MemoryAddress != IntPtr.Zero)
+				{
+					int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1EC : 0x1DC;
+					return (MemoryAccess.ReadByte(MemoryAddress + offset) & (1 << 3)) != 0;
+				}
 
-                return false;
-            }
-        }
-        public bool IsTouching
-        {
-            get
-            {
-                if (MemoryAddress != IntPtr.Zero)
-                {
-                    int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1EC : 0x1DC;
-                    return (MemoryAccess.ReadByte(MemoryAddress + offset) & (1 << 0)) != 0;
-                }
+				return false;
+			}
+		}
+		public bool IsTouching
+		{
+			get
+			{
+				if (MemoryAddress != IntPtr.Zero)
+				{
+					int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1EC : 0x1DC;
+					return (MemoryAccess.ReadByte(MemoryAddress + offset) & (1 << 0)) != 0;
+				}
 
-                return false;
-            }
-        }
-        public float WheelHealth
-        {
-            get
-            {
-                if (MemoryAddress != IntPtr.Zero)
-                {
-                    int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1E0 : 0x1D0;
-                    return MemoryAccess.ReadFloat(MemoryAddress + offset);
-                }
+				return false;
+			}
+		}
+		public float WheelHealth
+		{
+			get
+			{
+				if (MemoryAddress != IntPtr.Zero)
+				{
+					int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1E0 : 0x1D0;
+					return MemoryAccess.ReadFloat(MemoryAddress + offset);
+				}
 
-                return 0.0f;
-            }
-        }
-        public float TireHealth
-        {
-            get
-            {
-                if (MemoryAddress != IntPtr.Zero)
-                {
-                    int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1E4 : 0x1D4;
-                    return MemoryAccess.ReadFloat(MemoryAddress + offset);
-                }
+				return 0.0f;
+			}
+		}
+		public float TireHealth
+		{
+			get
+			{
+				if (MemoryAddress != IntPtr.Zero)
+				{
+					int offset = Game.Version >= GameVersion.v1_0_372_2_Steam ? 0x1E4 : 0x1D4;
+					return MemoryAccess.ReadFloat(MemoryAddress + offset);
+				}
 
-                return 0.0f;
-            }
-        }
-        public Vehicle Vehicle
-        {
-            get { return _owner; }
-        }
+				return 0.0f;
+			}
+		}
+		public Vehicle Vehicle
+		{
+			get { return _owner; }
+		}
 
-        public void Burst()
-        {
-            Function.Call(Hash.SET_VEHICLE_TYRE_BURST, _owner.Handle, Index, true, 1000f);
-        }
-        public void Fix()
-        {
-            Function.Call(Hash.SET_VEHICLE_TYRE_FIXED, _owner.Handle, Index);
-        }
-    }
+		public void Burst()
+		{
+			Function.Call(Hash.SET_VEHICLE_TYRE_BURST, _owner.Handle, Index, true, 1000f);
+		}
+		public void Fix()
+		{
+			Function.Call(Hash.SET_VEHICLE_TYRE_FIXED, _owner.Handle, Index);
+		}
+	}
 }
