def2a1ea191f52243550e8ccd8810c4c696f6d40
crosire <crosiredev@gmail.com>
2016-06-11 23:37:51 +0200

    Update version number

--- source/AssemblyInfo.cpp
+++ source/AssemblyInfo.cpp
@@ -3,4 +3,4 @@ using namespace System::Reflection;
 [assembly:AssemblyTitleAttribute("Community Script Hook V .NET")];
 [assembly:AssemblyDescriptionAttribute("An ASI plugin for Grand Theft Auto V, which allows running scripts written in any .NET language in-game.")];
 [assembly:AssemblyCopyrightAttribute("Copyright © 2015 crosire")];
-[assembly:AssemblyVersionAttribute("2.9.0")];
+[assembly:AssemblyVersionAttribute("2.9.1")];
