b0eec0b4ae34e06ee91effb3e6e477970802ef58
Nathan James <nayjames@hotmail.com>
2016-06-12 22:25:57 +0100

    Added VehicleModCollection.HasVehicleMod, GetAllMods, AllowedWheelTypes, LocalizedWheelTypeName Added Bennys wheel types

--- source/scripting/VehicleModCollection.cs
+++ source/scripting/VehicleModCollection.cs
@@ -1,6 +1,8 @@
 using GTA.Native;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GTA
 {
@@ -23,8 +25,8 @@ namespace GTA
 		Horns,
 		Suspension,
 		Armor,
-		FrontWheels = 23,
-		BackWheels,
+		FrontWheel = 23,
+		RearWheel,
 		PlateHolder,
 		VanityPlates,
 		TrimDesign,
@@ -66,7 +68,9 @@ namespace GTA
 		Offroad,
 		Tuner,
 		BikeWheels,
-		HighEnd
+		HighEnd,
+		BennysOriginals,
+		BennysBespoke
 	}
 
 	public sealed class VehicleModCollection
@@ -77,6 +81,21 @@ namespace GTA
 		readonly Dictionary<int, VehicleMod> _vehicleMods = new Dictionary<int, VehicleMod>();
 		readonly Dictionary<int, VehicleToggleMod> _vehicleToggleMods = new Dictionary<int, VehicleToggleMod>();
 
+		private static readonly ReadOnlyDictionary<VehicleWheelType, Tuple<string, string>> _wheelNames = new ReadOnlyDictionary
+			<VehicleWheelType, Tuple<string, string>>(
+			new Dictionary<VehicleWheelType, Tuple<string, string>>
+			{
+				{VehicleWheelType.BikeWheels, new Tuple<string, string>("CMOD_WHE1_0", "Bike")},
+				{VehicleWheelType.HighEnd, new Tuple<string, string>("CMOD_WHE1_1", "High End")},
+				{VehicleWheelType.Lowrider, new Tuple<string, string>("CMOD_WHE1_2", "Lowrider")},
+				{VehicleWheelType.Muscle, new Tuple<string, string>("CMOD_WHE1_3", "Muscle")},
+				{VehicleWheelType.Offroad, new Tuple<string, string>("CMOD_WHE1_4", "Offroad")},
+				{VehicleWheelType.Sport, new Tuple<string, string>("CMOD_WHE1_5", "Sport")},
+				{VehicleWheelType.SUV, new Tuple<string, string>("CMOD_WHE1_6", "SUV")},
+				{VehicleWheelType.Tuner, new Tuple<string, string>("CMOD_WHE1_7", "Tuner")},
+				{VehicleWheelType.BennysOriginals, new Tuple<string, string>("CMOD_WHE1_8", "Benny's Originals")},
+				{VehicleWheelType.BennysBespoke, new Tuple<string, string>("CMOD_WHE1_9", "Benny's Bespoke")}
+			});
 		#endregion
 
 		internal VehicleModCollection(Vehicle owner)
@@ -117,10 +136,86 @@ namespace GTA
 			}
 		}
 
+		public bool HasVehicleMod(VehicleModType type)
+		{
+			return Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, _owner.Handle, type) > 0;
+		}
+
+		public VehicleMod[] GetAllMods()
+		{
+			return
+				Enum.GetValues(typeof(VehicleModType)).Cast<VehicleModType>().Where(HasVehicleMod).Select(modType => this[modType]).ToArray();
+		}
+
 		public VehicleWheelType WheelType
 		{
 			get { return Function.Call<VehicleWheelType>(Hash.GET_VEHICLE_WHEEL_TYPE, _owner.Handle); }
 			set { Function.Call(Hash.SET_VEHICLE_WHEEL_TYPE, _owner.Handle, value); }
+		}
+
+		public VehicleWheelType[] AllowedWheelTypes
+		{
+			get
+			{
+				if (_owner.Model.IsBicycle || _owner.Model.IsBike)
+				{
+					return new VehicleWheelType[] {VehicleWheelType.BikeWheels};
+				}
+				if (_owner.Model.IsCar)
+				{
+					var res = new List<VehicleWheelType>()
+					{
+						VehicleWheelType.Sport,
+						VehicleWheelType.Muscle,
+						VehicleWheelType.Lowrider,
+						VehicleWheelType.SUV,
+						VehicleWheelType.Offroad,
+						VehicleWheelType.Tuner,
+						VehicleWheelType.HighEnd
+					};
+					switch ((VehicleHash)_owner.Model)
+					{
+						case VehicleHash.Faction2:
+						case VehicleHash.Buccaneer2:
+						case VehicleHash.Chino2:
+						case VehicleHash.Moonbeam2:
+						case VehicleHash.Primo2:
+						case VehicleHash.Voodoo2:
+						case VehicleHash.SabreGT2:
+						case VehicleHash.Tornado5:
+						case VehicleHash.Virgo2:
+						case VehicleHash.Minivan2:
+						case VehicleHash.SlamVan3:
+						case VehicleHash.Faction3:
+							res.AddRange(new VehicleWheelType[] {VehicleWheelType.BennysOriginals, VehicleWheelType.BennysBespoke});
+							break;
+						case VehicleHash.SultanRS:
+						case VehicleHash.Banshee2:
+							res.Add(VehicleWheelType.BennysOriginals);
+							break;
+					}
+					return res.ToArray();
+				}
+				return new VehicleWheelType[0];
+			}
+		}
+
+		public string LocalizedWheelTypeName
+		{
+			get { return GetLocalizedWheelTypeName(WheelType); }
+		}
+
+		public static string GetLocalizedWheelTypeName(VehicleWheelType wheelType)
+		{
+			if (_wheelNames.ContainsKey(wheelType))
+			{
+				if (Game.DoesGXTEntryExist(_wheelNames[wheelType].Item1))
+				{
+					return Game.GetGXTEntry(_wheelNames[wheelType].Item1);
+				}
+				return _wheelNames[wheelType].Item2;
+			}
+			throw new ArgumentException("Wheel Type is undefined", "wheelType");
 		}
 
 		public void InstallModKit()
