d35ca9f5788381279570b7bdd3130d0c3746d6d9
Nathan James <nayjames@hotmail.com>
2016-07-21 16:16:54 +0100

    Added Documentation

--- source/scripting/ButtonCombination.cs
+++ source/scripting/ButtonCombination.cs
@@ -1,6 +1,9 @@
 using System;
 namespace GTA
 {
+    /// <summary>
+    /// The Buttons enum used for creating <see cref="ButtonCombination"/>s
+    /// </summary>
     public enum Button
     {
         PadUp = 117,
@@ -17,11 +20,26 @@ namespace GTA
         PadRT = 52
     }
 
+    /// <summary>
+    /// A value type used for checking if a sequence of <see cref="Button"/>s has been pressed.
+    /// Check if the combination has been entered with <see cref="Game.WasButtonCombinationJustEntered"/>.
+    /// </summary>
     public struct ButtonCombination
     {
+        /// <summary>
+        /// Gets the calculated hash of the Sequence
+        /// </summary>
         public int Hash { get; private set; }
+        /// <summary>
+        /// Gets the length of the sequence
+        /// </summary>
         public int Length { get; private set; }
 
+        /// <summary>
+        /// Creates a <see cref="ButtonCombination"/> from a given list of <see cref="Button"/>s.
+        /// </summary>
+        /// <param name="buttons">The sequence of <see cref="Button"/>s in the order a user should enter in game.</param>
+        /// <remarks>There must be between 6 and 15 inclusive <see cref="Button"/>s otherwise an <see cref="ArgumentException"/> is thrown</remarks>
         public ButtonCombination(params Button[] buttons)
         {
             if (buttons.Length < 6 || buttons.Length > 15)
--- source/scripting/Game.cs
+++ source/scripting/Game.cs
@@ -403,9 +403,18 @@ namespace GTA
 			return ScriptDomain.CurrentDomain.IsKeyPressed(key);
 		}
 
+        /// <summary>
+        /// Gets whether a <see cref="ButtonCombination"/> was entered.
+        /// </summary>
+        /// <param name="combination">The <see cref="ButtonCombination"/> to check against</param>
+        /// <returns><c>true</c> if the <see cref="ButtonCombination"/> was just entered; otherwise, <c>false</c></returns>
+        /// <remarks>
+        /// Only works for Gamepad inputs
+        /// Cheat combinations use the same system
+        /// </remarks>
 	    public static bool WasButtonCombinationJustEntered(ButtonCombination combination)
 	    {
-            return Function.Call<bool>(Native.Hash._HAS_BUTTON_COMBINATION_JUST_BEEN_ENTERED, combination.Hash, combination.Length);
+            return Function.Call<bool>(Hash._HAS_BUTTON_COMBINATION_JUST_BEEN_ENTERED, combination.Hash, combination.Length);
         }
 
 		/// <summary>
