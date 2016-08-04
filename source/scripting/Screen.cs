e82f98375b898dd0b0c2331135f40a7de8bd359c
Nathan James <nayjames@hotmail.com>
2016-08-04 12:14:12 +0100

    Added support for ScreenEffects

--- source/scripting/Screen.cs
+++ source/scripting/Screen.cs
@@ -85,6 +85,91 @@ namespace GTA.UI
 		NormalWithMinus
 	}
 
+	public enum ScreenEffect
+	{
+		SwitchHudIn,
+		SwitchHudOut,
+		FocusIn,
+		FocusOut,
+		MinigameEndNeutral,
+		MinigameEndTrevor,
+		MinigameEndFranklin,
+		MinigameEndMichael,
+		MinigameTransitionOut,
+		MinigameTransitionIn,
+		SwitchShortNeutralIn,
+		SwitchShortFranklinIn,
+		SwitchShortTrevorIn,
+		SwitchShortMichaelIn,
+		SwitchOpenMichaelIn,
+		SwitchOpenFranklinIn,
+		SwitchOpenTrevorIn,
+		SwitchHudMichaelOut,
+		SwitchHudFranklinOut,
+		SwitchHudTrevorOut,
+		SwitchShortFranklinMid,
+		SwitchShortMichaelMid,
+		SwitchShortTrevorMid,
+		DeathFailOut,
+		CamPushInNeutral,
+		CamPushInFranklin,
+		CamPushInMichael,
+		CamPushInTrevor,
+		SwitchSceneFranklin,
+		SwitchSceneTrevor,
+		SwitchSceneMichael,
+		SwitchSceneNeutral,
+		MpCelebWin,
+		MpCelebWinOut,
+		MpCelebLose,
+		MpCelebLoseOut,
+		DeathFailNeutralIn,
+		DeathFailMpDark,
+		DeathFailMpIn,
+		MpCelebPreloadFade,
+		PeyoteEndOut,
+		PeyoteEndIn,
+		PeyoteIn,
+		PeyoteOut,
+		MpRaceCrash,
+		SuccessFranklin,
+		SuccessTrevor,
+		SuccessMichael,
+		DrugsMichaelAliensFightIn,
+		DrugsMichaelAliensFight,
+		DrugsMichaelAliensFightOut,
+		DrugsTrevorClownsFightIn,
+		DrugsTrevorClownsFight,
+		DrugsTrevorClownsFightOut,
+		HeistCelebPass,
+		HeistCelebPassBw,
+		HeistCelebEnd,
+		HeistCelebToast,
+		MenuMgHeistIn,
+		MenuMgTournamentIn,
+		MenuMgSelectionIn,
+		ChopVision,
+		DmtFlightIntro,
+		DmtFlight,
+		DrugsDrivingIn,
+		DrugsDrivingOut,
+		SwitchOpenNeutralFib5,
+		HeistLocate,
+		MpJobLoad,
+		RaceTurbo,
+		MpIntroLogo,
+		HeistTripSkipFade,
+		MenuMgHeistOut,
+		MpCoronaSwitch,
+		MenuMgSelectionTint,
+		SuccessNeutral,
+		ExplosionJosh3,
+		SniperOverlay,
+		RampageOut,
+		Rampage,
+		DontTazemeBro,
+	}
+
 	public sealed class Notification
 	{
 		#region Fields
@@ -111,30 +196,28 @@ namespace GTA.UI
 		/// The base width of the screen used for all UI Calculations, unless ScaledDraw is used
 		/// </summary>
 		public const float Width = 1280f;
+
 		/// <summary>
 		/// The base height of the screen used for all UI Calculations
 		/// </summary>
 		public const float Height = 720f;
+
 		/// <summary>
 		/// Gets the current Screen Aspect Ratio
 		/// </summary>		   
 		public static float AspectRatio
 		{
-			get
-			{
-				return Function.Call<float>(Hash._GET_SCREEN_ASPECT_RATIO, 0);
-			}
+			get { return Function.Call<float>(Hash._GET_SCREEN_ASPECT_RATIO, 0); }
 		}
+
 		/// <summary>
 		/// Gets the width of the scaled against a 720pixel height base.
 		/// </summary>
 		public static float ScaledWidth
 		{
-			get
-			{
-				return Height * AspectRatio;
-			}
+			get { return Height*AspectRatio; }
 		}
+
 		/// <summary>
 		/// Gets the actual Screen resolution the game is being rendered at
 		/// </summary>
@@ -163,7 +246,8 @@ namespace GTA.UI
 
 			for (int i = 0; i < message.Length; i += maxStringLength)
 			{
-				Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, message.Substring(i, System.Math.Min(maxStringLength, message.Length - i)));
+				Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME,
+					message.Substring(i, System.Math.Min(maxStringLength, message.Length - i)));
 			}
 
 			Function.Call(Hash._DRAW_SUBTITLE_TIMED, duration, 1);
@@ -180,7 +264,8 @@ namespace GTA.UI
 
 			for (int i = 0; i < helpText.Length; i += maxStringLength)
 			{
-				Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, helpText.Substring(i, System.Math.Min(maxStringLength, helpText.Length - i)));
+				Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME,
+					helpText.Substring(i, System.Math.Min(maxStringLength, helpText.Length - i)));
 			}
 			Function.Call(Hash._DISPLAY_HELP_TEXT_FROM_STRING_LABEL, 0, 0, 1, -1);
 		}
@@ -199,7 +284,8 @@ namespace GTA.UI
 
 			for (int i = 0; i < message.Length; i += maxStringLength)
 			{
-				Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, message.Substring(i, System.Math.Min(maxStringLength, message.Length - i)));
+				Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME,
+					message.Substring(i, System.Math.Min(maxStringLength, message.Length - i)));
 			}
 
 			return new Notification(Function.Call<int>(Hash._DRAW_NOTIFICATION, blinking, true));
@@ -215,6 +301,7 @@ namespace GTA.UI
 		{
 			return WorldToScreen(position, scaleWidth ? ScaledWidth : Width, Height);
 		}
+
 		private static PointF WorldToScreen(Vector3 position, float screenWidth, float screenHeight)
 		{
 			var pointX = new OutputArgument();
@@ -225,7 +312,7 @@ namespace GTA.UI
 				return PointF.Empty;
 			}
 
-			return new PointF(pointX.GetResult<float>() * screenWidth, pointY.GetResult<float>() * screenHeight);
+			return new PointF(pointX.GetResult<float>()*screenWidth, pointY.GetResult<float>()*screenHeight);
 		}
 
 
@@ -240,7 +327,8 @@ namespace GTA.UI
 			/// <see cref="LoadingSpinnerType.Clockwise1"/>, <see cref="LoadingSpinnerType.Clockwise2"/>, <see cref="LoadingSpinnerType.Clockwise3"/> and <see cref="LoadingSpinnerType.RegularClockwise"/> all see to be the same. 
 			/// But Rockstar always seem to use the <see cref="LoadingSpinnerType.RegularClockwise"/> in the scripts
 			/// </remarks>
-			public static void Show(string loadingText = null, LoadingSpinnerType spinnerType = LoadingSpinnerType.RegularClockwise)
+			public static void Show(string loadingText = null,
+				LoadingSpinnerType spinnerType = LoadingSpinnerType.RegularClockwise)
 			{
 				if (IsActive)
 					Hide();
@@ -250,10 +338,11 @@ namespace GTA.UI
 				}
 				else
 				{
-					Function.Call((Hash)0xABA17D7CE615ADBF, MemoryAccess.StringPtr);//TO DO update this to Hash._SET_LOADING_PROMPT_TEXT_ENTRY when Hash enum next gets rebuilt
+					Function.Call((Hash) 0xABA17D7CE615ADBF, MemoryAccess.StringPtr);
+						//TO DO update this to Hash._SET_LOADING_PROMPT_TEXT_ENTRY when Hash enum next gets rebuilt
 					Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, loadingText);
 				}
-				Function.Call((Hash)0xBD12F8228410D9B4, spinnerType);//TO DO update to Hash._SHOW_LOADING_PROMPT
+				Function.Call((Hash) 0xBD12F8228410D9B4, spinnerType); //TO DO update to Hash._SHOW_LOADING_PROMPT
 			}
 
 			/// <summary>
@@ -262,14 +351,16 @@ namespace GTA.UI
 			public static void Hide()
 			{
 				if (IsActive)
-					Function.Call((Hash)0x10D373323E5B9C0D);//TO DO update to Hash._REMOVE_LOADING_PROMPT
+					Function.Call((Hash) 0x10D373323E5B9C0D); //TO DO update to Hash._REMOVE_LOADING_PROMPT
 			}
+
 			/// <summary>
 			/// Gets a value indicating whether the Loading Prompt is currently being displayed
 			/// </summary>
 			public static bool IsActive
 			{
-				get { return Function.Call<bool>((Hash)0xD422FCC5F239A915); }//TO DO update hash to Hash._IS_LOADING_PROMPT_BEING_DISPLAYED
+				get { return Function.Call<bool>((Hash) 0xD422FCC5F239A915); }
+				//TO DO update hash to Hash._IS_LOADING_PROMPT_BEING_DISPLAYED
 			}
 
 		}
@@ -285,6 +376,7 @@ namespace GTA.UI
 			{
 				return Function.Call<bool>(Hash.IS_HUD_COMPONENT_ACTIVE, component);
 			}
+
 			/// <summary>
 			/// Draws the specified <see cref="HudComponent"/> this frame.
 			/// </summary>
@@ -294,6 +386,7 @@ namespace GTA.UI
 			{
 				Function.Call(Hash.SHOW_HUD_COMPONENT_THIS_FRAME, component);
 			}
+
 			/// <summary>
 			/// Hides the specified <see cref="HudComponent"/> this frame.
 			/// </summary>
@@ -310,6 +403,7 @@ namespace GTA.UI
 			{
 				Function.Call(Hash._SHOW_CURSOR_THIS_FRAME);
 			}
+
 			/// <summary>
 			/// Gets or sets the sprite the cursor should used when drawn
 			/// </summary>
@@ -334,8 +428,9 @@ namespace GTA.UI
 			public static bool IsRadarVisible
 			{
 				get { return !Function.Call<bool>(Hash.IS_RADAR_HIDDEN); }
-				set { Function.Call(Hash.DISPLAY_RADAR, value);}
+				set { Function.Call(Hash.DISPLAY_RADAR, value); }
 			}
+
 			/// <summary>
 			/// Sets how far the Minimap should be zoomed in
 			/// </summary>
@@ -344,10 +439,7 @@ namespace GTA.UI
 			/// </value>
 			public static int RadarZoom
 			{
-				set
-				{
-					Function.Call(Hash.SET_RADAR_ZOOM, value);
-				}
+				set { Function.Call(Hash.SET_RADAR_ZOOM, value); }
 			}
 		}
 
@@ -361,10 +453,7 @@ namespace GTA.UI
 			/// </value>
 			public static bool IsFadedIn
 			{
-				get
-				{
-					return Function.Call<bool>(Hash.IS_SCREEN_FADED_IN);
-				}
+				get { return Function.Call<bool>(Hash.IS_SCREEN_FADED_IN); }
 			}
 
 			/// <summary>
@@ -375,10 +464,7 @@ namespace GTA.UI
 			/// </value>
 			public static bool IsFadedOut
 			{
-				get
-				{
-					return Function.Call<bool>(Hash.IS_SCREEN_FADED_OUT);
-				}
+				get { return Function.Call<bool>(Hash.IS_SCREEN_FADED_OUT); }
 			}
 
 			/// <summary>
@@ -389,10 +475,7 @@ namespace GTA.UI
 			/// </value>
 			public static bool IsFadingIn
 			{
-				get
-				{
-					return Function.Call<bool>(Hash.IS_SCREEN_FADING_IN);
-				}
+				get { return Function.Call<bool>(Hash.IS_SCREEN_FADING_IN); }
 			}
 
 			/// <summary>
@@ -403,10 +486,7 @@ namespace GTA.UI
 			/// </value>
 			public static bool IsFadingOut
 			{
-				get
-				{
-					return Function.Call<bool>(Hash.IS_SCREEN_FADING_OUT);
-				}
+				get { return Function.Call<bool>(Hash.IS_SCREEN_FADING_OUT); }
 			}
 
 			/// <summary>
@@ -425,6 +505,123 @@ namespace GTA.UI
 			public static void FadeOut(int time)
 			{
 				Function.Call(Hash.DO_SCREEN_FADE_OUT, time);
+			}
+		}
+
+		public static class Effects
+		{
+			private static readonly string[] _effects = new string[]
+			{
+				"SwitchHUDIn",
+				"SwitchHUDOut",
+				"FocusIn",
+				"FocusOut",
+				"MinigameEndNeutral",
+				"MinigameEndTrevor",
+				"MinigameEndFranklin",
+				"MinigameEndMichael",
+				"MinigameTransitionOut",
+				"MinigameTransitionIn",
+				"SwitchShortNeutralIn",
+				"SwitchShortFranklinIn",
+				"SwitchShortTrevorIn",
+				"SwitchShortMichaelIn",
+				"SwitchOpenMichaelIn",
+				"SwitchOpenFranklinIn",
+				"SwitchOpenTrevorIn",
+				"SwitchHUDMichaelOut",
+				"SwitchHUDFranklinOut",
+				"SwitchHUDTrevorOut",
+				"SwitchShortFranklinMid",
+				"SwitchShortMichaelMid",
+				"SwitchShortTrevorMid",
+				"DeathFailOut",
+				"CamPushInNeutral",
+				"CamPushInFranklin",
+				"CamPushInMichael",
+				"CamPushInTrevor",
+				"SwitchSceneFranklin",
+				"SwitchSceneTrevor",
+				"SwitchSceneMichael",
+				"SwitchSceneNeutral",
+				"MP_Celeb_Win",
+				"MP_Celeb_Win_Out",
+				"MP_Celeb_Lose",
+				"MP_Celeb_Lose_Out",
+				"DeathFailNeutralIn",
+				"DeathFailMPDark",
+				"DeathFailMPIn",
+				"MP_Celeb_Preload_Fade",
+				"PeyoteEndOut",
+				"PeyoteEndIn",
+				"PeyoteIn",
+				"PeyoteOut",
+				"MP_race_crash",
+				"SuccessFranklin",
+				"SuccessTrevor",
+				"SuccessMichael",
+				"DrugsMichaelAliensFightIn",
+				"DrugsMichaelAliensFight",
+				"DrugsMichaelAliensFightOut",
+				"DrugsTrevorClownsFightIn",
+				"DrugsTrevorClownsFight",
+				"DrugsTrevorClownsFightOut",
+				"HeistCelebPass",
+				"HeistCelebPassBW",
+				"HeistCelebEnd",
+				"HeistCelebToast",
+				"MenuMGHeistIn",
+				"MenuMGTournamentIn",
+				"MenuMGSelectionIn",
+				"ChopVision",
+				"DMT_flight_intro",
+				"DMT_flight",
+				"DrugsDrivingIn",
+				"DrugsDrivingOut",
+				"SwitchOpenNeutralFIB5",
+				"HeistLocate",
+				"MP_job_load",
+				"RaceTurbo",
+				"MP_intro_logo",
+				"HeistTripSkipFade",
+				"MenuMGHeistOut",
+				"MP_corona_switch",
+				"MenuMGSelectionTint",
+				"SuccessNeutral",
+				"ExplosionJosh3",
+				"SniperOverlay",
+				"RampageOut",
+				"Rampage",
+				"Dont_tazeme_bro"
+			};
+
+			private static string EffectToString(ScreenEffect effectName)
+			{
+				if ((int) effectName >= 0 && (int) effectName <= _effects.Length)
+				{
+					return _effects[(int) effectName];
+				}
+				return "INVALID";
+			}
+
+			public static void Start(ScreenEffect effectName, int duration = 0, bool looped = false)
+			{
+				Function.Call(Hash._START_SCREEN_EFFECT, EffectToString(effectName), duration, looped);
+			}
+
+			public static void Stop()
+			{
+				Function.Call(Hash._STOP_ALL_SCREEN_EFFECTS);
+			}
+
+			public static void Stop(ScreenEffect effectName)
+			{
+				Function.Call(Hash._STOP_SCREEN_EFFECT, EffectToString(effectName));
+			}
+
+			public static bool IsActive(ScreenEffect effectName)
+			{
+				return Function.Call<bool>(Hash._GET_SCREEN_EFFECT_IS_ACTIVE, EffectToString(effectName));
 			}
 		}
 	}
