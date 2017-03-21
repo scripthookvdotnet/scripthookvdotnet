88e44f70496282dd7fe2cedc5b59201a5a452f59
kagikn <kagikn.3939@gmail.com>
2017-03-21 23:40:52 +0900

    Add 1.0.1011.1 to GameVersion enum

--- source/scripting/Game.cs
+++ source/scripting/Game.cs
@@ -36,7 +36,9 @@ namespace GTA
 		v1_0_877_1_Steam,
 		v1_0_877_1_NoSteam,
 		v1_0_944_2_Steam,
-		v1_0_944_2_NoSteam
+		v1_0_944_2_NoSteam,
+		v1_0_1011_1_Steam,
+		v1_0_1011_1_NoSteam
 	}
 	public enum Language
 	{
