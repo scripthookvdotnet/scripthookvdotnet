57c6b2f83e5bbd9c59d48702e00667b9135f66c7
crosire <crosiredev@gmail.com>
2017-05-05 19:02:13 +0200

    Set assembly version based on value of environment variable "SHVDN_VERSION"

--- source/AssemblyInfo.cpp
+++ source/AssemblyInfo.cpp
@@ -3,4 +3,8 @@ using namespace System::Reflection;
 [assembly:AssemblyTitleAttribute("Community Script Hook V .NET")];
 [assembly:AssemblyDescriptionAttribute("An ASI plugin for Grand Theft Auto V, which allows running scripts written in any .NET language in-game.")];
 [assembly:AssemblyCopyrightAttribute("Copyright © 2015 crosire")];
-[assembly:AssemblyVersionAttribute("3.0.0")];
+
+#ifdef SHVDN_VERSION
+[assembly:AssemblyVersionAttribute(SHVDN_VERSION)];
+[assembly:AssemblyFileVersionAttribute(SHVDN_VERSION)];
+#endif
