1f140898f6752096a7e54b22e3dd5afc69e5e50f
crosire <crosiredev@gmail.com>
2017-06-18 16:29:15 +0200

    Add separate loader ASI that can load multiple ScriptHookVDotNet versions

--- source/core/Main.cpp
+++ source/core/Main.cpp
@@ -27,115 +27,41 @@ using namespace System;
 using namespace System::Reflection;
 namespace WinForms = System::Windows::Forms;
 
-ref struct ScriptHook
+ref struct ScriptHookVDotNet
 {
 	static GTA::ScriptDomain ^Domain = nullptr;
+
+	static bool Init()
+	{
+		if (!Object::ReferenceEquals(Domain, nullptr))
+		{
+			GTA::ScriptDomain::Unload(Domain);
+		}
+
+		auto settings = GTA::ScriptSettings::Load(IO::Path::ChangeExtension(Assembly::GetExecutingAssembly()->Location, ".ini"));
+		Domain = GTA::ScriptDomain::Load(settings->GetValue<String ^>(String::Empty, "ScriptsLocation", "scripts"));
+
+		if (!Object::ReferenceEquals(Domain, nullptr))
+		{
+			Domain->Start();
+
+			return true;
+		}
+
+		return false;
+	}
+	static void Tick()
+	{
+		Domain->DoTick();
+	}
+
+	static void KeyboardMessage(WinForms::Keys key, bool status, bool statusCtrl, bool statusShift, bool statusAlt)
+	{
+		if (Object::ReferenceEquals(Domain, nullptr))
+		{
+			return;
+		}
+
+		Domain->DoKeyboardMessage(key, status, statusCtrl, statusShift, statusAlt);
+	}
 };
-
-bool ManagedInit()
-{
-	if (!Object::ReferenceEquals(ScriptHook::Domain, nullptr))
-	{
-		GTA::ScriptDomain::Unload(ScriptHook::Domain);
-	}
-
-	auto settings = GTA::ScriptSettings::Load(IO::Path::ChangeExtension(Assembly::GetExecutingAssembly()->Location, ".ini"));
-	ScriptHook::Domain = GTA::ScriptDomain::Load(settings->GetValue<String ^>(String::Empty, "ScriptsLocation", "scripts"));
-
-	if (!Object::ReferenceEquals(ScriptHook::Domain, nullptr))
-	{
-		ScriptHook::Domain->Start();
-
-		return true;
-	}
-
-	return false;
-}
-void ManagedTick()
-{
-	ScriptHook::Domain->DoTick();
-}
-void ManagedKeyboardMessage(int key, bool status, bool statusCtrl, bool statusShift, bool statusAlt)
-{
-	if (Object::ReferenceEquals(ScriptHook::Domain, nullptr))
-	{
-		return;
-	}
-
-	ScriptHook::Domain->DoKeyboardMessage(static_cast<WinForms::Keys>(key), status, statusCtrl, statusShift, statusAlt);
-}
-
-#pragma unmanaged
-
-#include <Main.h>
-#include <Windows.h>
-
-bool sGameReloaded = false;
-PVOID sMainFib = nullptr;
-PVOID sScriptFib = nullptr;
-
-static void ScriptMain()
-{
-	sGameReloaded = true;
-
-	// ScriptHookV already turned the current thread into a fiber, so we can safely retrieve it.
-	sMainFib = GetCurrentFiber();
-
-	// Check if our CLR fiber already exists. It should be created only once for the entire lifetime of the game process.
-	if (sScriptFib == nullptr)
-	{
-		const LPFIBER_START_ROUTINE FiberMain = [](LPVOID lpFiberParameter) {
-			while (ManagedInit())
-			{
-				sGameReloaded = false;
-
-				// If the game is reloaded, ScriptHookV will call the script main function again.
-				// This will set the global 'sGameReloaded' variable to 'true' and on the next fiber switch to our CLR fiber, run into this condition, therefore exiting the inner loop and re-initialize.
-				while (!sGameReloaded)
-				{
-					ManagedTick();
-
-					// Switch back to main script fiber used by ScriptHookV.
-					// Code continues from here the next time the loop below switches back to our CLR fiber.
-					SwitchToFiber(sMainFib);
-				}
-			}
-		};
-
-		// Create our own fiber for the common language runtime, aka CLR, once.
-		// This is done because ScriptHookV switches its internal fibers sometimes, which would corrupt the CLR stack.
-		sScriptFib = CreateFiber(0, FiberMain, nullptr);
-	}
-
-	while (true)
-	{
-		// Yield execution and give it back to ScriptHookV.
-		scriptWait(0);
-
-		// Switch to our CLR fiber and wait for it to switch back.
-		SwitchToFiber(sScriptFib);
-	}
-}
-static void ScriptKeyboardMessage(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, BOOL isWithAlt, BOOL wasDownBefore, BOOL isUpNow)
-{
-	ManagedKeyboardMessage(static_cast<int>(key), isUpNow == FALSE, (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0, (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0, isWithAlt != FALSE);
-}
-
-BOOL WINAPI DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpvReserved)
-{
-	switch (fdwReason)
-	{
-		case DLL_PROCESS_ATTACH:
-			DisableThreadLibraryCalls(hModule);
-			scriptRegister(hModule, &ScriptMain);
-			keyboardHandlerRegister(&ScriptKeyboardMessage);
-			break;
-		case DLL_PROCESS_DETACH:
-			DeleteFiber(sScriptFib);
-			scriptUnregister(hModule);
-			keyboardHandlerUnregister(&ScriptKeyboardMessage);
-			break;
-	}
-
-	return TRUE;
-}
