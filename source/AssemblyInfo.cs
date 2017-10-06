bbcdf3ce3fc2017d82af83d8811594ae99514074
crosire <crosiredev@gmail.com>
2017-10-06 18:47:17 +0200

    Clean up solution and project files

---
+++ source/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Community Script Hook V .NET")]
+[assembly: AssemblyDescription("An ASI plugin for Grand Theft Auto V, which allows running scripts written in any .NET language in-game.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScriptHookVDotNet")]
+[assembly: AssemblyCopyright("Copyright © 2015 crosire")]
+[assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//   Major Version . Minor Version . Build Number . Revision
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
