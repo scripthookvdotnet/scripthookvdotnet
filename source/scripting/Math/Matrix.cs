579de949e5e3ceb9fc8d65cc3a8f32b5c2684f3d
kagikn <kagikn.3939@gmail.com>
2018-02-17 15:32:23 +0900

    Don't inverse themselves in InverseTransformPoint

--- source/scripting/Math/Matrix.cs
+++ source/scripting/Math/Matrix.cs
@@ -385,8 +385,7 @@ namespace GTA.Math
 		/// <returns>The original vertex location before being transformed by the given <see cref="Matrix"/></returns>
 		public Vector3 InverseTransformPoint(Vector3 vector)
 		{
-			Invert();
-			return TransformPoint(vector);
+			return Invert(this).TransformPoint(vector);
 		}
 
 		/// <summary>
