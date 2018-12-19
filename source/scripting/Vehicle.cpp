b73c18d6406f09312e2dd483c366eeb8c5cf6076
ikt <emily66666666ok@outlook.com>
2018-12-19 12:33:14 +0100

    Add b1604 CVehicle offsets
    Needs offsets for b1365 and b1493 or confirmation they did not change

--- source/scripting/Vehicle.cpp
+++ source/scripting/Vehicle.cpp
@@ -539,6 +539,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x89C : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x8BC : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x8DC : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x92C : offset);
 
 		return address == 0 ? false : (*reinterpret_cast<int *>(address + offset) & (1 << 2)) != 0;
 	}
@@ -572,6 +574,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x89C : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x8BC : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x8DC : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x92C : offset);
 
 		return address == 0 ? false : (*reinterpret_cast<int *>(address + offset) & (1 << 1)) != 0;
 	}
@@ -706,6 +710,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x7F0 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x810 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x830 : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x870 : offset);
 
 		return address == 0 ? 0 : static_cast<int>(*reinterpret_cast<const unsigned char *>(address + offset));
 	}
@@ -719,6 +725,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x7F6 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x816 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x836 : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x876 : offset);
 
 		return address == 0 ? 0 : static_cast<int>(*reinterpret_cast<const unsigned char *>(address + offset));
 	}
@@ -742,7 +750,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x7F6 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x816 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x836 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x876 : offset);
+		
 		*reinterpret_cast<unsigned char *>(address + offset) = static_cast<unsigned char>(value);
 	}
 	float Vehicle::FuelLevel::get()
@@ -755,7 +765,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x7B8 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x7D4 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x7F4 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x834 : offset);
+		
 		return address == 0 ? 0.0f : *reinterpret_cast<const float *>(address + offset);
 	}
 	void Vehicle::FuelLevel::set(float value)
@@ -773,7 +785,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x7B8 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x7D4 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x7F4 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x834 : offset);
+		
 		*reinterpret_cast<float *>(address + offset) = value;
 	}
 	float Vehicle::CurrentRPM::get()
@@ -786,7 +800,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x824 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x844 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x864 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x8B4 : offset);
+		
 		return address == 0 ? 0.0f : *reinterpret_cast<const float *>(address + offset);
 	}
 	void Vehicle::CurrentRPM::set(float value)
@@ -799,7 +815,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x824 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x844 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x864 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x8B4 : offset);
+		
 		*reinterpret_cast<float *>(address + offset) = value;
 	}
 	float Vehicle::Acceleration::get()
@@ -812,7 +830,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x834 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x854 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x874 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x8C4 : offset);
+		
 		return address == 0 ? 0.0f : *reinterpret_cast<const float *>(address + offset);
 	}
 	float Vehicle::WheelSpeed::get()
@@ -825,7 +845,9 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0xA00 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0xA10 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0xA30 : offset);
-
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0xA80 : offset);
+		
 		return address == 0 ? 0.0f : *reinterpret_cast<const float *>(address + offset);
 	}
 	float Vehicle::SteeringAngle::get()
@@ -838,6 +860,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x904 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x924 : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x944 : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x994 : offset);
 
 		if (!address == 0)
 		{
@@ -859,6 +883,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x8FC : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x91C : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x93C : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x98C : offset);
 
 		return address == 0 ? 0.0f : *reinterpret_cast<const float *>(address + offset);
 	}
@@ -877,6 +903,8 @@ namespace GTA
 		offset = (Game::Version >= GameVersion::VER_1_0_1103_2_STEAM ? 0x8FC : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1180_2_STEAM ? 0x91C : offset);
 		offset = (Game::Version >= GameVersion::VER_1_0_1290_1_STEAM ? 0x93C : offset);
+		// TODO: 1365, 1493.0/1
+		offset = (Game::Version >= GameVersion::VER_1_0_1604_0_STEAM ? 0x98C : offset);
 
 		*reinterpret_cast<float *>(address + offset) = value;
 	}
