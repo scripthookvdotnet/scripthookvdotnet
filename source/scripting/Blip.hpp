da4bbdfaa13220610f155493e17f385a76ba6745
kagikn <kagikn.3939@gmail.com>
2019-01-18 17:48:08 +0900

    Add description to some BlipSprite enums
    This helps developers avoid gettting confused

--- source/scripting/Blip.hpp
+++ source/scripting/Blip.hpp
@@ -117,10 +117,15 @@ namespace GTA
 	};
 	public enum class BlipSprite
 	{
+		///<summary>The default English text for this value is "Destination".</summary>
 		Standard = 1,
+		///<summary>The default English text for this value is "Destination".</summary>
 		BigBlip = 2,
+		///<summary>The default English text for this value is "Police".</summary>
 		PoliceOfficer = 3,
+		///<summary>When this value is set, the blip will flash. The default English text for this value is "Objective".</summary>
 		PoliceArea = 4,
+		///<summary>The sprite shape is square and the default English text for this value is "Objective".</summary>
 		Square = 5,
 		Player = 6,
 		North = 7,
@@ -132,6 +137,7 @@ namespace GTA
 		ArrowUp = 13,
 		ArrowDown = 14,
 		PoliceHelicopterAnimated = 15,
+		///<summary>The default English text for this value is "Police Plane".</summary>
 		Jet = 16,
 		Number1 = 17,
 		Number2 = 18,
@@ -145,18 +151,25 @@ namespace GTA
 		Number10 = 26,
 		GTAOCrew = 27,
 		GTAOFriendly = 28,
+		///<summary>The default English text for this value is "Cable Car".</summary>
 		Lift = 36,
 		RaceFinish = 38,
 		Safehouse = 40,
+		///<summary>The default English text for this value is "Police".</summary>
 		PoliceOfficer2 = 41,
+		///<summary>The default English text for this value is "Police Chase".</summary>
 		PoliceCarDot = 42,
 		PoliceHelicopter = 43,
+		///<summary>The default English text for this value is "Snitch".</summary>
 		ChatBubble = 47,
+		///<summary>The default English text for this value is "Criminal Carsteal".</summary>
 		Garage2 = 50,
+		///<summary>The default English text for this value is "Criminal Drugs".</summary>
 		Drugs = 51,
+		///<summary>The default English text for this value is "Criminal Holdups".</summary>
 		Store = 52,
 		PoliceCar = 56,
-		PolicePlayer = 57,
+		CriminalWanted = 57,
 		PolicePlayer = 58,
 		HeistStore = 59,
 		PoliceStation = 60,
@@ -167,6 +180,7 @@ namespace GTA
 		TowTruck = 68,
 		Barber = 71,
 		LosSantosCustoms = 72,
+		///<summary>The default English text for this value is "Clothes Store".</summary>
 		Clothes = 73,
 		TattooParlor = 75,
 		Simeon = 76,
@@ -185,10 +199,13 @@ namespace GTA
 		BiolabHeist = 96,
 		CarWash = 100,
 		ComedyClub = 102,
+		///<summary>The default English text for this value is "Darts".</summary>
 		Dart = 103,
 		ThePortOfLSHeist = 104,
 		TheBureauRaid = 105,
 		FIB = 106,
+		TheBigScore = 107,
+		///<summary>The default English text for this value is "Devin".</summary>
 		DollarSign = 108,
 		Golf = 109,
 		AmmuNation = 110,
@@ -202,15 +219,19 @@ namespace GTA
 		Exile2 = 123,
 		Michael2 = 124,
 		Triathlon = 126,
+		///<summary>The default English text for this value is "Off Road Racing".</summary>
 		OffRoadRaceFinish = 127,
 		GangPolice = 128,
 		GangMexicans = 129,
 		GangBikers = 130,
 		Snitch2 = 133,
+		///<summary>The default English text for this value is "Criminal Cuff Keys".</summary>
 		Key = 134,
 		MovieTheater = 135,
+		///<summary>The default English text for this value is "Music Venue".</summary>
 		Music = 136,
 		PoliceStation2 = 137,
+		///<summary>The default English text for this value is "Stash".</summary>
 		Marijuana = 140,
 		Hunting = 141,
 		Objective2 = 143,
@@ -231,10 +252,12 @@ namespace GTA
 		PointOfInterest = 162,
 		GTAOPassive = 163,
 		GTAOUsingMenu = 164,
+		///<summary>The default English text for this value is "Gang Police Partner".</summary>
 		Link = 171,
 		Minigun = 173,
 		GrenadeLauncher = 174,
 		Armor = 175,
+		///<summary>The default English text for this value is "Property Takeover".</summary>
 		Castle = 176,
 		CriminalSnitchMexican = 177,
 		CriminalSnitchLost = 178,
@@ -245,6 +268,7 @@ namespace GTA
 		PlayerPositon = 185,
 		BikerHandcuffKeys = 186,
 		VagosHandcuffKeys = 187,
+		///<summary>The default English text for this value is "Biker Handcuffs Closed".</summary>
 		Handcuffs = 188,
 		VagosHandcuffsClosed = 189,
 		Yoga = 197,
@@ -263,7 +287,9 @@ namespace GTA
 		Franklin2 = 210,
 		Franklin3 = 211,
 		FranklinC = 214,
+		///<summary>The default English text for this value is "Gang Vehicle".</summary>
 		PersonalVehicleCar = 225,
+		///<summary>The default English text for this value is "Gang Vehicle Bikers".</summary>
 		PersonalVehicleBike = 226,
 		GangVehiclePolice = 227,
 		GangPoliceHighlight = 233,
@@ -286,6 +312,7 @@ namespace GTA
 		CashPickupLost = 276,
 		CashPickupVagos = 277,
 		CashPickupPolice = 278,
+		///<summary>The default English text for this value is "Drop Off Hooker".</summary>
 		Hooker = 279,
 		Friend = 280,
 		CustodyDropoff = 285,
@@ -297,7 +324,9 @@ namespace GTA
 		CriminalCarstealVagos = 291,
 		SimeonFamily = 293,
 		BountyHit = 303,
+		///<summary>The default English text for this value is "UGC Mission".</summary>
 		GTAOMission = 304,
+		///<summary>The default English text for this value is "Horde".</summary>
 		GTAOSurvival = 305,
 		CrateDrop = 306,
 		PlaneDrop = 307,
@@ -307,13 +336,17 @@ namespace GTA
 		ArmWrestling = 311,
 		AmmuNationShootingRange = 313,
 		RaceAir = 314,
+		///<summary>The default English text for this value is "Street Race".</summary>
 		RaceCar = 315,
+		///<summary>The default English text for this value is "Sea Race".</summary>
 		RaceSea = 316,
 		TowTruck2 = 317,
 		GarbageTruck = 318,
 		GetawayCar = 326,
 		GangBike = 348,
+		///<summary>The default English text for this value is "Property For Sale".</summary>
 		SafehouseForSale = 350,
+		///<summary>The default English text for this value is "Gang Attack Package".</summary>
 		Package = 351,
 		MartinMadrazo = 352,
 		EnemyHelicopter = 353,
@@ -325,22 +358,28 @@ namespace GTA
 		Hangar = 359,
 		Helipad = 360,
 		JerryCan = 361,
+		///<summary>The default English text for this value is "Masks Store".</summary>
 		Masks = 362,
 		HeistSetup = 363,
 		Incapacitated = 364,
+		///<summary>The default English text for this value is "Spawn Point Pickup".</summary>
 		PickupSpawn = 365,
 		BoilerSuit = 366,
 		Completed = 367,
+		///<summary>The default English text for this value is "Missiles".</summary>
 		Rockets = 368,
 		GarageForSale = 369,
 		HelipadForSale = 370,
+		///<summary>The default English text for this value is "Dock For Sale".</summary>
 		MarinaForSale = 371,
 		HangarForSale = 372,
 		Business = 374,
 		BusinessForSale = 375,
+		///<summary>The default English text for this value is "Bike Race".</summary>
 		RaceBike = 376,
 		Parachute = 377,
 		TeamDeathmatch = 378,
+		///<summary>The default English text for this value is "Foot Race".</summary>
 		RaceFoot = 379,
 		VehicleDeathmatch = 380,
 		Barry = 381,
@@ -352,12 +391,14 @@ namespace GTA
 		Omega = 387,
 		Tonya = 388,
 		Paparazzo = 389,
+		///<summary>The default English text for this value is "Aim".</summary>
 		Crosshair = 390,
 		Creator = 398,
 		CreatorDirection = 399,
 		Abigail = 400,
 		Blimp = 401,
 		Repair = 402,
+		///<summary>The default English text for this value is "Raging".</summary>
 		Testosterone = 403,
 		Dinghy = 404,
 		Fanatic = 405,
@@ -369,13 +410,17 @@ namespace GTA
 		JerryCan2 = 415,
 		RP = 416,
 		GTAOPlayerSafehouse = 417,
+		///<summary>In GTA Online, when some player is a bounty and is in a safehouse, a blip with this sprite will be attached.</summary>
 		GTAOPlayerSafehouseDead = 418,
 		CaptureAmericanFlag = 419,
 		CaptureFlag = 420,
 		Tank = 421,
 		HelicopterAnimated = 422,
+		///<summary>The sprite is the same as <see cref="BlipSprite::Jet"></see> has, but the default English text for this value is "Jet".</summary>
 		Plane = 423,
+		///<summary>This enum is wrongly named. The sprite doesn't have outline, but you can change the color.</summary>
 		PlayerNoColor = 425,
+		///<summary>The default English text for this value is "Insurgent".</summary>
 		GunCar = 426,
 		Speedboat = 427,
 		Heist = 428,
@@ -386,6 +431,7 @@ namespace GTA
 		StuntRace = 435,
 		HotProperty,
 		KillListCompetitive,
+		///<summary>The default English text for this value is "Player King".</summary>
 		King = 439,
 		DeadDrop,
 		PennedIn,
@@ -411,9 +457,11 @@ namespace GTA
 		AssetRecovery,
 		YachtLocation,
 		Beasted,
+		///<summary>The default English text for this value is "Zoned".</summary>
 		Loading,
 		Random,
 		SlowTime,
+		///<summary>The default English text for this value is "Flipped".</summary>
 		Flip,
 		ThermalVision,
 		Doped,
@@ -434,6 +482,7 @@ namespace GTA
 		Ghost,
 		Detonator,
 		Bomb,
+		///<summary>The sprite image is a shield, but the default English text for this value is "Beast".</summary>
 		Shield,
 		Stunt,
 		Heart,
@@ -443,11 +492,17 @@ namespace GTA
 		CagedIn,
 		TurfWar,
 		Joust,
+		///<summary>The default English text for this value is "Weed Production".</summary>
 		Weed,
+		///<summary>The default English text for this value is "Weed Production".</summary>
 		Cocaine,
+		///<summary>The default English text for this value is "Weed Production".</summary>
 		IdentityCard,
+		///<summary>The default English text for this value is "Weed Production".</summary>
 		Meth,
+		///<summary>The default English text for this value is "Weed Production".</summary>
 		DollarBill,
+		///<summary>The default English text for this value is "Package", whose label text hash is used in Biker Business missions in GTA Online.</summary>
 		Package2,
 		Capture1,
 		Capture2,
@@ -461,6 +516,7 @@ namespace GTA
 		Capture10,
 		QuadBike,
 		Bus,
+		///<summary>The default English text for this value is "Drugs Package".</summary>
 		DrugPackage,
 		Hop,
 		Adversary4,
@@ -469,9 +525,11 @@ namespace GTA
 		Adversary12,
 		Adversary16,
 		Laptop,
+		///<summary>The default English text for this value is "Deadline".</summary>
 		Motorcycle,
 		SportsCar,
 		VehicleWarehouse,
+		///<summary>The default English text for this value is "Registration Papers".</summary>
 		Document,
 		PoliceStationInverted,
 		Junkyard,
@@ -491,9 +549,12 @@ namespace GTA
 		TargetG,
 		TargetH,
 		Juggernaut,
+		///<summary>The default English text for this value is "Special Vehicle Race Series".</summary>
 		SteeringWheel = 545,
+		///<summary>The default English text for this value is "Challenge Series".</summary>
 		Cup,
 		RocketBoost,
+		///<summary>The default English text for this value is "Homing Rocket".</summary>
 		Rocket,
 		MachineGun,
 		Parachute2,
