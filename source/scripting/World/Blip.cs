c98db33096e6949afbc3d1e2172ea3dcdc801724
kagikn <kagikn.3939@gmail.com>
2019-02-12 21:58:08 +0900

    Update BlipColor

--- source/scripting/World/Blip.cs
+++ source/scripting/World/Blip.cs
@@ -6,14 +6,113 @@ namespace GTA
 {
 	public enum BlipColor
 	{
-		White,
+		///<summary>The default RGB value of this color is the same as HUD_COLOUR_PURE_WHITE, whose default RGB value is #FFFFFF.</summary>
+		PureWhite,
 		Red,
 		Green,
 		Blue,
-		MichaelBlue = 42,
-		FranklinGreen,
-		TrevorOrange,
-		Yellow = 66
+		///<summary>The default RGB value of this color is the same as HUD_COLOUR_WHITE, whose default RGB value is #F0F0F0.</summary>
+		White,
+		Yellow,
+		NetPlayer1,
+		NetPlayer2,
+		NetPlayer3,
+		NetPlayer4,
+		NetPlayer5,
+		NetPlayer6,
+		NetPlayer7,
+		NetPlayer8,
+		NetPlayer9,
+		NetPlayer10,
+		NetPlayer11,
+		NetPlayer12,
+		NetPlayer13,
+		NetPlayer14,
+		NetPlayer15,
+		NetPlayer16,
+		NetPlayer17,
+		NetPlayer18,
+		NetPlayer19,
+		NetPlayer20,
+		NetPlayer21,
+		NetPlayer22,
+		NetPlayer23,
+		NetPlayer24,
+		NetPlayer25,
+		NetPlayer26,
+		NetPlayer27,
+		NetPlayer28,
+		NetPlayer29,
+		NetPlayer30,
+		NetPlayer31,
+		NetPlayer32,
+		Freemode,
+		InactiveMission,
+		GreyDark,
+		RedLight,
+		///<summary>This color is usually #65B4D4 sky blue, which is similar to <see cref="System.Drawing.Color.SkyBlue"></see>.</summary>
+		Michael,
+		///<summary>This color is usually #ABEDAB light green, which is very similar to <see cref="System.Drawing.Color.LightGreen"></see>.</summary>
+		Franklin,
+		///<summary>This color is usually #ABEDAB orange, which is very similar to <see cref="System.Drawing.Color.SandyBrown"></see>.</summary>
+		Trevor,
+		GolfPlayer1,
+		GolfPlayer2,
+		GolfPlayer3,
+		GolfPlayer4,
+		///<summary>This color is always the same as <see cref="Red"></see>, the only difference is color index.</summary>
+		Red2,
+		Purple,
+		Orange,
+		GreenDark,
+		BlueLight,
+		BlueDark,
+		Grey,
+		YellowDark,
+		///<summary>This color is always the same as <see cref="Blue"></see>, the only difference is color index.</summary>
+		Blue2,
+		PurpleDark,
+		///<summary>This color is always the same as <see cref="Red"></see>, the only difference is color index.</summary>
+		Red3,
+		///<summary>This color is always the same as <see cref="Yellow"></see>, the only difference is color index.</summary>
+		Yellow2,
+		Pink,
+		GreyLight,
+		Gang,
+		Gang2,
+		Gang3,
+		///<summary>The default color.</summary>
+		Yellow3,
+		///<summary>This color is always the same as <see cref="Blue"></see>, the only difference is color index.</summary>
+		Blue3,
+		///<summary>This color is always the same as <see cref="Blue"></see>, the only difference is color index.</summary>
+		Blue4,
+		///<summary>This color is always the same as <see cref="Green"></see>, the only difference is color index.</summary>
+		Green2,
+		///<summary>This color is always the same as <see cref="Yellow"></see>, the only difference is color index.</summary>
+		Yellow4,
+		///<summary>This color is always the same as <see cref="Yellow"></see>, the only difference is color index.</summary>
+		Yellow5,
+		///<summary>This color is always the same as <see cref="PureWhite"></see>, the only difference is color index.</summary>
+		PureWhite2,
+		///<summary>This color is always the same as <see cref="Yellow"></see>, the only difference is color index.</summary>
+		Yellow6,
+		///<summary>This color is always the same as <see cref="Blue"></see>, the only difference is color index.</summary>
+		Blue5,
+		///<summary>This color is always the same as <see cref="Red"></see>, the only difference is color index.</summary>
+		Red4,
+		RedDark,
+		///<summary>This color is always the same as <see cref="Blue"></see>, the only difference is color index.</summary>
+		Blue6,
+		///<summary>This color is always the same as <see cref="BlueDark"></see>, the only difference is color index.</summary>
+		BlueDark2,
+		///<summary>This color is always the same as <see cref="RedDark"></see>, the only difference is color index.</summary>
+		RedDark2,
+		MenuYellow,
+		SimpleBlipDefault,
+		Waypoint,
+		///<summary>This color is always the same as <see cref="Blue"></see>, the only difference is color index.</summary>
+		Blue7,
 	}
 	public enum BlipSprite
 	{
