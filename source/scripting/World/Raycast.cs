198e028043d96da85fa14d23ccae3271f1b1125d
kagikn <kagikn.3939@gmail.com>
2019-02-15 11:19:17 +0900

    Make RaycastResult readonly struct
    This class is only intended to get result

--- source/scripting/World/Raycast.cs
+++ source/scripting/World/Raycast.cs
@@ -3,7 +3,7 @@ using GTA.Native;
 
 namespace GTA
 {
-	public struct RaycastResult
+	public readonly struct RaycastResult
 	{
 		public RaycastResult(int handle) : this()
 		{
@@ -27,35 +27,35 @@ namespace GTA
 		/// <summary>
 		/// Gets the raycast result code.
 		/// </summary>
-		public int Result { get; private set; }
+		public int Result { get; }
 
 		/// <summary>
 		/// Gets the <see cref="Entity" /> this raycast collided with.
 		/// <remarks>Returns <c>null</c> if the raycast didn't collide with any <see cref="Entity"/>.</remarks>
 		/// </summary>
-		public Entity HitEntity { get; private set; }
+		public Entity HitEntity { get; }
 		/// <summary>
 		/// Gets the world coordinates where this raycast collided.
 		/// <remarks>Returns <see cref="Vector3.Zero"/> if the raycast didn't collide with anything.</remarks>
 		/// </summary>
-		public Vector3 HitPosition { get; private set; }
+		public Vector3 HitPosition { get; }
 
 		/// <summary>
 		/// Gets the normal of the surface where this raycast collided.
 		/// <remarks>Returns <see cref="Vector3.Zero"/> if the raycast didn't collide with anything.</remarks>
 		/// </summary>
-		public Vector3 SurfaceNormal { get; private set; }
+		public Vector3 SurfaceNormal { get; }
 
 		/// <summary>
 		/// Gets a hash indicating the material type of what this raycast collided with.
 		/// <remarks>Returns <see cref="MaterialHash.None"/> if the raycast didn't collide with anything.</remarks>
 		/// </summary>
-		public MaterialHash MaterialHash { get; private set; }
+		public MaterialHash MaterialHash { get; }
 
 		/// <summary>
 		/// Gets a value indicating whether this raycast collided with anything.
 		/// </summary>
-		public bool DidHit { get; private set; }
+		public bool DidHit { get; }
 		/// <summary>
 		/// Gets a value indicating whether this raycast collided with any <see cref="Entity"/>.
 		/// </summary>
