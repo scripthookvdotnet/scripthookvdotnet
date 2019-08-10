ec972e7c427ac008925b7f6058a768674c70d1c2
crosire <crosiredev@gmail.com>
2019-08-10 13:39:04 +0200

    Ignore assemblies that cause IO exceptions during reading

--- source/core/ScriptDomain.cs
+++ source/core/ScriptDomain.cs
@@ -642,43 +642,51 @@ namespace SHVDN
 
 		static bool IsManagedAssembly(string filename)
 		{
-			using (Stream file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+			try
 			{
-				if (file.Length < 64)
-					return false;
-
-				using (BinaryReader bin = new BinaryReader(file))
+				using (Stream file = new FileStream(filename, FileMode.Open, FileAccess.Read))
 				{
-					// PE header starts at offset 0x3C (60). Its a 4 byte header.
-					file.Position = 0x3C;
-					uint offset = bin.ReadUInt32();
-					if (offset == 0)
-						offset = 0x80;
-
-					// Ensure there is at least enough room for the following structures:
-					//     24 byte PE Signature & Header
-					//     28 byte Standard Fields         (24 bytes for PE32+)
-					//     68 byte NT Fields               (88 bytes for PE32+)
-					// >= 128 byte Data Dictionary Table
-					if (offset > file.Length - 256)
+					if (file.Length < 64)
 						return false;
 
-					// Check the PE signature. Should equal 'PE\0\0'.
-					file.Position = offset;
-					if (bin.ReadUInt32() != 0x00004550)
-						return false;
+					using (BinaryReader bin = new BinaryReader(file))
+					{
+						// PE header starts at offset 0x3C (60). Its a 4 byte header.
+						file.Position = 0x3C;
+						uint offset = bin.ReadUInt32();
+						if (offset == 0)
+							offset = 0x80;
 
-					// Read PE magic number from Standard Fields to determine format.
-					file.Position += 20;
-					var peFormat = bin.ReadUInt16();
-					if (peFormat != 0x10b /* PE32 */ && peFormat != 0x20b /* PE32Plus */)
-						return false;
+						// Ensure there is at least enough room for the following structures:
+						//     24 byte PE Signature & Header
+						//     28 byte Standard Fields         (24 bytes for PE32+)
+						//     68 byte NT Fields               (88 bytes for PE32+)
+						// >= 128 byte Data Dictionary Table
+						if (offset > file.Length - 256)
+							return false;
 
-					// Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
-					// When this is non-zero then the file contains CLI data otherwise not.
-					file.Position = offset + (peFormat == 0x10b ? 232 : 248);
-					return bin.ReadUInt32() != 0;
+						// Check the PE signature. Should equal 'PE\0\0'.
+						file.Position = offset;
+						if (bin.ReadUInt32() != 0x00004550)
+							return false;
+
+						// Read PE magic number from Standard Fields to determine format.
+						file.Position += 20;
+						var peFormat = bin.ReadUInt16();
+						if (peFormat != 0x10b /* PE32 */ && peFormat != 0x20b /* PE32Plus */)
+							return false;
+
+						// Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
+						// When this is non-zero then the file contains CLI data otherwise not.
+						file.Position = offset + (peFormat == 0x10b ? 232 : 248);
+						return bin.ReadUInt32() != 0;
+					}
 				}
+			}
+			catch
+			{
+				// This is likely not a valid assembly if any IO exceptions occur during reading
+				return false;
 			}
 		}
 
