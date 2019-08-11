cb4e1dcb3067b27e656a59b41b4c72632ff8a268
crosire <crosiredev@gmail.com>
2019-08-11 22:51:16 +0200

    Add assembly description to scripting API

--- source/scripting_v2/Properties/AssemblyInfo.cpp
+++ source/scripting_v2/Properties/AssemblyInfo.cpp
@@ -1,14 +1,14 @@
-using namespace System::Reflection;
+﻿using namespace System::Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitleAttribute("ScriptHookVDotNet2")];
-[assembly: AssemblyDescriptionAttribute("")];
+[assembly: AssemblyDescriptionAttribute("The .NET scripting API for Grand Theft Auto V.")];
 [assembly: AssemblyConfigurationAttribute("")];
 [assembly: AssemblyCompanyAttribute("")];
 [assembly: AssemblyProductAttribute("ScriptHookVDotNet")];
-[assembly: AssemblyCopyrightAttribute("Copyright � 2015 crosire")];
+[assembly: AssemblyCopyrightAttribute("Copyright © 2015 crosire")];
 [assembly: AssemblyTrademarkAttribute("")];
 [assembly: AssemblyCultureAttribute("")];
 
--- source/scripting_v3/Properties/AssemblyInfo.cs
+++ source/scripting_v3/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScriptHookVDotNet3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The .NET scripting API for Grand Theft Auto V.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ScriptHookVDotNet")]
