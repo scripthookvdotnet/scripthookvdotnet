b38d4445e47bad7a3595141821317f43a6a01c64
crosire <crosiredev@gmail.com>
2020-12-30 10:44:14 +0100

    Update version
    Fixes #970

--- source/scripting_v3/Properties/AssemblyInfo.cs
+++ source/scripting_v3/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.4.0")]
-[assembly: AssemblyFileVersion("3.0.4.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
