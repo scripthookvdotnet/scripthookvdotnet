f68c193a1b655a51fa87ef2f0f2913f94583521c
kagikn <kagikn.3939@gmail.com>
2021-08-19 18:33:40 +0900

    Add ProjectileCount and ProjectileCapacity

--- source/scripting_v3/GTA/World.cs
+++ source/scripting_v3/GTA/World.cs
@@ -345,6 +345,11 @@ namespace GTA
 		public static int PickupObjectCount => SHVDN.NativeMemory.GetPickupObjectCount();
 
 		/// <summary>
+		/// A fast way to get the total number of <see cref="Projectile"/>s spawned in the world.
+		/// </summary>
+		public static int ProjectileCount => SHVDN.NativeMemory.GetProjectileCount();
+
+		/// <summary>
 		/// The total number of <see cref="Vehicle"/>s that can exist in the world.
 		/// </summary>
 		/// <remarks>The game will crash when the number of <see cref="Vehicle"/> is the same as this limit and the game tries to create a <see cref="Vehicle"/>.</remarks>
@@ -363,6 +368,11 @@ namespace GTA
 		/// The total number of <see cref="Prop"/>s in the world associated with a <see cref="Pickup"/> that can exist in the world.
 		/// </summary>
 		public static int PickupObjectCapacity => SHVDN.NativeMemory.GetPickupObjectCapacity();
+		/// <summary>
+		/// The total number of <see cref="Projectile"/>s that can exist in the world.
+		/// Always returns 50 currently since the limit is hard-coded in the exe.
+		/// </summary>
+		public static int ProjectileCapacity => SHVDN.NativeMemory.GetProjectileCapacity();
 
 		/// <summary>
 		/// Gets the closest <see cref="Ped"/> to a given position in the World.
