scripthookvdotnet/scripthookvdotnet

commit d4a780e65a028e7d127d36fe2464f8ad1b17558c

Add getter methods for weapon hash for ped and weapon component hash (#1013)
* Add getter methods for weapon hash for ped and weapon component hash

Close #966

* Improve WeaponComponentCollection.GetComponentsFromHash

No more massive native calls when the weapon is added in some DLC

* Improve WeaponComponent.GetAttachmentPoint

No more massive native calls when the weapon is added in some DLC

* Improve Weapon.GetComponentsFromHash in v2 API

source/core/NativeMemory.cs
@@ -10,6 +10,7 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Linq;
 using static System.Runtime.InteropServices.Marshal;
 
 namespace SHVDN
@@ -283,6 +284,30 @@ namespace SHVDN
 
 			address = FindPattern("\x8B\x54\x00\x00\x00\x8D\x0D\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x8A\xC3", "xx???xx????x????xx");
 			cStreamingAddr = (ulong*)(*(int*)(address + 7) + address + 11);
+
+			address = FindPattern("\x48\x8B\x05\x00\x00\x00\x00\x41\x8B\x1E", "xxx????xxx");
+			weaponAndAmmoInfoArrayPtr = (RageAtArrayPtr*)(*(int*)(address + 3) + address + 7);
+
+			address = FindPattern("\x8B\x05\x00\x00\x00\x00\x44\x8B\xD3\x8D\x48\xFF", "xx????xxxxxx");
+			if (address != null)
+			{
+				weaponComponentArrayCountAddr = (uint*)(*(int*)(address + 2) + address + 6);
+
+				address = FindPattern("\x46\x8D\x04\x11\x48\x8D\x15\x00\x00\x00\x00\x41\xD1\xF8", "xxxxxxx????xxx", new IntPtr(address));
+				offsetForCWeaponComponentArrayAddr = (ulong)(address + 7);
+
+				address = FindPattern("\x74\x10\x49\x8B\xC9\xE8\x00\x00\x00\x00", "xxxxxx????");
+				var findAttachPointFuncAddr = new IntPtr((long)(*(int*)(address + 6) + address + 10));
+
+				address = FindPattern("\x4C\x8D\x81\x00\x00\x00\x00", "xxx????", findAttachPointFuncAddr);
+				weaponAttachPointsStartOffset = *(int*)(address + 3);
+				address = FindPattern("\x4D\x63\x98\x00\x00\x00\x00", "xxx????", new(address));
+				weaponAttachPointsEndOffset = *(int*)(address + 3) + weaponAttachPointsStartOffset;
+				address = FindPattern("\x4C\x63\x50\x00", "xxx?", new(address));
+				weaponAttachPointElementComponentCountOffset = *(byte*)(address + 3);
+				address = FindPattern("\x48\x83\xC0\x00", "xxx?", new(address));
+				weaponAttachPointElementSize = *(byte*)(address + 3);
+			}
 
 			address = FindPattern("\x33\xD2\x48\x85\xC0\x74\x1E\x0F\xBF\x88\x00\x00\x00\x00\x48\x8B\x05\x00\x00\x00\x00", "xxxxxxxxxx????xxx????");
 			if (address != null)
@@ -879,6 +904,26 @@ namespace SHVDN
 
 			return dest;
 		}
+
+		#region -- RAGE classes --
+
+		[StructLayout(LayoutKind.Explicit, Size=0xC)]
+		struct RageAtArrayPtr
+		{
+			[FieldOffset(0x0)]
+			internal ulong* data;
+			[FieldOffset(0x8)]
+			internal ushort size;
+			[FieldOffset(0xA)]
+			internal ushort capacity;
+
+			internal ulong GetElementAddress(int i)
+			{
+				return data[i];
+			}
+		}
+
+		#endregion
 
 		#region -- Cameras --
 
@@ -2295,6 +2340,216 @@ namespace SHVDN
 		#region -- Projectile Offsets --
 		public static int ProjectileAmmoInfoOffset { get; }
 		public static int ProjectileOwnerOffset { get; }
+		#endregion
+
+		#region -- Weapon Info And Ammo Info --
+
+		// The function uses rax and rdx registers in newer versions (probably since b2189), and it uses only rax register in older versions.
+		// The function returns the address where the class name hash is in all the version (the address will be the outVal address in newer versions).
+		delegate uint* GetClassNameHashOfCItemInfoDelegate(ulong unused, uint* outVal);
+		static Dictionary<ulong, GetClassNameHashOfCItemInfoDelegate> getClassNameHashOfCItemInfoCacheDict = new Dictionary<ulong, GetClassNameHashOfCItemInfoDelegate>();
+
+		static RageAtArrayPtr* weaponAndAmmoInfoArrayPtr;
+		static HashSet<uint> disallowWeaponHashSetForHumanPedsOnFoot = new HashSet<uint>() {
+				0x1B79F17,  /* weapon_briefcase_02 */
+				0x166218FF, /* weapon_passenger_rocket */
+				0x32A888BD, /* weapon_tranquilizer */
+				0x687652CE, /* weapon_stinger */
+				0x6D5E2801, /* weapon_bird_crap */
+				0x88C78EB7, /* weapon_briefcase */
+				0xFDBADCED, /* weapon_digiscanner */
+			};
+
+		static uint* weaponComponentArrayCountAddr;
+		// Store the offset instead of the calculated address for compatibility with mods like Weapon Limits Adjuster by alexguirre (although Weapon Limits Adjuster allocates a new array in the very beginning).
+		static ulong offsetForCWeaponComponentArrayAddr;
+		static int weaponAttachPointsStartOffset;
+		static int weaponAttachPointsEndOffset;
+		static int weaponAttachPointElementComponentCountOffset;
+		static int weaponAttachPointElementSize;
+
+		[StructLayout(LayoutKind.Explicit, Size=0x20)]
+		struct ItemInfo
+		{
+			[FieldOffset(0x0)]
+			internal ulong* vTable;
+			[FieldOffset(0x10)]
+			internal uint nameHash;
+			[FieldOffset(0x14)]
+			internal uint modelHash;
+			[FieldOffset(0x18)]
+			internal uint audioHash;
+			[FieldOffset(0x1C)]
+			internal uint slot;
+		}
+
+		static ItemInfo* FindItemInfoFromWeaponAndAmmoInfoArray(uint nameHash)
+		{
+			if (weaponAndAmmoInfoArrayPtr == null)
+			{
+				return null;
+			}
+
+			var weaponAndAmmoInfoElementCount = weaponAndAmmoInfoArrayPtr->size;
+
+			if (weaponAndAmmoInfoElementCount == 0)
+				return null;
+
+			int offset1 = 0, offset2 = weaponAndAmmoInfoElementCount - 1;
+			while (true)
+			{
+				int indexToRead = (offset1 + offset2) >> 1;
+				var weaponOrAmmoInfo = (ItemInfo*)weaponAndAmmoInfoArrayPtr->GetElementAddress(indexToRead);
+
+				if (weaponOrAmmoInfo->nameHash == nameHash)
+					return weaponOrAmmoInfo;
+
+				// The array is sorted in ascending order
+				if (weaponOrAmmoInfo->nameHash <= nameHash)
+					offset1 = indexToRead + 1;
+				else
+					offset2 = indexToRead - 1;
+
+				if (offset1 > offset2)
+					return null;
+			}
+		}
+
+		static ItemInfo* FindWeaponInfo(uint nameHash)
+		{
+			var itemInfoPtr = FindItemInfoFromWeaponAndAmmoInfoArray(nameHash);
+
+			if (itemInfoPtr == null)
+				return null;
+
+			var GetClassNameHashFunc = CreateGetClassNameHashDelegateIfNotCreated(itemInfoPtr->vTable[2]);
+			uint outVal = 0;
+			var returnClassNameHashAddr = GetClassNameHashFunc(0, &outVal);
+
+			const uint CWEAPONINFO_NAME_HASH = 0x861905B4;
+			if (*returnClassNameHashAddr == CWEAPONINFO_NAME_HASH)
+				return itemInfoPtr;
+
+			return null;
+		}
+
+		public static uint GetAttachmentPointHash(uint weaponHash, uint componentHash)
+		{
+			var weaponInfo = FindWeaponInfo(weaponHash);
+
+			if (weaponInfo == null)
+				return 0xFFFFFFFF;
+
+			for (int attachPointOffset = weaponAttachPointsStartOffset; attachPointOffset < weaponAttachPointsEndOffset; attachPointOffset += weaponAttachPointElementSize)
+			{
+				int componentItemsOffset = attachPointOffset + 0x8;
+				var componentItemAddr = (byte*)weaponInfo + componentItemsOffset;
+				int componentItemsCount = *(int*)(componentItemAddr + weaponAttachPointElementComponentCountOffset);
+
+				if (componentItemsCount <= 0)
+					continue;
+
+				for (int i = 0; i < componentItemsCount; i++)
+				{
+					var componentHashInItemArray = *(uint*)((byte*)weaponInfo + componentItemsOffset + i * 0x8);
+					if (componentHashInItemArray == componentHash)
+						return *(uint*)((byte*)weaponInfo + componentItemsOffset);
+				}
+			}
+
+			return 0xFFFFFFFF;
+		}
+
+		public static List<uint> GetAllWeaponHashesForHumanPeds()
+		{
+			if (weaponAndAmmoInfoArrayPtr == null)
+			{
+				return new List<uint>();
+			}
+
+			var weaponAndAmmoInfoElementCount = weaponAndAmmoInfoArrayPtr->size;
+			var resultList = new List<uint>();
+
+			for (int i = 0; i < weaponAndAmmoInfoElementCount; i++)
+			{
+				var weaponOrAmmoInfo = (ItemInfo*)weaponAndAmmoInfoArrayPtr->GetElementAddress(i);
+
+				if (!CanPedEquip(weaponOrAmmoInfo) && !disallowWeaponHashSetForHumanPedsOnFoot.Contains(weaponOrAmmoInfo->nameHash))
+					continue;
+
+				var GetClassNameHashFunc = CreateGetClassNameHashDelegateIfNotCreated(weaponOrAmmoInfo->vTable[2]);
+				uint outVal = 0;
+				var returnClassNameHashAddr = GetClassNameHashFunc(0, &outVal);
+
+				const uint CWEAPONINFO_NAME_HASH = 0x861905B4;
+				if (*returnClassNameHashAddr == CWEAPONINFO_NAME_HASH)
+					resultList.Add(weaponOrAmmoInfo->nameHash);
+			}
+
+			return resultList;
+
+			bool CanPedEquip(ItemInfo* weaponInfoAddress)
+			{
+				return weaponInfoAddress->modelHash != 0 && weaponInfoAddress->slot != 0;
+			}
+		}
+
+		public static List<uint> GetAllWeaponComponentHashes()
+		{
+			var cWeaponComponentArrayFirstPtr = (ulong*)((byte*)offsetForCWeaponComponentArrayAddr + 4 + *(int*)offsetForCWeaponComponentArrayAddr);
+			var arrayCount = weaponComponentArrayCountAddr != null ? *(uint*)weaponComponentArrayCountAddr : 0;
+			var resultList = new List<uint>();
+
+			for (uint i = 0; i < arrayCount; i++)
+			{
+				var cWeaponComponentInfo = cWeaponComponentArrayFirstPtr[i];
+				var weaponComponentNameHash = *(uint*)(cWeaponComponentInfo + 0x10);
+				resultList.Add(weaponComponentNameHash);
+			}
+
+			return resultList;
+		}
+
+		public static List<uint> GetAllCompatibleWeaponComponentHashes(uint weaponHash)
+		{
+			var weaponInfo = FindWeaponInfo(weaponHash);
+
+			if (weaponInfo == null)
+				return new List<uint>();
+
+			var returnList = new List<uint>();
+			for (int attachPointOffset = weaponAttachPointsStartOffset; attachPointOffset < weaponAttachPointsEndOffset; attachPointOffset += weaponAttachPointElementSize)
+			{
+				int componentItemsOffset = attachPointOffset + 0x8;
+				var componentItemAddr = (byte*)weaponInfo + componentItemsOffset;
+				int componentItemsCount = *(int*)(componentItemAddr + weaponAttachPointElementComponentCountOffset);
+
+				if (componentItemsCount <= 0)
+					continue;
+
+				for (int i = 0; i < componentItemsCount; i++)
+				{
+					returnList.Add(*(uint*)((byte*)weaponInfo + componentItemsOffset + i * 0x8));
+				}
+			}
+			return returnList;
+		}
+
+		private static GetClassNameHashOfCItemInfoDelegate CreateGetClassNameHashDelegateIfNotCreated(ulong virtualFuncAddr)
+		{
+			if (getClassNameHashOfCItemInfoCacheDict.TryGetValue(virtualFuncAddr, out var outDelegate))
+			{
+				return outDelegate;
+			}
+			else
+			{
+				var newDelegate = GetDelegateForFunctionPointer<GetClassNameHashOfCItemInfoDelegate>(new IntPtr((long)virtualFuncAddr));
+				getClassNameHashOfCItemInfoCacheDict.Add(virtualFuncAddr, newDelegate);
+
+				return newDelegate;
+			}
+		}
+
 		#endregion
 
 		#region -- NaturalMotion Euphoria --

source/scripting_v2/GTA/Weapons/Weapon.cs
@@ -4,6 +4,7 @@
 //
 
 using GTA.Native;
+using System.Linq;
 
 namespace GTA
 {
@@ -569,229 +570,7 @@ namespace GTA
 
 		public static WeaponComponent[] GetComponentsFromHash(WeaponHash hash)
 		{
-			switch (hash)
-			{
-				case WeaponHash.Pistol:
-					return new[] {
-						WeaponComponent.PistolClip01,
-						WeaponComponent.PistolClip02,
-						WeaponComponent.AtPiFlsh,
-						WeaponComponent.AtPiSupp02,
-						WeaponComponent.PistolVarmodLuxe };
-				case WeaponHash.CombatPistol:
-					return new[] {
-						WeaponComponent.CombatPistolClip01,
-						WeaponComponent.CombatPistolClip02,
-						WeaponComponent.AtPiFlsh,
-						WeaponComponent.AtPiSupp,
-						WeaponComponent.CombatPistolVarmodLowrider };
-				case WeaponHash.APPistol:
-					return new[] {
-						WeaponComponent.APPistolClip01,
-						WeaponComponent.APPistolClip02,
-						WeaponComponent.AtPiFlsh,
-						WeaponComponent.AtPiSupp,
-						WeaponComponent.APPistolVarmodLuxe };
-				case WeaponHash.MicroSMG:
-					return new[] {
-						WeaponComponent.MicroSMGClip01,
-						WeaponComponent.MicroSMGClip02,
-						WeaponComponent.AtPiFlsh,
-						WeaponComponent.AtScopeMacro,
-						WeaponComponent.AtArSupp02,
-						WeaponComponent.MicroSMGVarmodLuxe };
-				case WeaponHash.SMG:
-					return new[] {
-						WeaponComponent.SMGClip01,
-						WeaponComponent.SMGClip02,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtPiSupp,
-						WeaponComponent.AtScopeMacro02,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.SMGVarmodLuxe };
-				case WeaponHash.AssaultRifle:
-					return new[] {
-						WeaponComponent.AssaultRifleClip01,
-						WeaponComponent.AssaultRifleClip02,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeMacro,
-						WeaponComponent.AtArSupp02,
-						WeaponComponent.AssaultRifleVarmodLuxe };
-				case WeaponHash.CarbineRifle:
-					return new[] {
-						WeaponComponent.CarbineRifleClip01,
-						WeaponComponent.CarbineRifleClip02,
-						WeaponComponent.AtRailCover01,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeMedium,
-						WeaponComponent.AtArSupp,
-						WeaponComponent.CarbineRifleVarmodLuxe };
-				case WeaponHash.AdvancedRifle:
-					return new[] {
-						WeaponComponent.AdvancedRifleClip01,
-						WeaponComponent.AdvancedRifleClip02,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeSmall,
-						WeaponComponent.AtArSupp,
-						WeaponComponent.AdvancedRifleVarmodLuxe };
-				case WeaponHash.MG:
-					return new[] {
-						WeaponComponent.MGClip01,
-						WeaponComponent.MGClip02,
-						WeaponComponent.AtScopeSmall02,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.MGVarmodLowrider };
-				case WeaponHash.CombatMG:
-					return new[] {
-						WeaponComponent.CombatMGClip01,
-						WeaponComponent.CombatMGClip02,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.AtScopeMedium,
-						WeaponComponent.CombatMGVarmodLowrider };
-				case WeaponHash.PumpShotgun:
-					return new[] {
-						WeaponComponent.AtSrSupp,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.PumpShotgunVarmodLowrider };
-				case WeaponHash.AssaultShotgun:
-					return new[] {
-						WeaponComponent.AssaultShotgunClip01,
-						WeaponComponent.AssaultShotgunClip02,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtArSupp };
-				case WeaponHash.SniperRifle:
-					return new[] {
-						WeaponComponent.SniperRifleClip01,
-						WeaponComponent.AtScopeLarge,
-						WeaponComponent.AtScopeMax,
-						WeaponComponent.AtArSupp02,
-						WeaponComponent.SniperRifleVarmodLuxe };
-				case WeaponHash.HeavySniper:
-					return new[] {
-						WeaponComponent.HeavySniperClip01,
-						WeaponComponent.AtScopeLarge,
-						WeaponComponent.AtScopeMax };
-				case WeaponHash.GrenadeLauncher:
-					return new[] {
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeSmall };
-				case WeaponHash.Minigun:
-					return new[] {
-						WeaponComponent.MinigunClip01 };
-				case WeaponHash.AssaultSMG:
-					return new[] {
-						WeaponComponent.AssaultSMGClip01,
-						WeaponComponent.AssaultSMGClip02,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeMacro,
-						WeaponComponent.AtArSupp02,
-						WeaponComponent.AssaultSMGVarmodLowrider };
-				case WeaponHash.BullpupShotgun:
-					return new[] {
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtArSupp02 };
-				case WeaponHash.Pistol50:
-					return new[] {
-						WeaponComponent.Pistol50Clip01,
-						WeaponComponent.Pistol50Clip02,
-						WeaponComponent.AtPiFlsh,
-						WeaponComponent.AtArSupp02,
-						WeaponComponent.Pistol50VarmodLuxe };
-				case WeaponHash.CombatPDW:
-					return new[] {
-						WeaponComponent.CombatPDWClip01,
-						WeaponComponent.CombatPDWClip02,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeSmall,
-						WeaponComponent.AtArAfGrip };
-				case WeaponHash.SawnOffShotgun:
-					return new[] {
-						WeaponComponent.SawnoffShotgunVarmodLuxe };
-				case WeaponHash.BullpupRifle:
-					return new[] {
-						WeaponComponent.BullpupRifleClip01,
-						WeaponComponent.BullpupRifleClip02,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeSmall,
-						WeaponComponent.AtArSupp,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.BullpupRifleVarmodLow };
-				case WeaponHash.SNSPistol:
-					return new[] {
-						WeaponComponent.SNSPistolClip01,
-						WeaponComponent.SNSPistolClip02,
-						WeaponComponent.SNSPistolVarmodLowrider };
-				case WeaponHash.SpecialCarbine:
-					return new[] {
-						WeaponComponent.SpecialCarbineClip01,
-						WeaponComponent.SpecialCarbineClip02,
-						WeaponComponent.AtArFlsh,
-						WeaponComponent.AtScopeMedium,
-						WeaponComponent.AtArSupp02,
-						WeaponComponent.AtArAfGrip,
-						WeaponComponent.SpecialCarbineVarmodLowrider };
-				case WeaponHash.KnuckleDuster:
-					return new[] {
-						WeaponComponent.KnuckleVarmodPimp,
-						WeaponComponent.KnuckleVarmodBallas,
-						WeaponComponent.KnuckleVarmodDollar,
-						WeaponComponent.KnuckleVarmodDiamond,
-						WeaponComponent.KnuckleVarmodHate,
-						WeaponComponent.KnuckleVarmodLove,
-						WeaponComponent.KnuckleVarmodPlayer,
-						WeaponComponent.KnuckleVarmodKing,
-						WeaponComponent.KnuckleVarmodVagos };
-				case WeaponHash.MachinePistol:
-					return new[] {
-						WeaponComponent.MachinePistolClip01,
-						WeaponComponent.MachinePistolClip02,
-						WeaponComponent.AtPiSupp };
-				case WeaponHash.SwitchBlade:
-					return new[] {
-						WeaponComponent.SwitchbladeVarmodVar1,
-						WeaponComponent.SwitchbladeVarmodVar2 };
-				case WeaponHash.Revolver:
-					return new[] {
-						WeaponComponent.RevolverClip01,
-						WeaponComponent.RevolverVarmodBoss,
-						WeaponComponent.RevolverVarmodGoon };
-			}
-
-			DlcWeaponData data;
-			DlcWeaponComponentData componentData;
-			for (int i = 0, max = Function.Call<int>(Native.Hash.GET_NUM_DLC_WEAPONS); i < max; i++)
-			{
-				unsafe
-				{
-					if (Function.Call<bool>(Native.Hash.GET_DLC_WEAPON_DATA, i, (int*)(void*)&data))
-					{
-						if (data.Hash == hash)
-						{
-							int maxComp = Function.Call<int>(Native.Hash.GET_NUM_DLC_WEAPON_COMPONENTS, i);
-							WeaponComponent[] components = new WeaponComponent[maxComp];
-
-							for (int j = 0; j < maxComp; j++)
-							{
-								if (Function.Call<bool>(Native.Hash.GET_DLC_WEAPON_COMPONENT_DATA, i, j, (int*)(void*)&componentData))
-								{
-									components[j] = (WeaponComponent)componentData.Hash;
-								}
-								else
-								{
-									components[j] = WeaponComponent.Invalid;
-								}
-							}
-						}
-					}
-				}
-			}
-
-			return new WeaponComponent[0];
+			return SHVDN.NativeMemory.GetAllCompatibleWeaponComponentHashes((uint)hash).Select(x => (WeaponComponent)x).ToArray();
 		}
 	}
 }

source/scripting_v3/GTA/Weapons/Weapon.cs
@@ -291,6 +291,11 @@ namespace GTA
 			return "WT_INVALID";
 		}
 
+		public static WeaponHash[] GetAllWeaponHashesForHumanPeds()
+		{
+			return SHVDN.NativeMemory.GetAllWeaponHashesForHumanPeds().Select(x => (WeaponHash)x).ToArray();
+		}
+
 		public static Model[] GetAllModels()
 		{
 			return SHVDN.NativeMemory.WeaponModels.Select(x => new Model(x)).ToArray();

commit 3fda55fd550919810cbf7f15c9faf95fa7e9ea94

Fix WeaponComponent.AttachmentPoint
The problem was introduced by #1013

source/core/NativeMemory.cs
@@ -330,13 +330,13 @@ namespace SHVDN
 				address = FindPattern("\x46\x8D\x04\x11\x48\x8D\x15\x00\x00\x00\x00\x41\xD1\xF8", "xxxxxxx????xxx", new IntPtr(address));
 				offsetForCWeaponComponentArrayAddr = (ulong)(address + 7);
 
-				address = FindPattern("\x74\x10\x49\x8B\xC9\xE8\x00\x00\x00\x00", "xxxxxx????");
+				address = FindPattern("\x74\x10\x49\x8B\xC9\xE8\x00\x00\x00\x00", "xxxxxx????", new IntPtr(address));
 				var findAttachPointFuncAddr = new IntPtr((long)(*(int*)(address + 6) + address + 10));
 
 				address = FindPattern("\x4C\x8D\x81\x00\x00\x00\x00", "xxx????", findAttachPointFuncAddr);
 				weaponAttachPointsStartOffset = *(int*)(address + 3);
-				address = FindPattern("\x4D\x63\x98\x00\x00\x00\x00", "xxx????", new(address));
-				weaponAttachPointsEndOffset = *(int*)(address + 3) + weaponAttachPointsStartOffset;
+				address = FindPattern("\x4D\x63\x98\x00\x00\x00\x00", "xxx????", new IntPtr(address));
+				weaponAttachPointsArrayCountOffset = *(int*)(address + 3);
 				address = FindPattern("\x4C\x63\x50\x00", "xxx?", new(address));
 				weaponAttachPointElementComponentCountOffset = *(byte*)(address + 3);
 				address = FindPattern("\x48\x83\xC0\x00", "xxx?", new(address));
@@ -2628,7 +2628,7 @@ namespace SHVDN
 		// Store the offset instead of the calculated address for compatibility with mods like Weapon Limits Adjuster by alexguirre (although Weapon Limits Adjuster allocates a new array in the very beginning).
 		static ulong offsetForCWeaponComponentArrayAddr;
 		static int weaponAttachPointsStartOffset;
-		static int weaponAttachPointsEndOffset;
+		static int weaponAttachPointsArrayCountOffset;
 		static int weaponAttachPointElementComponentCountOffset;
 		static int weaponAttachPointElementSize;
 
@@ -2704,20 +2704,23 @@ namespace SHVDN
 			if (weaponInfo == null)
 				return 0xFFFFFFFF;
 
-			for (int attachPointOffset = weaponAttachPointsStartOffset; attachPointOffset < weaponAttachPointsEndOffset; attachPointOffset += weaponAttachPointElementSize)
+			var weaponAttachPointsAddr = (byte*)weaponInfo + weaponAttachPointsStartOffset;
+			var weaponAttachPointsCount = *(int*)(weaponAttachPointsAddr + weaponAttachPointsArrayCountOffset);
+			var weaponAttachPointElementStartAddr = (byte*)(weaponAttachPointsAddr);
+
+			for (int i = 0; i < weaponAttachPointsCount; i++)
 			{
-				int componentItemsOffset = attachPointOffset + 0x8;
-				var componentItemAddr = (byte*)weaponInfo + componentItemsOffset;
-				int componentItemsCount = *(int*)(componentItemAddr + weaponAttachPointElementComponentCountOffset);
+				var weaponAttachPointElementAddr = weaponAttachPointElementStartAddr + (i * weaponAttachPointElementSize) + 0x8;
+				int componentItemsCount = *(int*)(weaponAttachPointElementAddr + weaponAttachPointElementComponentCountOffset);
 
 				if (componentItemsCount <= 0)
 					continue;
 
-				for (int i = 0; i < componentItemsCount; i++)
+				for (int j = 0; j < componentItemsCount; j++)
 				{
-					var componentHashInItemArray = *(uint*)((byte*)weaponInfo + componentItemsOffset + i * 0x8);
+					var componentHashInItemArray = *(uint*)(weaponAttachPointElementAddr + j * 0x8);
 					if (componentHashInItemArray == componentHash)
-						return *(uint*)((byte*)weaponInfo + componentItemsOffset);
+						return *(uint*)(weaponAttachPointElementStartAddr + i * weaponAttachPointElementSize);
 				}
 			}
 
@@ -2782,20 +2785,24 @@ namespace SHVDN
 				return new List<uint>();
 
 			var returnList = new List<uint>();
-			for (int attachPointOffset = weaponAttachPointsStartOffset; attachPointOffset < weaponAttachPointsEndOffset; attachPointOffset += weaponAttachPointElementSize)
+
+			var weaponAttachPointsAddr = (byte*)weaponInfo + weaponAttachPointsStartOffset;
+			var weaponAttachPointsCount = *(int*)(weaponAttachPointsAddr + weaponAttachPointsArrayCountOffset);
+			var weaponAttachPointElementStartAddr = (byte*)(weaponAttachPointsAddr + 0x8);
+			for (int i = 0; i < weaponAttachPointsCount; i++)
 			{
-				int componentItemsOffset = attachPointOffset + 0x8;
-				var componentItemAddr = (byte*)weaponInfo + componentItemsOffset;
-				int componentItemsCount = *(int*)(componentItemAddr + weaponAttachPointElementComponentCountOffset);
+				var weaponAttachPointElementAddr = weaponAttachPointElementStartAddr + i * weaponAttachPointElementSize;
+				int componentItemsCount = *(int*)(weaponAttachPointElementAddr + weaponAttachPointElementComponentCountOffset);
 
 				if (componentItemsCount <= 0)
 					continue;
 
-				for (int i = 0; i < componentItemsCount; i++)
+				for (int j = 0; j < componentItemsCount; j++)
 				{
-					returnList.Add(*(uint*)((byte*)weaponInfo + componentItemsOffset + i * 0x8));
+					returnList.Add(*(uint*)(weaponAttachPointElementAddr + j * 0x8));
 				}
 			}
+
 			return returnList;
 		}
 