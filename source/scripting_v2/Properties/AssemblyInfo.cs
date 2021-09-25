0322c0b7bd85c233e284616250be3deb3d1a0303
kagikn <kagikn.3939@gmail.com>
2021-09-26 00:21:17 +0900

    Update version
    Update minor version of v2 API because Native.Function.Call with
    normal parameter overloads were added

--- source/scripting_v2/Properties/AssemblyInfo.cs
+++ source/scripting_v2/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.10.14.0")]
-[assembly: AssemblyFileVersion("2.10.14.0")]
+[assembly: AssemblyVersion("2.11.0.0")]
+[assembly: AssemblyFileVersion("2.11.0.0")]
--- source/scripting_v3/Properties/AssemblyInfo.cs
+++ source/scripting_v3/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
