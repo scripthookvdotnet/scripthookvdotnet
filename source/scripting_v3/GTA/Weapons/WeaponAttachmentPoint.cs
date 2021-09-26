7f991f0005070f3809925a94cf8557a8fcf0d91e
kagikn <kagikn.3939@gmail.com>
2021-09-27 06:56:01 +0900

    Add missing weapon attachment point hashes

--- source/scripting_v3/GTA/Weapons/WeaponAttachmentPoint.cs
+++ source/scripting_v3/GTA/Weapons/WeaponAttachmentPoint.cs
@@ -8,13 +8,18 @@ namespace GTA
 	public enum WeaponAttachmentPoint : uint
 	{
 		Invalid = 4294967295u,
+		Barrel = 2982890265u,
 		Clip = 3723347892u,
 		Clip2 = 291640902u,
+		/// <summary> Used for <see cref="WeaponHash.Flashlight"/>. </summary>
+		Flash = 953267555u,
 		FlashLaser = 679107254u,
 		FlashLaser2 = 2722126698u,
 		Supp = 1863181664u,
 		Supp2 = 945598191u,
 		GunRoot = 962500902u,
+		/// <summary> Used for <see cref="WeaponComponentHash.GunrunMk2Upgrade"/>. </summary>
+		GunGripR = 4263393586u,
 		Scope = 196630833u,
 		Scope2 = 1684637069u,
 		Grip = 2972950469u,
