ff1fd5bb4a1d31e481f4b7ff33e61bdef4bb3da6
kagikn <kagikn.3939@gmail.com>
2022-01-17 21:08:34 +0900

    Update version

--- source/scripting_v3/Properties/AssemblyInfo.cs
+++ source/scripting_v3/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.3.2.0")]
-[assembly: AssemblyFileVersion("3.3.2.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
