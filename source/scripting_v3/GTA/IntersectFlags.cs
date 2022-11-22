527bca46ceffdd9b506e1156e2a079ef19468619
kagikn <kagikn.3939@gmail.com>
2022-11-22 13:21:36 +0900

    Add missing enums and mark incorrect one obsolete

--- source/scripting_v3/GTA/IntersectFlags.cs
+++ source/scripting_v3/GTA/IntersectFlags.cs
@@ -10,11 +10,30 @@ namespace GTA
 	[Flags]
 	public enum IntersectFlags
 	{
-		Everything = -1,
 		Map = 1,
-		MissionEntities = 2,
+		Vehicles = 2,
+		/// <summary>
+		/// Detect <see cref="Ped"/> who are not ragdolled (not running any NM tasks) by detecting the simple capsule shape of <see cref="Ped"/>.
+		/// </summary>
+		PedCapsules = 4,
+		/// <summary>
+		/// Detect <see cref="Ped"/>'s ragdoll. Can detect those who are not ragdolled.
+		/// </summary>
+		Ragdolls = 8,
 		Peds = 12,
 		Objects = 16,
+		Pickups = 32,
+		Glass = 64,
+		Rivers = 128,
+		/// <summary>
+		/// Detect foliage, which can be affected by the wind or contacts of <see cref="Entity"/>.
+		/// </summary>
+		Foliage = 256,
+		Everything = 511,
+
+		[Obsolete("IntersectFlags.MissionEntities is obsolete because it is incorrect, use IntersectFlags.Vehicles instead.")]
+		MissionEntities = 2,
+		[Obsolete("IntersectFlags.Vegetation is obsolete because it is incorrect, use IntersectFlags.Foliage instead.")]
 		Vegetation = 256,
 	}
 }
