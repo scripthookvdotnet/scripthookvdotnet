c915d008b636eccce8e66f8d430deb0f3d9de496
kagikn <kagikn.3939@gmail.com>
2022-12-29 19:30:36 +0900

    Add PedPropAnchorPosition.cs

---
+++ source/scripting_v3/GTA/Entities/Peds/PedPropAnchorPosition.cs
@@ -0,0 +1,20 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA
+{
+	public enum PedPropAnchorPosition
+	{
+		Head,
+		Eyes,
+		Ears,
+		Mouth,
+		LeftHand,
+		RightHand,
+		LeftWrist,
+		RightWrist,
+		Hip,
+	}
+}
