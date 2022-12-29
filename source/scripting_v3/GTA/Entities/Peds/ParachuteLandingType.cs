0addb5ca39fac40b612f375efc3a2c9d2a1fa497
kagikn <kagikn.3939@gmail.com>
2022-12-29 16:19:06 +0900

    Update ParachuteLandingType enum
    New enum names are based on leaked source

--- source/scripting_v3/GTA/Entities/Peds/ParachuteLandingType.cs
+++ source/scripting_v3/GTA/Entities/Peds/ParachuteLandingType.cs
@@ -3,13 +3,38 @@
 // License: https://github.com/crosire/scripthookvdotnet#license
 //
 
+using System;
+
 namespace GTA
 {
 	public enum ParachuteLandingType
 	{
+		/// <summary>
+		/// Ped is not in a valid parachute landing state.
+		/// </summary>
+		Invalid,
+		Slow,
+		/// <summary>
+		/// Ped is landing at regular speed (they are stumbling).
+		/// </summary>
+		Regular,
+		/// <summary>
+		/// Ped is landing at fast speed (they are rolling).
+		/// </summary>
+		Fast,
+		/// <summary>
+		/// Ped is crashing (ragdolling).
+		/// </summary>
+		Crashing,
+		Water,
+
+		[Obsolete("ParachuteLandingType.None is obsolete, use ParachuteLandingType.Invalid instead.")]
 		None = -1,
+		[Obsolete("ParachuteLandingType.Stumbling is obsolete, use ParachuteLandingType.Regular instead.")]
 		Stumbling = 1,
+		[Obsolete("ParachuteLandingType.Rolling is obsolete, use ParachuteLandingType.Fast instead.")]
 		Rolling,
+		[Obsolete("ParachuteLandingType.Ragdoll is obsolete, use ParachuteLandingType.Crashing instead.")]
 		Ragdoll,
 	}
 }
