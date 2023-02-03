30f65606484651e4394b69a3bc1e9800c7bc400d
kagikn <kagikn.3939@gmail.com>
2023-02-04 02:05:20 +0900

    Add new ApplyForce methods

---
+++ source/scripting_v3/GTA/Entities/Peds/RagdollComponent.cs
@@ -0,0 +1,35 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA
+{
+	/// <summary>
+	/// The ragdoll component enum, which is used for apply force natives and some config files such as <c>physicstasks.ymt</c>.
+	/// </summary>
+	public enum RagdollComponent
+	{
+		Buttocks = 0,
+		ThighLeft = 1,
+		ShinLeft = 2,
+		FootLeft = 3,
+		ThighRight = 4,
+		ShinRight = 5,
+		FootRight = 6,
+		Spine0 = 7,
+		Spine1 = 8,
+		Spine2 = 9,
+		Spine3 = 10,
+		ClavicleLeft = 11,
+		UpperArmLeft = 12,
+		LowerArmLeft = 13,
+		HandLeft = 14,
+		ClavicleRight = 15,
+		UpperArmRight = 16,
+		LowerArmRight = 17,
+		HandRight = 18,
+		Neck = 19,
+		Head = 20,
+	}
+}
