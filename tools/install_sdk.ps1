712bad29175b0bd50e2d9ae7c12086c0ec6801e8
crosire <crosiredev@gmail.com>
2023-02-08 21:42:47 +0100

    Change ScriptHookV SDK download link to the official source
    See #1151

--- tools/install_sdk.ps1
+++ tools/install_sdk.ps1
@@ -1,8 +1,7 @@
 # Set the working directory to SDK
 Set-Location "sdk/"
 # Download the file with Invoke-WebRequest
-$source = "https://drive.google.com/uc?export=download&id=1NPh-CQYDUm10vk53LagICROEpDY4aTo8"
-Invoke-WebRequest $source -OutFile "sdk.zip"
+Invoke-WebRequest -Uri "http://www.dev-c.com/files/ScriptHookV_SDK_1.0.617.1a.zip" -OutFile "sdk.zip" -Headers @{ "Referer" = "http://www.dev-c.com/gtav/scripthookv/" }
 # Extract the contents of the file in the current directory
 7z x sdk.zip
 # Return to the location where we started
