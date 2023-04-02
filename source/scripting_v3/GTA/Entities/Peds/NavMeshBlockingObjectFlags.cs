51c1e28ec98ce6daa819c0f9861de9a8ca22321c
Ghillie <kagikn.3939@gmail.com>
2023-04-03 01:13:44 +0900

    Add NavMeshBlockingObject (#1191)
    * Add NavMeshBlockingObject

---
+++ source/scripting_v3/GTA/Entities/Peds/NavMeshBlockingObjectFlags.cs
@@ -0,0 +1,28 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+using System;
+
+namespace GTA
+{
+    [Flags]
+    public enum NavMeshBlockingObjectFlags
+    {
+        Default = 0,
+        /// <summary>
+        /// Blocking object will block wander paths.
+        /// </summary>
+        WanderPath = 1,
+        /// <summary>
+        /// Blocking object will block (regular) shortest-paths.
+        /// </summary>
+        ShortestPath = 2,
+        /// <summary>
+        /// Blocking object will block flee paths.
+        /// </summary>
+        FleePath = 4,
+        AllPaths = WanderPath | ShortestPath | FleePath,
+    }
+}
