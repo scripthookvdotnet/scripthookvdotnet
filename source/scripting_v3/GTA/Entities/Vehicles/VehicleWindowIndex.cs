4bcef88143fa0f9865c10555be3407caa04d655d
nomakewan <nomakewan@users.noreply.github.com>
2023-04-18 11:20:23 -0700

    Add obsolete flags for VehicleWindowIndex
    Updated change broke compatibility with BTTFV and will therefore likely break compatibility with several available scripts. As such, it is pertinent to include the obsoleted values with the appropriate tags.

--- source/scripting_v3/GTA/Entities/Vehicles/VehicleWindowIndex.cs
+++ source/scripting_v3/GTA/Entities/Vehicles/VehicleWindowIndex.cs
@@ -15,5 +15,13 @@ namespace GTA
 		MiddleRightWindow,
 		Windshield,
 		BackWindshield,
+		[Obsolete("ExtraWindow1 is obsolete, please use MiddleLeftWindow instead.")]
+		ExtraWindow1 = 4,
+		[Obsolete("ExtraWindow2 is obsolete, please use MiddleRightWindow instead.")]
+		ExtraWindow2 = 5,
+		[Obsolete("ExtraWindow3 is obsolete, please use Windshield instead.")]
+		ExtraWindow3 = 6,
+		[Obsolete("ExtraWindow4 is obsolete, please use BackWindshield instead.")]
+		ExtraWindow4 = 7,
 	}
 }
