c2fb28bba57d227e593d4107e2146a7ae61c3d00
kagikn <kagikn.3939@gmail.com>
2023-05-23 02:13:49 +0900

    Add "kagikn" to copyright and authors
    I have been officially assigned to maintenance, so I thought I should
    clarify this more than before

--- AssemblyInfo.cpp
+++ AssemblyInfo.cpp
@@ -4,9 +4,9 @@ using namespace System::Reflection;
 // The file encoding has to be UTF-8 with BOM and literal strings have to be wstring so the ANSI encoding won't have influence on the literal strings
 [assembly:AssemblyTitle(L"Community Script Hook V .NET")];
 [assembly:AssemblyDescription(L"An ASI plugin for Grand Theft Auto V, which allows running scripts written in any .NET language in-game.")];
-[assembly:AssemblyCompany(L"crosire & contributors")];
+[assembly:AssemblyCompany(L"crosire & kagikn & contributors")];
 [assembly:AssemblyProduct(L"ScriptHookVDotNet")];
-[assembly:AssemblyCopyright(L"Copyright © 2015 crosire")];
+[assembly:AssemblyCopyright(L"Copyright © 2015 crosire & kagikn")];
 [assembly:AssemblyVersion(SHVDN_VERSION)];
 [assembly:AssemblyFileVersion(SHVDN_VERSION)];
 // Sign with a strong name to distinguish from older versions and cause .NET framework runtime to bind the correct assemblies
