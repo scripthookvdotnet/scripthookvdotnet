9372a484e4e55925dba28dd190d1549c9aa06960
kagikn <kagikn.3939@gmail.com>
2023-06-03 19:43:33 +0900

    Introduce GitHub Actions (#1225)
    * Introduce GitHub Actions
    
    Remove files for AppVeyor CI
    
    Note: Does not include publishing job on NuGet yet

--- tools/upload_artifact.ps1
+++
@@ -1,6 +0,0 @@
-# Find all of the NuGet Packages
-Get-ChildItem ".\*.nupkg" |
-# And upload every single one of them as AppVeyor Artifacts
-ForEach-Object {
-	Push-AppveyorArtifact $_.FullName -FileName $_.Name
-}
