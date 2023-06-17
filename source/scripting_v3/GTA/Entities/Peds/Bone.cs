66ae69fd979d5f0b03691491537388a8b09048b9
kagikn <kagikn.3939@gmail.com>
2023-06-17 17:30:20 +0900

    Revert "Fix Bone.Invalid specifying incorrect value"
    This reverts commit b2b81084d053dbdf6cfdca42f5386ec1e805d660.
    
    RAGE parser supports 32 bit, 16 bit, and 8 bit range enums in GTA V
    and eAnimBoneTag uses 32 bit range so -1 (0xFFFFFFFF) is the correct
    value for BONETAG_INVALID

--- source/scripting_v3/GTA/Entities/Peds/Bone.cs
+++ source/scripting_v3/GTA/Entities/Peds/Bone.cs
@@ -5,18 +5,12 @@
 
 namespace GTA
 {
-	/// <summary>
-	/// An enumeration of possible bone tag values for <see cref="Ped"/>.
-	/// </summary>
 	public enum Bone
 	{
 		/// <summary>
 		/// The correspoinding bone tag value for <c>BONETAG_INVALID</c>, which is used in some meta/xml files such as weapon meta files and <c>damages.meta</c>.
 		/// </summary>
-		/// <remarks>
-		/// This value specifies 0xFFFF instead of -1 which will be 0xFFFFFFFF in uint32_t, because a bone tag value takes 2 bytes, not 4 bytes.
-		/// </remarks>
-		Invalid = 0xFFFF,
+		Invalid = -1,
 		SkelRoot = 0x0,
 		SkelPelvis = 0x2E28,
 		SkelLeftThigh = 0xE39F,
