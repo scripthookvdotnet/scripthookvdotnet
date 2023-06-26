scripthookvdotnet/scripthookvdotnet

commit 9b4cc2d41d28a4df53911b040dbb1d7ca0760601

Add UI.IElement.WorldDraw and UI.IElement.WorldScaledDraw (#1131)

source/scripting_v3/GTA.UI/ContainerElement.cs
@@ -3,6 +3,7 @@
 // License: https://github.com/crosire/scripthookvdotnet#license
 //
 
+using GTA.Math;
 using GTA.Native;
 using System.Collections.Generic;
 using System.Drawing;
@@ -187,6 +188,45 @@ namespace GTA.UI
 			{
 				item.ScaledDraw(offset);
 			}
+		}
+
+		/// <summary>
+		/// Draws this <see cref="ContainerElement"/> this frame in the specified <see cref="Vector3"/> position.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="ContainerElement"/> to be drawn</param>
+		public virtual void WorldDraw(Vector3 position)
+		{
+			WorldDraw(position, SizeF.Empty);
+		}
+		/// <summary>
+		/// Draws this <see cref="ContainerElement"/> this frame at the specified <see cref="Vector3"/> position and offset.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="ContainerElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="ContainerElement"/> using a 1280*720 pixel base.</param>
+		public virtual void WorldDraw(Vector3 position, SizeF offset)
+		{
+			Function.Call(Hash.SET_DRAW_ORIGIN, position.X, position.Y, position.Z, 0);
+			InternalDraw(offset, Screen.Width, Screen.Height);
+			Function.Call(Hash.CLEAR_DRAW_ORIGIN);
+		}
+		/// <summary>
+		/// Draws this <see cref="ContainerElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="ContainerElement"/> to be drawn</param>
+		public virtual void WorldScaledDraw(Vector3 position)
+		{
+			WorldScaledDraw(position, SizeF.Empty);
+		}
+		/// <summary>
+		/// Draws this <see cref="ContainerElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="ContainerElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="ContainerElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
+		public virtual void WorldScaledDraw(Vector3 position, SizeF offset)
+		{
+			Function.Call(Hash.SET_DRAW_ORIGIN, position.X, position.Y, position.Z, 0);
+			InternalDraw(offset, Screen.ScaledWidth, Screen.Height);
+			Function.Call(Hash.CLEAR_DRAW_ORIGIN);
 		}
 
 		void InternalDraw(SizeF offset, float screenWidth, float screenHeight)

source/scripting_v3/GTA.UI/CustomSprite.cs
@@ -3,6 +3,7 @@
 // License: https://github.com/crosire/scripthookvdotnet#license
 //
 
+using GTA.Math;
 using GTA.Native;
 using System.Collections.Generic;
 using System.Drawing;
@@ -200,6 +201,52 @@ namespace GTA.UI
 		public virtual void ScaledDraw(SizeF offset)
 		{
 			InternalDraw(offset, Screen.ScaledWidth, Screen.Height);
+		}
+
+		/// <summary>
+		/// Draws this <see cref="CustomSprite"/> this frame in the specified <see cref="Vector3"/> position.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="CustomSprite"/> to be drawn</param>
+		public virtual void WorldDraw(Vector3 position)
+		{
+			WorldDraw(position, SizeF.Empty);
+		}
+
+		/// <summary>
+		/// Draws this <see cref="CustomSprite"/> this frame at the specified <see cref="Vector3"/> position and offset.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="CustomSprite"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="CustomSprite"/> using a 1280*720 pixel base.</param>
+		public virtual void WorldDraw(Vector3 position, SizeF offset)
+		{
+			PointF pointF = Screen.WorldToScreen(position);
+			if (!(pointF == PointF.Empty))
+			{
+				InternalDraw(new SizeF(pointF) + offset, Screen.Width, Screen.Height);
+			}
+		}
+
+		/// <summary>
+		/// Draws this <see cref="CustomSprite"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="CustomSprite"/> to be drawn</param>
+		public virtual void WorldScaledDraw(Vector3 position)
+		{
+			WorldScaledDraw(position, SizeF.Empty);
+		}
+
+		/// <summary>
+		/// Draws this <see cref="CustomSprite"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="CustomSprite"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="CustomSprite"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
+		public virtual void WorldScaledDraw(Vector3 position, SizeF offset)
+		{
+			PointF pointF = Screen.WorldToScreen(position, scaleWidth: true);
+			if (!(pointF == PointF.Empty))
+			{
+				InternalDraw(new SizeF(pointF) + offset, Screen.ScaledWidth, Screen.Height);
+			}
 		}
 
 		void InternalDraw(SizeF offset, float screenWidth, float screenHeight)

source/scripting_v3/GTA.UI/IElement.cs
@@ -3,6 +3,7 @@
 // License: https://github.com/crosire/scripthookvdotnet#license
 //
 
+using GTA.Math;
 using System.Drawing;
 
 namespace GTA.UI
@@ -75,5 +76,29 @@ namespace GTA.UI
 		/// </summary>
 		/// <param name="offset">The offset to shift the draw position of this <see cref="IElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
 		void ScaledDraw(SizeF offset);
+
+		/// <summary>
+		/// Draws this <see cref="IElement"/> this frame in the specified <see cref="Vector3"/> position.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
+		void WorldDraw(Vector3 position);
+		/// <summary>
+		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="IElement"/> using a 1280*720 pixel base.</param>
+		void WorldDraw(Vector3 position, SizeF offset);
+
+		/// <summary>
+		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
+		void WorldScaledDraw(Vector3 position);
+		/// <summary>
+		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="IElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
+		void WorldScaledDraw(Vector3 position, SizeF offset);
 	}
 }

source/scripting_v3/GTA.UI/Sprite.cs
@@ -3,6 +3,7 @@
 // License: https://github.com/crosire/scripthookvdotnet#license
 //
 
+using GTA.Math;
 using GTA.Native;
 using System;
 using System.Collections.Generic;
@@ -231,6 +232,45 @@ namespace GTA.UI
 		public virtual void ScaledDraw(SizeF offset)
 		{
 			InternalDraw(offset, Screen.ScaledWidth, Screen.Height);
+		}
+
+		/// <summary>
+		/// Draws this <see cref="Sprite"/> this frame in the specified <see cref="Vector3"/> position.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="Sprite"/> to be drawn</param>
+		public virtual void WorldDraw(Vector3 position)
+		{
+			WorldDraw(position, SizeF.Empty);
+		}
+		/// <summary>
+		/// Draws this <see cref="Sprite"/> this frame at the specified <see cref="Vector3"/> position and offset.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="Sprite"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="Sprite"/> using a 1280*720 pixel base.</param>
+		public virtual void WorldDraw(Vector3 position, SizeF offset)
+		{
+			Function.Call(Hash.SET_DRAW_ORIGIN, position.X, position.Y, position.Z, 0);
+			InternalDraw(offset, Screen.Width, Screen.Height);
+			Function.Call(Hash.CLEAR_DRAW_ORIGIN);
+		}
+		/// <summary>
+		/// Draws this <see cref="Sprite"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="Sprite"/> to be drawn</param>
+		public virtual void WorldScaledDraw(Vector3 position)
+		{
+			WorldScaledDraw(position, SizeF.Empty);
+		}
+		/// <summary>
+		/// Draws this <see cref="Sprite"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="Sprite"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="Sprite"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
+		public virtual void WorldScaledDraw(Vector3 position, SizeF offset)
+		{
+			Function.Call(Hash.SET_DRAW_ORIGIN, position.X, position.Y, position.Z, 0);
+			InternalDraw(offset, Screen.ScaledWidth, Screen.Height);
+			Function.Call(Hash.CLEAR_DRAW_ORIGIN);
 		}
 
 		void InternalDraw(SizeF offset, float screenWidth, float screenHeight)

source/scripting_v3/GTA.UI/TextElement.cs
@@ -3,6 +3,7 @@
 // License: https://github.com/crosire/scripthookvdotnet#license
 //
 
+using GTA.Math;
 using GTA.Native;
 using System;
 using System.Collections.Generic;
@@ -188,7 +189,8 @@ namespace GTA.UI
 				}
 				_pinnedText.Clear();
 
-				SHVDN.NativeFunc.PushLongString(value, (string str) => {
+				SHVDN.NativeFunc.PushLongString(value, (string str) =>
+				{
 					byte[] data = Encoding.UTF8.GetBytes(str + "\0");
 					IntPtr next = Marshal.AllocCoTaskMem(data.Length);
 					Marshal.Copy(data, 0, next, data.Length);
@@ -367,6 +369,45 @@ namespace GTA.UI
 		public virtual void ScaledDraw(SizeF offset)
 		{
 			InternalDraw(offset, Screen.ScaledWidth, Screen.Height);
+		}
+
+		/// <summary>
+		/// Draws this <see cref="TextElement"/> this frame in the specified <see cref="Vector3"/> position.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="TextElement"/> to be drawn</param>
+		public virtual void WorldDraw(Vector3 position)
+		{
+			WorldDraw(position, SizeF.Empty);
+		}
+		/// <summary>
+		/// Draws this <see cref="TextElement"/> this frame at the specified <see cref="Vector3"/> position and offset.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="TextElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="TextElement"/> using a 1280*720 pixel base.</param>
+		public virtual void WorldDraw(Vector3 position, SizeF offset)
+		{
+			Function.Call(Hash.SET_DRAW_ORIGIN, position.X, position.Y, position.Z, 0);
+			InternalDraw(offset, Screen.Width, Screen.Height);
+			Function.Call(Hash.CLEAR_DRAW_ORIGIN);
+		}
+		/// <summary>
+		/// Draws this <see cref="TextElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="TextElement"/> to be drawn</param>
+		public virtual void WorldScaledDraw(Vector3 position)
+		{
+			WorldScaledDraw(position, SizeF.Empty);
+		}
+		/// <summary>
+		/// Draws this <see cref="TextElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="TextElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="TextElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
+		public virtual void WorldScaledDraw(Vector3 position, SizeF offset)
+		{
+			Function.Call(Hash.SET_DRAW_ORIGIN, position.X, position.Y, position.Z, 0);
+			InternalDraw(offset, Screen.ScaledWidth, Screen.Height);
+			Function.Call(Hash.CLEAR_DRAW_ORIGIN);
 		}
 
 		void InternalDraw(SizeF offset, float screenWidth, float screenHeight)

commit 3adb7f4ed00d9b5915c692ebc7eaffc7d2478cea

Avoid breaking changes on IElement by new interface
This has been done to follow "DISALLOWED: Adding a member to an interface"
https://learn.microsoft.com/en-us/dotnet/core/compatibility/library-change-rules
The breaking changes was being introduced by #1131 but not shipped in
any stable releases

The .NET Framework runtime does not support interfaces with default
implementations for your information, which is different from CoreCLR

source/scripting_v2/GTA/Entities/Peds/Bone.cs
@@ -8,7 +8,6 @@ namespace GTA
 	/// <summary>
 	/// An enumeration of possible bone tag values for <see cref="Ped"/>, which is for <c>eAnimBoneTag</c>.
 	/// </summary>
-	/// <remarks>
 	public enum Bone
 	{
 		SKEL_ROOT = 0x0,

source/scripting_v3/GTA.UI/ContainerElement.cs
@@ -10,7 +10,7 @@ using System.Drawing;
 
 namespace GTA.UI
 {
-	public class ContainerElement : IElement
+	public class ContainerElement : IWorldDrawableElement
 	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ContainerElement"/> class used for grouping items on screen.

source/scripting_v3/GTA.UI/CustomSprite.cs
@@ -14,7 +14,7 @@ namespace GTA.UI
 	/// <summary>
 	/// A sprite element using a custom image texture.
 	/// </summary>
-	public class CustomSprite : ISpriteElement
+	public class CustomSprite : ISpriteElement, IWorldDrawableElement
 	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="CustomSprite"/> class used for drawing external textures on the screen.

source/scripting_v3/GTA.UI/IElement.cs
@@ -3,7 +3,6 @@
 // License: https://github.com/scripthookvdotnet/scripthookvdotnet#license
 //
 
-using GTA.Math;
 using System.Drawing;
 
 namespace GTA.UI
@@ -76,29 +75,5 @@ namespace GTA.UI
 		/// </summary>
 		/// <param name="offset">The offset to shift the draw position of this <see cref="IElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
 		void ScaledDraw(SizeF offset);
-
-		/// <summary>
-		/// Draws this <see cref="IElement"/> this frame in the specified <see cref="Vector3"/> position.
-		/// </summary>
-		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
-		void WorldDraw(Vector3 position);
-		/// <summary>
-		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset.
-		/// </summary>
-		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
-		/// <param name="offset">The offset to shift the draw position of this <see cref="IElement"/> using a 1280*720 pixel base.</param>
-		void WorldDraw(Vector3 position, SizeF offset);
-
-		/// <summary>
-		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
-		/// </summary>
-		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
-		void WorldScaledDraw(Vector3 position);
-		/// <summary>
-		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
-		/// </summary>
-		/// <param name="position">Position in the world where you want the <see cref="IElement"/> to be drawn</param>
-		/// <param name="offset">The offset to shift the draw position of this <see cref="IElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
-		void WorldScaledDraw(Vector3 position, SizeF offset);
 	}
 }

source/scripting_v3/GTA.UI/IWorldDrawableElement.cs
@@ -1,0 +1,37 @@
+//
+// Copyright (C) 2015 kagikn & contributors
+// License: https://github.com/scripthookvdotnet/scripthookvdotnet#license
+//
+
+using GTA.Math;
+using System.Drawing;
+
+namespace GTA.UI
+{
+	public interface IWorldDrawableElement : IElement
+	{
+		/// <summary>
+		/// Draws this <see cref="IWorldDrawableElement"/> this frame in the specified <see cref="Vector3"/> position.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IWorldDrawableElement"/> to be drawn</param>
+		void WorldDraw(Vector3 position);
+		/// <summary>
+		/// Draws this <see cref="IWorldDrawableElement"/> this frame at the specified <see cref="Vector3"/> position and offset.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IWorldDrawableElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="IWorldDrawableElement"/> using a 1280*720 pixel base.</param>
+		void WorldDraw(Vector3 position, SizeF offset);
+
+		/// <summary>
+		/// Draws this <see cref="IWorldDrawableElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IWorldDrawableElement"/> to be drawn</param>
+		void WorldScaledDraw(Vector3 position);
+		/// <summary>
+		/// Draws this <see cref="IElement"/> this frame at the specified <see cref="Vector3"/> position and offset using the width returned in <see cref="Screen.ScaledWidth"/>.
+		/// </summary>
+		/// <param name="position">Position in the world where you want the <see cref="IWorldDrawableElement"/> to be drawn</param>
+		/// <param name="offset">The offset to shift the draw position of this <see cref="IWorldDrawableElement"/> using a <see cref="Screen.ScaledWidth"/>*720 pixel base.</param>
+		void WorldScaledDraw(Vector3 position, SizeF offset);
+	}
+}

source/scripting_v3/GTA.UI/Sprite.cs
@@ -16,7 +16,7 @@ namespace GTA.UI
 	/// <summary>
 	/// A sprite element using a built-in texture.
 	/// </summary>
-	public class Sprite : ISpriteElement, IDisposable
+	public class Sprite : ISpriteElement, IWorldDrawableElement, IDisposable
 	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Sprite"/> class used for drawing in game textures on the screen.

source/scripting_v3/GTA.UI/TextElement.cs
@@ -13,7 +13,7 @@ using System.Text;
 
 namespace GTA.UI
 {
-	public class TextElement : IElement
+	public class TextElement : IWorldDrawableElement
 	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="TextElement"/> class used for drawing text on the screen.