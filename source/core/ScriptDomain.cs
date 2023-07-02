2ead3e928fc98b32c3f55870ff77f522b4c21205
kagikn <kagikn.3939@gmail.com>
2023-07-02 13:20:04 +0900

    Don't warn of deprecated scripts if there's actually none
    Fixes #1255

--- source/core/ScriptDomain.cs
+++ source/core/ScriptDomain.cs
@@ -559,7 +559,10 @@ namespace SHVDN
 				LoadScriptsFromAssembly(filename);
 			}
 
-			WarnOfScriptsUsingDeprecatedApi();
+			if (DeprecatedScriptAssemblyNamesPerApiVersion.Count > 0)
+			{
+				WarnOfScriptsUsingDeprecatedApi();
+			}
 
 			// Instantiate scripts after they were all loaded, so that dependencies are launched with the right ordering
 			foreach (Type type in _scriptTypes.Values.Select(x => x.Item2))
@@ -575,29 +578,29 @@ namespace SHVDN
 
 			void WarnOfScriptsUsingDeprecatedApi()
 			{
-				unsafe
+				if (ShouldWarnOfScriptsBuiltAgainstDeprecatedApiWithTicker)
 				{
-					if (ShouldWarnOfScriptsBuiltAgainstDeprecatedApiWithTicker)
-					{
-						int scriptCountUsingDeprecatedApi = DeprecatedScriptAssemblyNamesPerApiVersion.Values.Aggregate(0, (result, current) => result + current.Count);
+					int scriptCountUsingDeprecatedApi = DeprecatedScriptAssemblyNamesPerApiVersion.Values.Aggregate(0, (result, current) => result + current.Count);
 
+					unsafe
+					{
 						NativeFunc.InvokeInternal(0x202709F4C58A0424 /* BEGIN_TEXT_COMMAND_THEFEED_POST */, NativeMemory.CellEmailBcon);
 						NativeFunc.PushLongString($"~o~WARNING~s~: {scriptCountUsingDeprecatedApi} scripts are using the v2 API, which is not actively supported. Check the console or the log file for more details.");
 						NativeFunc.InvokeInternal(0x2ED7843F8F801023 /* END_TEXT_COMMAND_THEFEED_POST_TICKER */, true, false);
 					}
+				}
 
-					foreach (KeyValuePair<int, List<string>> apiVersionAndScriptNameDict in DeprecatedScriptAssemblyNamesPerApiVersion)
+				foreach (KeyValuePair<int, List<string>> apiVersionAndScriptNameDict in DeprecatedScriptAssemblyNamesPerApiVersion)
+				{
+					int apiVersion = apiVersionAndScriptNameDict.Key;
+					int scriptAssemblyCount = apiVersionAndScriptNameDict.Value.Count;
+
+					string apiVersionString = apiVersion != 0 ? $"{apiVersion}.x" : "0.x or 1.x (fallbacked to the v2 API)";
+
+					Log.Message(Log.Level.Warning, $"Found {scriptAssemblyCount} script(s) resolved to the API version {apiVersionString}. The v2 API is no longer actively supported. Please report to script developers. The list of script names:");
+					foreach (string scriptName in apiVersionAndScriptNameDict.Value)
 					{
-						int apiVersion = apiVersionAndScriptNameDict.Key;
-						int scriptAssemblyCount = apiVersionAndScriptNameDict.Value.Count;
-
-						string apiVersionString = apiVersion != 0 ? $"{apiVersion}.x" : "0.x or 1.x (fallbacked to the v2 API)";
-
-						Log.Message(Log.Level.Warning, $"Found {scriptAssemblyCount} script(s) resolved to the API version {apiVersionString}. The v2 API is no longer actively supported. Please report to script developers. The list of script names:");
-						foreach (string scriptName in apiVersionAndScriptNameDict.Value)
-						{
-							Log.Message(Log.Level.Warning, scriptName);
-						}
+						Log.Message(Log.Level.Warning, scriptName);
 					}
 				}
 			}
