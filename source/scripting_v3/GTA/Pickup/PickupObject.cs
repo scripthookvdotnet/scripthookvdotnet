f20a198cfd9ca964734a0e52812861cc6eaf5961
kagikn <kagikn.3939@gmail.com>
2023-07-13 20:44:05 +0900

    feat(api-v3): PickupObject and Pickup.Object
    PickupObject only has FromHandle as its own member for now, but in
    typewise, more safer than handling pickup props/objects as general
    Prop class
    
    Unfortunately we can't change the (static) return type of World.Get*
    for pickup objects, because that is a breaking change

---
+++ source/scripting_v3/GTA/Pickup/PickupObject.cs
@@ -0,0 +1,46 @@
+//
+// Copyright (C) 2023 kagikn & contributors
+// License: https://github.com/scripthookvdotnet/scripthookvdotnet#license
+//
+using System;
+
+namespace GTA
+{
+	/// <summary>
+	/// Represents a pickup object, which is for a <c>CPickup</c>.
+	/// </summary>
+	public sealed class PickupObject : Prop
+	{
+		internal PickupObject(int handle) : base(handle)
+		{
+		}
+
+		/// <summary>
+		/// Get a <see cref="PickupObject"/> instance by its handle.
+		/// </summary>
+		/// <param name="handle"></param>
+		/// <returns>
+		/// A <see cref="PickupObject"/> if the handle is for pickup object; otherwise, <see langword="null"/>.
+		/// </returns>
+		public static new PickupObject FromHandle(int handle)
+		{
+			IntPtr address = SHVDN.NativeMemory.GetEntityAddress(handle);
+			if (address == IntPtr.Zero)
+			{
+				return null;
+			}
+
+			// We have to use some of Rockstar's RTTI info to ensure if this CObject is CPickup
+			// Native functions that take a pickup object handle verify this in the same way
+			// (fetches an address from the associated handle then tests if the class id hash
+			// is the same as 0xAD2BCC1A (the joaat hash of the name "CPickup")
+			const uint CPickupNameHash = 0xAD2BCC1A;
+			if (SHVDN.NativeMemory.GetRageClassId(address) != CPickupNameHash)
+			{
+				return null;
+			}
+
+			return new PickupObject(handle);
+		}
+	}
+}
