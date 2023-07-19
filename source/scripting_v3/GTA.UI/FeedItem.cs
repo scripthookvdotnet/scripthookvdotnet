e3a7bb613e2b1e2a4674bced0d8c3ccd8750e5e2
kagikn <kagikn.3939@gmail.com>
2023-07-20 01:24:55 +0900

    feat(api-v3): dedicated FeedItem class, more feed post methods, and HudColor enum
    I feel like we could add a struct that represents a pair of a texture
    dictionary and texture name so we can handle txd stuff easily, but I
    don't have any ideas of good names for that struct type

---
+++ source/scripting_v3/GTA.UI/FeedItem.cs
@@ -0,0 +1,67 @@
+//
+// Copyright (C) 2023 kagikn & contributors
+// License: https://github.com/scripthookvdotnet/scripthookvdotnet#license
+//
+
+using GTA.Native;
+using System;
+
+namespace GTA
+{
+	/// <summary>
+	/// Represents a feed item.
+	/// </summary>
+	public class FeedItem
+	{
+		internal FeedItem(int handle)
+		{
+			Handle = handle;
+		}
+
+		/// <summary>
+		/// Gets the feed item handle.
+		/// </summary>
+		public int Handle
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Deletes this <see cref="FeedItem"/> if it still exists.
+		/// </summary>
+		public void Delete() => Function.Call(Hash.THEFEED_REMOVE_ITEM, Handle);
+
+		public override bool Equals(object obj)
+		{
+			if (obj is FeedItem feed)
+			{
+				return Equals(feed);
+			}
+
+			return false;
+		}
+
+		public static bool operator ==(FeedItem left, FeedItem right)
+		{
+			return left?.Equals(right) ?? right is null;
+		}
+		public static bool operator !=(FeedItem left, FeedItem right)
+		{
+			return !(left == right);
+		}
+
+		/// <summary>
+		/// Converts an <see cref="FeedItem"/> to a native input argument.
+		/// </summary>
+		public static implicit operator InputArgument(FeedItem value)
+		{
+			// -1 is the value when feed natives fail to create items
+			return new InputArgument((ulong)(value?.Handle ?? -1));
+		}
+
+		public override int GetHashCode()
+		{
+			return Handle.GetHashCode();
+		}
+	}
+}
---
+++ source/scripting_v3/GTA.UI/FeedTextIcon.cs
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2023 kagikn & contributors
+// License: https://github.com/scripthookvdotnet/scripthookvdotnet#license
+//
+
+namespace GTA.UI
+{
+	public enum FeedTextIcon
+	{
+		Blank,
+		Message,
+		Email,
+		NewContact,
+		Driver,
+		Hacker,
+		Shooter,
+		Invite,
+		RP,
+		Cash,
+		AP,
+		XPAlt,
+		CashAlt,
+	}
+}
