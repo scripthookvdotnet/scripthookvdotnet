e4c354d53e783bd166b7a5b04c2fb4594fca97fd
kagikn <kagikn.3939@gmail.com>
2023-08-07 20:13:29 +0900

    feat(api-v3): methods for preloading ped drawable data on CPedDrawHandler
    You all should learn to request multiple streaming data at once, not
    one by one like most SHVDN script devs and RPH plugin devs (including
    LSPDFR callout devs) do

--- source/scripting_v3/GTA/Entities/Peds/Style.cs
+++ source/scripting_v3/GTA/Entities/Peds/Style.cs
@@ -13,9 +13,11 @@ namespace GTA
 	public sealed class Style
 	{
 		#region Fields
+
 		readonly Ped _ped;
 		Dictionary<PedPropAnchorPoint, PedProp> _pedProps = new();
 		Dictionary<PedComponentType, PedComponent> _pedComponents = new();
+
 		#endregion
 
 		internal Style(Ped ped)
@@ -54,7 +56,7 @@ namespace GTA
 		}
 
 		[Obsolete("Use the indexer overload with the type PedPropAnchorPoint instead."),
-		EditorBrowsable(EditorBrowsableState.Never)]
+		 EditorBrowsable(EditorBrowsableState.Never)]
 		public PedProp this[PedPropType propId] => this[(PedPropAnchorPoint)propId];
 
 		public PedProp[] GetAllProps()
@@ -68,6 +70,7 @@ namespace GTA
 					props.Add(prop);
 				}
 			}
+
 			return props.ToArray();
 		}
 
@@ -82,6 +85,7 @@ namespace GTA
 					components.Add(component);
 				}
 			}
+
 			return components.ToArray();
 		}
 
@@ -92,6 +96,66 @@ namespace GTA
 			variations.AddRange(GetAllProps());
 			return variations.ToArray();
 		}
+
+		/// <summary>
+		/// Sets a component variation to preload into memory, without applying it on this <see cref="Ped"/>.
+		/// </summary>
+		/// <param name="componentType">The ped component type/id.</param>
+		/// <param name="drawableId">The drawable id.</param>
+		/// <param name="textureId">The texture id.</param>
+		public void PreloadVariationData(PedComponentType componentType, int drawableId, int textureId)
+			=> Function.Call(Hash.SET_PED_PRELOAD_VARIATION_DATA, _ped, (int)componentType, drawableId, textureId);
+
+		/// <summary>
+		/// Returns true if the preload data set with <see cref="PreloadVariationData"/> is in memory.
+		/// </summary>
+		public bool HasLoadedPreloadVariationData()
+			=> Function.Call<bool>(Hash.HAS_PED_PRELOAD_VARIATION_DATA_FINISHED, _ped);
+
+		/// <summary>
+		/// Releases the assets set with <see cref="PreloadVariationData"/>.
+		/// </summary>
+		/// <remarks>
+		/// Note that variation data set with <see cref="PreloadVariationData"/> counts towards
+		/// the script memory budget of the SHVDN runtime script.
+		/// For this reason, it is important to use this method to release these assets as soon as you don't need them
+		/// anymore.
+		/// In fact, you can call this method as soon as you have set the same variation with
+		/// <see cref="PedComponent.SetVariation(int, int)"/> since at that point the assets will be rendered on the ped
+		/// and have references to keep them in memory.
+		/// </remarks>
+		public void ReleasePreloadVariationData()
+			=> Function.Call(Hash.RELEASE_PED_PRELOAD_VARIATION_DATA, _ped);
+
+		/// <summary>
+		/// Sets a prop to preload into memory, without applying it on this <see cref="Ped"/>.
+		/// </summary>
+		/// <param name="anchor">The ped component type/id.</param>
+		/// <param name="propId">The prop id.</param>
+		/// <param name="textureId">The texture id.</param>
+		public void PreloadPropData(PedPropAnchorPoint anchor, int propId, int textureId)
+			=> Function.Call(Hash.SET_PED_PRELOAD_PROP_DATA, _ped, (int)anchor, propId, textureId);
+
+		/// <summary>
+		/// Returns true if the preload prop set with <see cref="PreloadPropData"/> is in memory.
+		/// </summary>
+		public bool HasLoadedPreloadPropData()
+			=> Function.Call<bool>(Hash.HAS_PED_PRELOAD_PROP_DATA_FINISHED, _ped);
+
+		/// <summary>
+		/// Releases the assets set with <see cref="PreloadPropData"/>.
+		/// </summary>
+		/// <remarks>
+		/// Note that variation data set with <see cref="PreloadPropData"/> counts towards
+		/// the script memory budget of the SHVDN runtime script.
+		/// For this reason, it is important to use this method to release these assets as soon as you don't need them
+		/// anymore.
+		/// In fact, you can call this method as soon as you have set the same variation with
+		/// <see cref="PedProp.SetVariation(int, int)"/> since at that point the assets will be rendered on the ped
+		/// and have references to keep them in memory.
+		/// </remarks>
+		public void ReleasePreloadPropData()
+			=> Function.Call(Hash.RELEASE_PED_PRELOAD_PROP_DATA, _ped);
 
 		public IEnumerator<IPedVariation> GetEnumerator()
 		{
