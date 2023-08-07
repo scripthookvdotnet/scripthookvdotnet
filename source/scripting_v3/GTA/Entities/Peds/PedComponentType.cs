abede8474caf4653d26d5460ba1f7a6f7f4dc425
kagikn <kagikn.3939@gmail.com>
2023-08-07 22:24:57 +0900

    docs(api-v3): add summary note for ped component type to hint what they are

--- source/scripting_v3/GTA/Entities/Peds/PedComponentType.cs
+++ source/scripting_v3/GTA/Entities/Peds/PedComponentType.cs
@@ -7,17 +7,42 @@ namespace GTA
 {
 	public enum PedComponentType
 	{
+		/// <summary>
+		/// The head component. Despite the symbol name, this component is not limited to face.
+		/// </summary>
 		Face,
+		/// <summary>
+		/// The beard component. Despite the symbol name, this component is supposed to be limited to beard.
+		/// </summary>
 		Head,
 		Hair,
+		/// <summary>
+		/// The upper body component.
+		/// </summary>
 		Torso,
+		/// <summary>
+		/// The lower body component.
+		/// </summary>
 		Legs,
 		Hands,
+		/// <summary>
+		/// The feet component.
+		/// </summary>
 		Shoes,
+		/// <summary>
+		/// The "teeth" component, which is not used for teeth geometry in practice and instead used as an accessory
+		/// component instead.
+		/// </summary>
 		Special1,
 		Special2,
 		Special3,
+		/// <summary>
+		/// The decal component.
+		/// </summary>
 		Textures,
-		Torso2,
+		/// <summary>
+		/// The JIJB component.
+		/// </summary>
+		Torso2
 	}
 }
