8a723bfafd80073fc983db0face18a768dabb4f1
kagikn <kagikn.3939@gmail.com>
2023-12-11 12:42:16 +0900

    feat!(api-v3): move 3 static water methods to new Water static class and rename to the same internal names in exe
    A leak of roadpathsgenerator_win64_beta probes that there are static
    Water class in GTA5, which contains GetWaterLevel, GetWaterLevelNoWaves,
    and TestLineAgainstWater, and I thought it would be great to move
    relevant water methods in our codebase into a new Water static class
    so the World class won't get NAIVELY bloated.
    
    Also renames the 3 methods to the same internal names defined in the
    exe.

---
+++ source/scripting_v3/GTA/Water.cs
@@ -0,0 +1,89 @@
+//
+// Copyright (C) 2023 kagikn & contributors
+// License: https://github.com/scripthookvdotnet/scripthookvdotnet#license
+//
+
+using GTA.Math;
+using GTA.Native;
+
+namespace GTA
+{
+    /// <summary>
+    /// Represents a collection of static methods about water.
+    /// </summary>
+    public static class Water
+    {
+        /// <summary>
+        /// Gets the level/height of the water below the position including the waves.
+        /// This method takes the waves into account so the result may be different depending on
+        /// the exact frame of calling.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if successfully get the level/height of the water below
+        /// the position; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <remarks>
+        /// This method gets the level of the water excluding the waves first, then adds the wave
+        /// level delta if the method have successfully retrieve the level/height of the water.
+        /// Therefore, this method returns <see langword="false"/> when
+        /// <see cref="GetWaterLevelNoWaves"/> returns <see langword="false"/> with the same
+        /// arguments.
+        /// </remarks>
+        public static bool GetWaterLevel(Vector3 position, out float height)
+        {
+            unsafe
+            {
+                float returnZ;
+                bool foundWater =
+                    Function.Call<bool>(Hash.GET_WATER_HEIGHT, position.X, position.Y, position.Z, &returnZ);
+
+                height = returnZ;
+                return foundWater;
+            }
+        }
+        /// <summary>
+        /// Gets the height of the water below the position excluding the waves.
+        /// This method does not take the waves into account so the result will be the same between
+        /// different frames.
+        /// </summary>
+        /// <returns></returns>
+        public static bool GetWaterLevelNoWaves(Vector3 position, out float height)
+        {
+            unsafe
+            {
+                float returnZ;
+                bool foundWater = Function.Call<bool>(Hash.GET_WATER_HEIGHT_NO_WAVES, position.X, position.Y,
+                    position.Z, &returnZ);
+
+                height = returnZ;
+                return foundWater;
+            }
+        }
+
+        /// <summary>
+        /// Test a directed line probe against the water.
+        /// </summary>
+        /// <param name="startPos">The start of the probe.</param>
+        /// <param name="endPos">The end of the probe.</param>
+        /// <param name="intersectionPos">
+        /// When this method returns, contains the intersection position on the water, the line probe hits water before
+        /// hitting land. This parameter is passed uninitialized.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the line probe hits water before hitting land; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        public static bool TestLineAgainstWater(Vector3 startPos, Vector3 endPos, out Vector3 intersectionPos)
+        {
+            unsafe
+            {
+                NativeVector3 outPos;
+                bool hitWaterBeforeLand = Function.Call<bool>(Hash.TEST_PROBE_AGAINST_WATER, startPos.X, startPos.Y,
+                    startPos.Z, endPos.X, endPos.Y, endPos.Z, &outPos);
+
+                intersectionPos = outPos;
+                return hitWaterBeforeLand;
+            }
+        }
+    }
+}
--- source/scripting_v3/GTA/World.cs
+++ source/scripting_v3/GTA/World.cs
@@ -2728,66 +2728,6 @@ namespace GTA
                 maxPosition.Y);
 
         /// <summary>
-        /// Gets the height of the water below the position including the waves.
-        /// This method takes the waves into account so the result may be different depending on the exact frame of calling.
-        /// </summary>
-        /// <returns></returns>
-        public static bool GetWaterHeight(Vector3 position, out float height)
-        {
-            unsafe
-            {
-                float returnZ;
-                bool foundWater =
-                    Function.Call<bool>(Hash.GET_WATER_HEIGHT, position.X, position.Y, position.Z, &returnZ);
-
-                height = returnZ;
-                return foundWater;
-            }
-        }
-        /// <summary>
-        /// Gets the height of the water below the position excluding the waves.
-        /// This method does not take the waves into account so the result will be the same between different frames.
-        /// </summary>
-        /// <returns></returns>
-        public static bool GetWaterHeightNoWaves(Vector3 position, out float height)
-        {
-            unsafe
-            {
-                float returnZ;
-                bool foundWater = Function.Call<bool>(Hash.GET_WATER_HEIGHT_NO_WAVES, position.X, position.Y,
-                    position.Z, &returnZ);
-
-                height = returnZ;
-                return foundWater;
-            }
-        }
-
-        /// <summary>
-        /// Test a directed probe against the water.
-        /// </summary>
-        /// <param name="startPos">The start of the probe.</param>
-        /// <param name="endPos">The end of the probe.</param>
-        /// <param name="intersectionPos">
-        /// When this method returns, contains the intersection position on the water, the probe hits water before
-        /// hitting land. This parameter is passed uninitialized.
-        /// </param>
-        /// <returns>
-        /// <see langword="true"/> if the probe hits water before hitting land; otherwise, <see langword="false"/>.
-        /// </returns>
-        public static bool TestProbeAgainstWater(Vector3 startPos, Vector3 endPos, out Vector3 intersectionPos)
-        {
-            unsafe
-            {
-                NativeVector3 outPos;
-                bool hitWaterBeforeLand = Function.Call<bool>(Hash.TEST_PROBE_AGAINST_WATER, startPos.X, startPos.Y,
-                    startPos.Z, endPos.X, endPos.Y, endPos.Z, &outPos);
-
-                intersectionPos = outPos;
-                return hitWaterBeforeLand;
-            }
-        }
-
-        /// <summary>
         /// Checks to see if it can find a safe bit of ground to place a <see cref="Ped"/>.
         /// </summary>
         /// <param name="position">The position to check around.</param>
