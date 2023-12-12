b95db42123f63bbda3582635a446ea5a5939bebd
kagikn <kagikn.3939@gmail.com>
2023-12-13 00:26:10 +0900

    feat(api-v3): add more known combat flags (for CTaskCombat::CombatFlags)
    The members in CTaskCombat::CombatFlags is named in the following
    convention: "ComF_[flag name]"
    
    Omitted members where I could not observe effects when initialing
    CTaskCombat:
    ```
    TargetChangedThisFrame = 32,
    WasCrouched = 64,
    UsingSecondaryTarget = 128,
    ExplosionValid = 256,
    RequiresOrder = 512,
    FiringAtTarget = 1024,
    WasPlayingAimPoseTransitionLastFrame = 2048,
    IsPlayingAimPoseTransition = 4096,
    HasStarted = 8192,
    DoingEarlyVehicleEntry = 131072,
    ExitCombatRequested = 262144,
    IsUsingFranticRuns = 524288,
    UseFrustratedAdvance = 1048576,
    MeleeAnimPhaseSync = 2097152,
    MeleeImmediateThreat = 4194304,
    IsWaitingAtRoadBlock = 8388608,
    TaskAbortedForStaticMovement = 16777216,
    MoveToCoverStopped = 33554432,
    TemporarilyForceFiringState = 268435456,
    QuitIfDriverJacked = 1073741824,
    ```

--- source/scripting_v3/GTA/Entities/Peds/Task/Combat/TaskCombatPedFlags.cs
+++ source/scripting_v3/GTA/Entities/Peds/Task/Combat/TaskCombatPedFlags.cs
@@ -14,7 +14,14 @@ namespace GTA
     public enum TaskCombatFlags
     {
         None = 0,
+        UseFlinchAimIntro = 16384,
+        UseSurprisedAimIntro = 32768,
+        /// <summary>
+        /// The <see cref="Ped"/> will try to arrest the target if they are a cop, or this flag does not have effect.
+        /// </summary>
+        ArrestTarget = 65536,
         PreventChangingTarget = 67108864,
         DisableAimIntro = 134217728,
+        UseSniperAimIntro = 536870912,
     }
 }
