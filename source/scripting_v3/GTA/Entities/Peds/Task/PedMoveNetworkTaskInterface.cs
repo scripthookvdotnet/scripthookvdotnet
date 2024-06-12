4b6ff1493922a32438a3138e0545e17fce0b068a
kagikn <kagikn.3939@gmail.com>
2024-06-13 00:49:11 +0900

    docs(api-v3): add a lot of docs to move network methods and properties

--- source/scripting_v3/GTA/Entities/Peds/Task/PedMoveNetworkTaskInterface.cs
+++ source/scripting_v3/GTA/Entities/Peds/Task/PedMoveNetworkTaskInterface.cs
@@ -15,32 +15,62 @@ namespace GTA
         }
 
         /// <summary>
-        /// Gets the <see cref="Ped"/>.
+        /// Gets the <see cref="GTA.Ped"/>.
         /// </summary>
         private Ped Ped
         {
             get;
         }
 
+        /// <summary>
+        /// Returns <see langword="true"/> if a move network is active.
+        /// </summary>
+        /// <value>
+        /// <see langword="true"/> if the <see cref="GTA.Ped"/> is processing a move network task
+        /// (`<c>CTaskMoVEScripted</c>`) and a move network is active in the found task; otherwise,
+        /// <see langword="false"/>.
+        /// </value>
+        /// <remarks>
+        /// The property searches for a `<c>CTaskMoVEScripted</c>` by searching primary tasks first, then secondary
+        /// tasks. The property returns <see langword="false"/> when `<c>CTaskMoVEScripted</c>`s are found among
+        /// both primary and secondary tasks and the found a move network is not active in
+        /// the found `<c>CTaskMoVEScripted</c>` of primary tasks.
+        /// </remarks>
         public bool IsTaskActive => Function.Call<bool>(Hash.IS_TASK_MOVE_NETWORK_ACTIVE, Ped);
 
+        /// <summary>
+        /// Returns <see langword="true"/> if a move network is ready for a state transition
+        /// </summary>
         public bool IsReadyForTransition => Function.Call<bool>(Hash.IS_TASK_MOVE_NETWORK_READY_FOR_TRANSITION, Ped);
 
+
+        /// <summary>
+        /// Returns <see langword="true"/> if a move network is ready for a state transition.
+        /// </summary>
         public bool RequestStateTransition(string stateName)
             => Function.Call<bool>(Hash.REQUEST_TASK_MOVE_NETWORK_STATE_TRANSITION, Ped, stateName);
 
-        /// <value>
-        /// The current script name if if a `<c>CTaskMoVEScripted</c>` is running on the <see cref="Ped"/> and
-        /// the network is active on the task; otherwise, the string `<c>Unknown</c>`.
-        /// </value>
+        /// <summary>
+        /// Returns the current state.
+        /// </summary>
+        /// <remarks>
+        /// Returns "<c>Unknown</c>" when <see cref="IsTaskActive"/> returns <see langword="false"/>.
+        /// </remarks>
         public string CurrentScriptStateName
             => Function.Call<string>(Hash.GET_TASK_MOVE_NETWORK_STATE, Ped);
 
         /// <summary>
         /// <para>
+        /// Sets a clip set for this move network to use.
+        /// </para>
+        /// <para>
         /// Only available in the game version v1.0.1493.0 and later.
         /// </para>
         /// </summary>
+        /// <param name="clipSet">The hash of the name of the actual clip set to use.</param>
+        /// <param name="varClipSet">
+        /// The hash of the name of the variable clip set you are setting.
+        /// </param>
         public void SetNetworkClipSet(AtHashValue clipSet, AtHashValue varClipSet)
         {
             GameVersionNotSupportedException.ThrowIfNotSupported(GameVersion.v1_0_1493_0_Steam,
@@ -49,16 +79,39 @@ namespace GTA
             Function.Call(Hash.SET_TASK_MOVE_NETWORK_ANIM_SET, Ped, clipSet, varClipSet);
         }
 
+        /// <summary>
+        /// Sets a <see cref="float"/> MoVE signal to the passed value.
+        /// </summary>
         public void SetSignalFloat(string signalName, float signal)
         {
             Function.Call(Hash.SET_TASK_MOVE_NETWORK_SIGNAL_FLOAT, Ped, signalName, signal);
         }
 
+        /*
+         *  We omit `SET_TASK_MOVE_NETWORK_SIGNAL_LOCAL_FLOAT` because there's nothing different from
+         *  `SET_TASK_MOVE_NETWORK_SIGNAL_FLOAT` when `NetworkInterface::IsGameInProgress()` returns false (meaning
+         *  the game is not online), or if the ped is not a network clone or the passed "allowOverrideCloneUpdate"
+         *  is set to false.
+         */
+
         /// <summary>
+        /// <para>
+        /// Sets the lerp rate to the passed value.
+        /// </para>
         /// <para>
         /// Only available in the game version v1.0.1493.0 and later.
         /// </para>
         /// </summary>
+        /// <remarks>
+        /// Lerp rate controls rate at which the corresponding MoVE float signal will lerp over frame updates on
+        /// the clone from the current value to the target value.
+        /// It is assumed that the corresponding MoVE float signal has already been created using
+        /// <see cref="SetSignalFloat"/>.
+        /// Currently the lerp rate defaults to a value of `<c>0.5f</c>`. The lerp value has to be above `<c>0.0f</c>`
+        /// and below `<c>1.0f</c>`.
+        /// If a lerp rate of `<c>1.0f</c>` is applied then no lerping is used and the exact float value will be
+        /// synced and applied immediately on the clone.
+        /// </remarks>
         public void SetSignalFloatLerpRate(string signalName, float lerpRate)
         {
             GameVersionNotSupportedException.ThrowIfNotSupported(GameVersion.v1_0_1493_0_Steam,
@@ -67,12 +120,18 @@ namespace GTA
             Function.Call(Hash.SET_TASK_MOVE_NETWORK_SIGNAL_FLOAT_LERP_RATE, Ped, signalName, lerpRate);
         }
 
+        /// <summary>
+        /// Sets a <see cref="bool"/> MoVE signal to the passed value.
+        /// </summary>
         public void SetSignalBool(string signalName, bool signal)
         {
             Function.Call(Hash.SET_TASK_MOVE_NETWORK_SIGNAL_BOOL, Ped, signalName, signal);
         }
 
         /// <summary>
+        /// <para>
+        /// Gets the value of a <see cref="float"/> type output parameter from the peds scripted MoVE network.
+        /// </para>
         /// <para>
         /// Only available in the game version v1.0.1493.0 and later.
         /// </para>
@@ -85,9 +144,16 @@ namespace GTA
             return Function.Call<float>(Hash.GET_TASK_MOVE_NETWORK_SIGNAL_FLOAT, Ped, signalName);
         }
 
+        /// <summary>
+        /// Gets the value of a <see cref="bool"/> type output parameter from the peds scripted MoVE network.
+        /// </summary>
         public bool GetSignalBool(string signalName)
             => Function.Call<bool>(Hash.GET_TASK_MOVE_NETWORK_SIGNAL_BOOL, Ped, signalName);
 
+        /// <summary>
+        /// Returns <see langword="true"/> if an event with the given name has just fired on
+        /// the <see cref="GTA.Ped"/>'s script owned MoVE network.
+        /// </summary>
         public bool GetEvent(string eventName)
             => Function.Call<bool>(Hash.GET_TASK_MOVE_NETWORK_EVENT, Ped, eventName);
     }
