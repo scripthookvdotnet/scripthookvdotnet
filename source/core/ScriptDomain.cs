fd3eca5cc035686f8d513c69765769223764bfd9
kagikn <kagikn.3939@gmail.com>
2024-07-13 02:15:35 +0900

    feat(core): clarify v2 API may stop being supported in deprecated warning messages
    Conversations in #1469 made me think the current messages may not clear
    enough to make *active* script authors migrate their scripts to v3 API.

--- source/core/ScriptDomain.cs
+++ source/core/ScriptDomain.cs
@@ -766,7 +766,7 @@ namespace SHVDN
                     int scriptCountUsingDeprecatedApi = DeprecatedScriptAssemblyNamesPerApiVersion.Values.Aggregate(0, (result, current) => result + current.Count);
 
                     PostTickerToFeed(
-                        message: $"~o~WARNING~s~: {scriptCountUsingDeprecatedApi.ToString()} scripts are using the v2 API (ScriptHookVDotNet2.dll), which is deprecated and not actively supported. See the console outputs or the log file for more details.",
+                        message: $"~o~WARNING~s~: {scriptCountUsingDeprecatedApi.ToString()} scripts are using the v2 API (ScriptHookVDotNet2.dll), which is deprecated and not actively supported. It may stop being supported in the future. See the console outputs or the log file for more details.",
                         isImportant: true,
                         cacheMessage: false
                     );
@@ -779,7 +779,7 @@ namespace SHVDN
 
                     string apiVersionString = apiVersion != 0 ? $"{apiVersion.ToString()}.x" : "0.x or 1.x (fallbacked to the v2 API)";
 
-                    Log.Message(Log.Level.Warning, $"Found {scriptAssemblyCount.ToString()} script(s) resolved to the deprecated API version {apiVersionString} (ScriptHookVDotNet2.dll). The v2 API is no longer actively supported. Please report to the authors who developed some of the deprecated scripts. The list of script names:");
+                    Log.Message(Log.Level.Warning, $"Found {scriptAssemblyCount.ToString()} script(s) resolved to the deprecated API version {apiVersionString} (ScriptHookVDotNet2.dll). The v2 API is no longer actively supported. It may stop being supported in the future. Please report to the authors who developed some of the deprecated scripts. The list of script names:");
                     foreach (string scriptName in apiVersionAndScriptNameDict.Value)
                     {
                         Log.Message(Log.Level.Warning, scriptName);
