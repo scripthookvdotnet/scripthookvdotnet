scripthookvdotnet/scripthookvdotnet

commit f86e2ce33b24ca8af876218e3e815c85b37e293e

feat(api-v3): remove `shouldLockInterpolationSourceFrame` from `StartRendering` and `StopRendering for being useless
`shouldLockInterpolationSourceFrame` does nothing if the duration param
is zero (or overridden with zero by `bInterp` being false)

source/scripting_v3/GTA/Camera/ScriptCameraDirector.cs
@@ -46,8 +46,8 @@ namespace GTA
         /// Tells the game that script thread of the SHVDN runtime (<c>GtaThread</c>, not individual SHVDN scripts)
         /// wants to enable rendering of scripted cameras.
         /// </summary>
-        public static void StartRendering(bool shouldLockInterpolationSourceFrame = true)
-            => Function.Call(Hash.RENDER_SCRIPT_CAMS, true, false, 3000, shouldLockInterpolationSourceFrame, 0, 0);
+        public static void StartRendering()
+            => Function.Call(Hash.RENDER_SCRIPT_CAMS, true, false, 3000, false, 0, 0);
         /// <summary>
         /// Starts rendering a scripted camera while interpolating from the gameplay camera
         /// that the gameplay camera director (<c>camGameplayDirector</c>) is using to a scripted camera.
@@ -65,10 +65,8 @@ namespace GTA
         /// wants to disable rendering of scripted cameras.
         /// </summary>
         /// <inheritdoc cref="StopRenderingWithInterp"/>
-        public static void StopRendering(bool shouldLockInterpolationSourceFrame = true,
-            bool shouldApplyAcrossAllThreads = false)
-            => Function.Call(Hash.RENDER_SCRIPT_CAMS, false, false, 3000, shouldLockInterpolationSourceFrame,
-                shouldApplyAcrossAllThreads, 0);
+        public static void StopRendering(bool shouldApplyAcrossAllThreads = false)
+            => Function.Call(Hash.RENDER_SCRIPT_CAMS, false, false, 3000, false, shouldApplyAcrossAllThreads, 0);
         /// <summary>
         /// Stops rendering a scripted camera while interpolating from the previously rendered scripted camera to
         /// the gameplay camera that the gameplay camera director (<c>camGameplayDirector</c>) is using.

commit c76e937c8ac74542d0c81ffcc7090b1971506ff8

docs(api-v3): enhance docs of `(Start|Stop)Rendering`
This commit mainly clarifies how `shouldLockInterpolationSourceFrame`
works, and explains how `interpDuration` works more.

Also fixes a XML cref error caused by f86e2ce33b24ca8af876218e3e815c85b37e293e.

source/scripting_v3/GTA/Camera/ScriptCameraDirector.cs
@@ -46,6 +46,9 @@ namespace GTA
         /// Tells the game that script thread of the SHVDN runtime (<c>GtaThread</c>, not individual SHVDN scripts)
         /// wants to enable rendering of scripted cameras.
         /// </summary>
+        /// <remarks>
+        /// <inheritdoc cref="StopRenderingWithInterp" path="/remarks"/>
+        /// </remarks>
         public static void StartRendering()
             => Function.Call(Hash.RENDER_SCRIPT_CAMS, true, false, 3000, false, 0, 0);
         /// <summary>
@@ -54,7 +57,17 @@ namespace GTA
         /// Tells the game that script thread of the SHVDN runtime (<c>GtaThread</c>, not individual SHVDN scripts)
         /// wants to enable rendering of scripted cameras.
         /// </summary>
-        /// <inheritdoc cref="StopRenderingWithInterp"/>
+        /// <param name="interpDuration">
+        /// The interpolation duration in milliseconds.
+        /// If zero, this method will behave the same as <see cref="StartRendering"/> and
+        /// <paramref name="shouldLockInterpolationSourceFrame"/> will not have effect.
+        /// </param>
+        /// <param name="shouldLockInterpolationSourceFrame">
+        /// <inheritdoc cref="StopRenderingWithInterp" path="/param[@name='shouldLockInterpolationSourceFrame']"/>
+        /// </param>
+        /// <remarks>
+        /// <inheritdoc cref="StopRenderingWithInterp" path="/remarks"/>
+        /// </remarks>
         public static void StartRenderingWithInterp(int interpDuration = 3000,
             bool shouldLockInterpolationSourceFrame = true)
             => Function.Call(Hash.RENDER_SCRIPT_CAMS, true, true, interpDuration, shouldLockInterpolationSourceFrame,
@@ -73,11 +86,23 @@ namespace GTA
         /// Tells the game that script thread of the SHVDN runtime (<c>GtaThread</c>, not individual SHVDN scripts)
         /// wants to disable rendering of scripted cameras.
         /// </summary>
-        /// <param name="interpDuration">The interpolation duration in milliseconds.</param>
+        /// <param name="interpDuration">
+        /// The interpolation duration in milliseconds.
+        /// If zero, this method will behave the same as <see cref="StopRendering"/> and
+        /// <paramref name="shouldLockInterpolationSourceFrame"/> will not have effect.
+        /// </param>
         /// <param name="shouldLockInterpolationSourceFrame">
-        /// If <see langword="false"/>, the source frame is updated throughout the interpolation,
+        /// <para>
+        /// If <see langword="false"/>, the source camera frame is updated throughout the interpolation,
         /// allowing for fully dynamic interpolation that can reduce the appearance of 'lag' when the source frame is
-        /// not static.
+        /// not static. For example, when the Z coordinates of the source scripted camera and a destination gameplay
+        /// camera are the same at the time when the interpolation starts but the Z coordinate of the source scripted
+        /// camera are added after the interpolation starts, that of the moving camera (frame) should be greater than
+        /// that of the destination gameplay camera.
+        /// </para>
+        /// <para>
+        /// Does not have any effect if <paramref name="interpDuration"/> is zero.
+        /// </para>
         /// </param>
         /// <param name="shouldApplyAcrossAllThreads">
         /// If <see langword="true"/>, a request to stop rendering will be enforced irrespective of whether other
@@ -86,10 +111,10 @@ namespace GTA
         /// </param>
         /// <remarks>
         /// At least one of the scripts loaded by SHVDN must have created a <see cref="Camera"/> that can be rendered,
-        /// so you would want to set <see cref="Camera.IsActive"/> to <see langword="true"/> on your
+        /// thus you would want to set <see cref="Camera.IsActive"/> to <see langword="true"/> on your
         /// <see cref="Camera"/>.
-        /// Note that rendering is typically not stopped if another script thread (<c>GtaThread</c>) other than
-        /// the SHVDN runtime still expects it to be active (see <paramref name="shouldApplyAcrossAllThreads"/>.)
+        /// Note that rendering is typically not stopped if other script threads (<c>GtaThread</c>) other than
+        /// the SHVDN runtime still expect it to be active (see <paramref name="shouldApplyAcrossAllThreads"/>.)
         /// </remarks>
         public static void StopRenderingWithInterp(int interpDuration = 3000,
             bool shouldLockInterpolationSourceFrame = true, bool shouldApplyAcrossAllThreads = false)

source/scripting_v3/GTA/World.cs
@@ -1605,7 +1605,7 @@ namespace GTA
         }
 
         /// <summary>
-        /// Creates a <see cref="Camera"/>, use <see cref="ScriptCameraDirector.StartRendering(bool)"/> to switch to
+        /// Creates a <see cref="Camera"/>, use <see cref="ScriptCameraDirector.StartRendering()"/> to switch to
         /// this camera.
         /// </summary>
         /// <param name="position">The position of the camera.</param>