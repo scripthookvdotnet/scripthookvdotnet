0fa6c543cb148bdf66521d6c4c1c667e1ed2e115
kagikn <kagikn.3939@gmail.com>
2024-09-27 22:51:58 +0900

    build: build ASI project using 1.0.3337.0 (2024) SDK
    The ASI project now expect the 1.0.3337.0 (2024) SDK rather than the
    legacy one, where the lastst version is 1.0.617.1. Also
    `install_sdk.ps1` now specifies the new SDK URL to download. The
    previous link, which refers to the 1.0.617.1 SDK, got unavailable at
    Sep 25, 2024, 6:20 PM UTC *at the latest*, although the new SDK
    contains the legacy one.
    
    Fixes #1500.

--- tools/install_sdk.ps1
+++ tools/install_sdk.ps1
@@ -1,7 +1,10 @@
 # Set the working directory to SDK
 Set-Location "sdk/"
-# Download the file with Invoke-WebRequest
-Invoke-WebRequest -Uri "http://www.dev-c.com/files/ScriptHookV_SDK_1.0.617.1a.zip" -OutFile "sdk.zip" -Headers @{ "Referer" = "http://www.dev-c.com/gtav/scripthookv/" }
+# Download the file with Invoke-WebRequest.
+# The SDK URL is expected to get unavailable every time a new SDK is released.
+# See the following issue for details. URL: https://github.com/scripthookvdotnet/scripthookvdotnet/issues/1500
+$SdkUrl = "http://www.dev-c.com/files/ScriptHookV_SDK_1.0.3337.0.zip"
+Invoke-WebRequest -Uri $SdkUrl -OutFile "sdk.zip" -Headers @{ "Referer" = "http://www.dev-c.com/gtav/scripthookv/" }
 # Extract the contents of the file in the current directory
 7z x sdk.zip
 # Return to the location where we started
