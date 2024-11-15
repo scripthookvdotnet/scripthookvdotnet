515c947f265ec9c41b49bab17f557e28fdbed39f
kagikn <kagikn.3939@gmail.com>
2024-11-15 13:01:04 +0900

    perf(core): avoid calling finalizers on streaming stuff after `IsManagedAssembly`
    We should avoid letting finalizers called since having to call them
    increases overhead a lot. We release file handles as soon as we
    finish testing if some assemblies are managed ones.

--- source/core/ScriptDomain.cs
+++ source/core/ScriptDomain.cs
@@ -1627,54 +1627,70 @@ namespace SHVDN
             return null;
         }
 
-        private static bool IsManagedAssembly(string filename)
+        private static bool IsManagedAssembly(string fileName)
         {
             try
             {
-                using Stream file = new FileStream(filename, FileMode.Open, FileAccess.Read);
-                if (file.Length < 64)
+                using (Stream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                using (BinaryReader binaryReader = new BinaryReader(fileStream))
                 {
-                    return false;
-                }
+                    if (fileStream.Length < 64)
+                    {
+                        return false;
+                    }
 
-                using var bin = new BinaryReader(file);
-                // PE header starts at offset 0x3C (60). Its a 4 byte header.
-                file.Position = 0x3C;
-                uint offset = bin.ReadUInt32();
-                if (offset == 0)
-                {
-                    offset = 0x80;
-                }
+                    //PE Header starts @ 0x3C (60). Its a 4 byte header.
+                    fileStream.Position = 0x3C;
+                    uint peHeaderPointer = binaryReader.ReadUInt32();
+                    if (peHeaderPointer == 0)
+                    {
+                        peHeaderPointer = 0x80;
+                    }
 
-                // Ensure there is at least enough room for the following structures:
-                //     24 byte PE Signature & Header
-                //     28 byte Standard Fields         (24 bytes for PE32+)
-                //     68 byte NT Fields               (88 bytes for PE32+)
-                // >= 128 byte Data Dictionary Table
-                if (offset > file.Length - 256)
-                {
-                    return false;
-                }
+                    // Ensure there is at least enough room for the following structures:
+                    //     24 byte PE Signature & Header
+                    //     28 byte Standard Fields         (24 bytes for PE32+)
+                    //     68 byte NT Fields               (88 bytes for PE32+)
+                    // >= 128 byte Data Dictionary Table
+                    if (peHeaderPointer > fileStream.Length - 256)
+                    {
+                        return false;
+                    }
 
-                // Check the PE signature. Should equal 'PE\0\0'.
-                file.Position = offset;
-                if (bin.ReadUInt32() != 0x00004550)
-                {
-                    return false;
-                }
+                    // Check the PE signature.  Should equal 'PE\0\0'.
+                    fileStream.Position = peHeaderPointer;
+                    uint peHeaderSignature = binaryReader.ReadUInt32();
+                    if (peHeaderSignature != 0x00004550)
+                    {
+                        return false;
+                    }
 
-                // Read PE magic number from Standard Fields to determine format.
-                file.Position += 20;
-                ushort peFormat = bin.ReadUInt16();
-                if (peFormat != 0x10b /* PE32 */ && peFormat != 0x20b /* PE32Plus */)
-                {
-                    return false;
-                }
+                    // skip over the PEHeader fields
+                    fileStream.Position += 20;
 
-                // Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
-                // When this is non-zero then the file contains CLI data otherwise not.
-                file.Position = offset + (peFormat == 0x10b ? 232 : 248);
-                return bin.ReadUInt32() != 0;
+                    const ushort PE32 = 0x10b;
+                    const ushort PE32Plus = 0x20b;
+
+                    // Read PE magic number from Standard Fields to determine format.
+                    var peFormat = binaryReader.ReadUInt16();
+                    if (peFormat != PE32 && peFormat != PE32Plus)
+                    {
+                        return false;
+                    }
+
+                    // Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
+                    // When this is non-zero then the file contains CLI data otherwise not.
+                    ushort dataDictionaryStart = (ushort)(peHeaderPointer + (peFormat == PE32 ? 232 : 248));
+                    fileStream.Position = dataDictionaryStart;
+
+                    uint cliHeaderRva = binaryReader.ReadUInt32();
+                    if (cliHeaderRva == 0)
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }
             }
             catch
             {
