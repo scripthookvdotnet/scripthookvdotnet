scripthookvdotnet/scripthookvdotnet

commit f91299d57b2fd370e27b387b6cd357e7ce1eb482

fix(api-v3): `PedProp.IsVariationValid` preloading a variation and returning if the preload handle is not zero
The method now tests what `CPedPropData::SetPedProp(Ped* pPed,
eAnchorPoints anchorID, s32 propID, s32 texID, eAnchorPoints
anchorOverrideID, Vector3 *pPosOffset, Quaternion *pRotOffset, u32
streamingFlags)` does before doing the main part of the procedures,
except for the flee slot test (to put the prop).

The method preloaded a variation and returns if the created handle is
not zero before. After preloading 36 variations without clearing, it
returned false even if the variation was valid.

Fixes #1542.

source/scripting_v3/GTA/Entities/Peds/PedProp.cs
@@ -79,13 +79,30 @@ namespace GTA
 
         public bool IsVariationValid(int index, int textureIndex = 0)
         {
-            if (index == 0)
+            const uint NumAnchors = 12;
+
+            if ((uint)AnchorPoint >= NumAnchors)
             {
-                return true; // No prop is always valid
+                return false;
             }
 
-            return Function.Call<bool>(Hash.SET_PED_PRELOAD_PROP_DATA, _ped.Handle, (int)AnchorPoint, index - 1, textureIndex);
+            // No prop is always valid if `index` or `textureIndex` is negative, and `SET_PED_PROP_INDEX` does nothing
+            // when either of the passed drawable index or texture index is negative.
+            int actualDrawableIndex = index - 1;
+            return IsPedPropDrawableVariationValid(_ped, AnchorPoint, actualDrawableIndex) &&
+                   IsPedPropTextureVariationValid(_ped, AnchorPoint, actualDrawableIndex, textureIndex);
         }
+
+        private static bool IsPedPropDrawableVariationValid(Ped ped, PedPropAnchorPoint anchorPoint, int drawableIndex)
+            => (drawableIndex > 0 &&
+                (Function.Call<int>(Hash.GET_NUMBER_OF_PED_PROP_DRAWABLE_VARIATIONS, ped.Handle,
+                (int)anchorPoint) < drawableIndex));
+
+        private static bool IsPedPropTextureVariationValid(Ped ped, PedPropAnchorPoint anchorPoint, int drawableIndex,
+            int textureIndex)
+            => (textureIndex >= 0 &&
+                (Function.Call<int>(Hash.GET_NUMBER_OF_PED_PROP_TEXTURE_VARIATIONS, ped.Handle,
+                    (int)anchorPoint, drawableIndex) < textureIndex));
 
         public bool HasVariations => Count > 1;
 

commit 09e24924144fa6544628c1f50c9a84335eea9dfb

fix(api-v3): `IsPedPropDrawableVariationValid` and `IsPedPropTextureVariationValid` returning reversed value in almost al cases
The bug is introduced by f91299d57b2fd370e27b387b6cd357e7ce1eb482.

Fixes #1542, by fixing the methods so they return correct values.

source/scripting_v3/GTA/Entities/Peds/PedProp.cs
@@ -104,9 +104,9 @@ namespace GTA
         /// variations.
         /// </summary>
         private static bool IsPedPropDrawableVariationValid(Ped ped, PedPropAnchorPoint anchorPoint, int drawableIndex)
-            => (drawableIndex > 0 &&
-                (Function.Call<int>(Hash.GET_NUMBER_OF_PED_PROP_DRAWABLE_VARIATIONS, ped.Handle,
-                (int)anchorPoint) < drawableIndex));
+            => (drawableIndex >= 0 &&
+                (drawableIndex < Function.Call<int>(Hash.GET_NUMBER_OF_PED_PROP_DRAWABLE_VARIATIONS, ped.Handle,
+                    (int)anchorPoint)));
 
         /// <summary>
         /// Returns a value that indicates whether the drawable index is valid for the specified <see cref="Ped"/>
@@ -117,8 +117,8 @@ namespace GTA
         private static bool IsPedPropTextureVariationValid(Ped ped, PedPropAnchorPoint anchorPoint, int drawableIndex,
             int textureIndex)
             => (textureIndex >= 0 &&
-                (Function.Call<int>(Hash.GET_NUMBER_OF_PED_PROP_TEXTURE_VARIATIONS, ped.Handle,
-                    (int)anchorPoint, drawableIndex) < textureIndex));
+                (textureIndex < Function.Call<int>(Hash.GET_NUMBER_OF_PED_PROP_TEXTURE_VARIATIONS, ped.Handle,
+                    (int)anchorPoint, drawableIndex)));
 
         public bool HasVariations => Count > 1;
 