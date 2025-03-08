6bd6c7a3956f1fee78adcb6692268d76127b68bd
kagikn <kagikn.3939@gmail.com>
2025-03-08 14:48:44 +0900

    build(core): use b617 version of SHV SDK again
    We didn't use any features added in the b3337 version of SHV at all.
    Maybe we would use `scriptsAreLaunchedUsingReloading`, which is added
    in SHV b3351 and @Tanjitsu suggested in #1549, but with dynamic checks
    so users can use SHV where the latest supported game version is b3337
    or older (probably). Custom start up and clean up callbacks that can
    take more than 2 seconds, which is different from `DllMain`, would be
    definitely worth bumping the minimal SHV version, but I still doubt if
    `scriptsAreLaunchedUsingReloading` is worth it.
    
    Resolves #1555 for eliminating the contradiction of how the SDK should
    be installed.

--- tools/install_sdk.ps1
+++ tools/install_sdk.ps1
@@ -3,7 +3,7 @@ Set-Location "sdk/"
 # Download the file with Invoke-WebRequest.
 # The SDK URL is expected to get unavailable every time a new SDK is released.
 # See the following issue for details. URL: https://github.com/scripthookvdotnet/scripthookvdotnet/issues/1500
-$SdkUrl = "https://ntscorp.ru/dev-c/ScriptHookV_SDK_1.0.3337.0.zip"
+$SdkUrl = "https://ntscorp.ru/dev-c/ScriptHookV_SDK_1.0.617.1a.zip"
 Invoke-WebRequest -Uri $SdkUrl -OutFile "sdk.zip" -Headers @{ "Referer" = "http://www.dev-c.com/gtav/scripthookv/" }
 # Extract the contents of the file in the current directory
 7z x sdk.zip
