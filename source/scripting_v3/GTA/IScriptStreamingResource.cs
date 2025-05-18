970aaeaed72ff7e740484bc5c3aba31dd76c1574
kagikn <kagikn.3939@gmail.com>
2025-05-18 22:22:20 +0900

    docs(api-v3): add docs to `IScriptStreamingResource`

--- source/scripting_v3/GTA/IScriptStreamingResource.cs
+++ source/scripting_v3/GTA/IScriptStreamingResource.cs
@@ -6,19 +6,36 @@
 namespace GTA
 {
     /// <summary>
-    /// A interface for streaming resources that can be requested and pinned by scripts
+    /// An interface for streaming resources that can be requested and pinned by scripts
     /// (by increasing reference counts).
     /// </summary>
     public interface IScriptStreamingResource
     {
+        /// <summary>
+        /// Gets a value that indicates whether the resource is loaded and ready to use.
+        /// </summary>
         bool IsLoaded
         {
             get;
         }
 
+        /// <summary>
+        /// <para>
+        /// Requests the global streaming loader to load the steaming resource so it will be eventually loaded
+        /// (unless getting interrupted by a <see cref="MarkAsNoLongerNeeded()"/> call of another SHVDN script).
+        /// </para>
+        /// <para>
+        /// You will need to test if the resource is loaded with <see cref="IsLoaded"/> every frame until it is loaded
+        /// before you can use it. The game starts loading pending streaming objects every frame
+        /// (with `<c>CStreaming::Update()</c>`) before the script update call.
+        /// </para>
+        /// </summary>
         void Request();
         bool Request(int timeout);
 
+        /// <summary>
+        /// Tells the game we have finished using the streaming resource, so it can be freed from memory.
+        /// </summary>
         void MarkAsNoLongerNeeded();
     }
 }
