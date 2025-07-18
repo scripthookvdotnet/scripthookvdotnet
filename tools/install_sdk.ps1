8860d17646a970b35a355e3971185febe9bf874a
kagikn <kagikn.3939@gmail.com>
2025-07-18 20:45:00 +0900

    ci: use local subset of SHV SDK
    It looks like we might as well use a local subset of SDK files
    downloaded from `https://ntscorp.ru` because that website is too
    unreliable as a SDK source. Also remove our regular workflow that tests
    if HTTP status code of the SDK URL is 404, since we don't rely on any
    online source for the SDK.
    
    `https://ntscorp.ru` is not letting clients (users) download the SHV
    SDK because of its SSL certificate issues, roughly since Jury 12th,
    2025 (UTC). The website lets clients the latest built binary as of the
    commit is made, but not for the SDK.
    
    This commit does not change the `.gitignore` in the sdk directory, so
    developers and contributers can change some content of the SDK in it
    without a lot of chore (this commit adds the SDK content with the force
    option).

--- tools/install_sdk.ps1
+++ tools/install_sdk.ps1
@@ -1,8 +1,8 @@
 # Set the working directory to SDK
 Set-Location "sdk/"
-# Download the file with Invoke-WebRequest.
-# The SDK URL is expected to get unavailable every time a new SDK is released.
-# See the following issue for details. URL: https://github.com/scripthookvdotnet/scripthookvdotnet/issues/1500
+# Download the file with `Invoke-WebRequest`.
+# The command can fail for `https://ntscorp.ru` returning the HTTP code 526 because its SSL certificate is expired.
+# If that happens, blame whoever configure that website, not clients (or users that are downloading the SDK).
 $SdkUrl = "https://ntscorp.ru/dev-c/ScriptHookV_SDK_1.0.617.1a.zip"
 Invoke-WebRequest -Uri $SdkUrl -OutFile "sdk.zip" -Headers @{ "Referer" = "http://www.dev-c.com/gtav/scripthookv/" }
 # Extract the contents of the file in the current directory
