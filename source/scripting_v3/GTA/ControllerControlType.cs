823b8e7a865686aa6bdb271559d050b2d7fe5f10
TiverT <jonasthebrinkmann@gmail.com>
2025-07-25 16:09:30 +0200

    Added ControllerControlType enum

---
+++ source/scripting_v3/GTA/ControllerControlType.cs
@@ -0,0 +1,19 @@
+
+namespace GTA
+{
+    public enum ControllerControlType
+    {
+        Standard = 0,
+        Alternate,
+        Southpaw,
+        AlternatePlusSouthpaw,
+        StandardFPS,
+        AlternateFPS,
+        SouthpawFPS,
+        AlternatePlusSouthpawFPS,
+        StandardFPS2,
+        AlternateFPS2,
+        SouthpawFPS2,
+        AlternatePlusSouthpawFPS2,
+    }
+}
