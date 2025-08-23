9342b41966543b25c4bb27b528c384b9374d278d
TiverT <jonasthebrinkmann@gmail.com>
2025-08-23 17:44:28 +0200

    refactor(v3): renamed functions in HudColors according to recommendations, also removed 2 functions.

--- source/scripting_v3/GTA.UI/HudColors.cs
+++ source/scripting_v3/GTA.UI/HudColors.cs
@@ -24,48 +24,11 @@ namespace GTA.UI
         }
 
         /// <summary>
-        /// Sets the color that <see cref="HudColor.ScriptVariable"/> is holding to a specific RGBA value.
-        /// </summary>
-        /// <param name="r">Red component.</param>
-        /// <param name="g">Green component.</param>
-        /// <param name="b">Blue component.</param>
-        /// <param name="a">Alpha component (0 = transparent, 255 = fully opaque).</param>
-        public static void SetScriptVariable(byte r, byte g, byte b, byte a)
-        {
-            Function.Call(Hash.SET_SCRIPT_VARIABLE_HUD_COLOUR, (int)r, (int)g, (int)b, (int)a);
-        }
-
-        /// <summary>
-        /// Sets the color that <see cref="HudColor.ScriptVariable"/> is holding to a <see cref="Color"/> value.
-        /// </summary>
-        /// <param name="color">The <see cref="Color"/> to apply.</param>
-        public static void SetScriptVariable(Color color) => SetScriptVariable(color.R, color.G, color.B, color.A);
-
-
-        /// <summary>
-        /// Sets the color that <see cref="HudColor.ScriptVariable2"/> is holding to a specific RGBA value.
-        /// </summary>
-        /// <param name="r">Red component.</param>
-        /// <param name="g">Green component.</param>
-        /// <param name="b">Blue component.</param>
-        /// <param name="a">Alpha component (0 = transparent, 255 = fully opaque).</param>
-        public static void SetScriptVariable2(byte r, byte g, byte b, byte a)
-        {
-            Function.Call(Hash.SET_SECOND_SCRIPT_VARIABLE_HUD_COLOUR, (int)r, (int)g, (int)b, (int)a);
-        }
-
-        /// <summary>
-        /// Sets the color that <see cref="HudColor.ScriptVariable2"/> is holding to a <see cref="Color"/> value.
-        /// </summary>
-        /// <param name="color">The <see cref="Color"/> to apply.</param>
-        public static void SetScriptVariable2(Color color) => SetScriptVariable2(color.R, color.G, color.B, color.A);
-
-        /// <summary>
         /// Replaces a <see cref="HudColor"/> with another existing <see cref="HudColor"/>.
         /// </summary>
         /// <param name="destination">The HUD color slot to replace.</param>
         /// <param name="source">The source HUD color to copy from.</param>
-        public static void Replace(HudColor destination, HudColor source)
+        public static void Set(HudColor destination, HudColor source)
         {
             Function.Call(Hash.REPLACE_HUD_COLOUR, (int)destination, (int)source);
         }
@@ -75,7 +38,7 @@ namespace GTA.UI
         /// </summary>
         /// <param name="destination">The <see cref="HudColor"/> slot to replace.</param>
         /// <param name="color">The new color.</param>
-        public static void Replace(HudColor destination, Color color)
+        public static void Set(HudColor destination, Color color)
         {
             Function.Call(Hash.REPLACE_HUD_COLOUR_WITH_RGBA, (int)destination, color.R, color.G, color.B, color.A);
         }
@@ -88,7 +51,7 @@ namespace GTA.UI
         /// <param name="g">Green component.</param>
         /// <param name="b">Blue component.</param>
         /// <param name="a">Alpha component (0 = transparent, 255 = fully opaque).</param>
-        public static void Replace(HudColor destination, byte r, byte g, byte b, byte a)
+        public static void Set(HudColor destination, byte r, byte g, byte b, byte a)
         {
             Function.Call(Hash.REPLACE_HUD_COLOUR_WITH_RGBA, (int)destination, (int)r, (int)g, (int)b, (int)a);
         }
