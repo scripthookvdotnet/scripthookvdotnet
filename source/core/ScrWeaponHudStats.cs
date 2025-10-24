4664b1a65e66fc497d2e3189ba0149c2a865623f
TiverT <jonasthebrinkmann@gmail.com>
2025-10-24 15:08:43 +0200

    docs(core): mention internal memory structure

--- source/core/ScrWeaponHudStats.cs
+++ source/core/ScrWeaponHudStats.cs
@@ -5,7 +5,9 @@ namespace SHVDN
 	[StructLayout(LayoutKind.Explicit, Size = 0x28)] // total size 40 bytes
 	public struct ScrWeaponHudStats
     {
-		[FieldOffset(0x00)] public int Damage;
+        // Internally, these fields are stored as 'scrValue's.
+        // For HUD stats, they represent signed 8-bit integers (s8), aka 'char'.
+        [FieldOffset(0x00)] public int Damage;
 		[FieldOffset(0x08)] public int Speed;
 		[FieldOffset(0x10)] public int Capacity;
 		[FieldOffset(0x18)] public int Accuracy;
