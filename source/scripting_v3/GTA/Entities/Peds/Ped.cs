682dc69ff1efb09a3983df371954e6b5a09277d9
TiverT <jonasthebrinkmann@gmail.com>
2025-10-25 13:29:36 +0200

    feat(api-v3): add field for GET/SET_AMBIENT_VOICE_NAME_HASH,

--- source/scripting_v3/GTA/Entities/Peds/Ped.cs
+++ source/scripting_v3/GTA/Entities/Peds/Ped.cs
@@ -2792,6 +2792,15 @@ namespace GTA
             set => Function.Call(Hash.SET_AMBIENT_VOICE_NAME, Handle, value);
         }
 
+        /// <summary>
+        /// Gets or sets the ambient voice hash to use when this <see cref="Ped"/> speaks.
+        /// </summary>
+        public string VoiceHash
+        {
+            set => Function.Call(Hash.SET_AMBIENT_VOICE_NAME_HASH, Handle, value);
+            get => Function.Call<string>(Hash.GET_AMBIENT_VOICE_NAME_HASH, Handle);
+        }
+
         #endregion
 
         #region Animation
