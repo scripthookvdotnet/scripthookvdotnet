f9b4f292e63247121256bafaaee14b31bf480674
TiverT <jonasthebrinkmann@gmail.com>
2025-10-28 20:41:14 +0100

    feat(api-v3): add missing `LicensePlateStyle` entries

--- source/scripting_v3/GTA/Entities/Vehicles/LicensePlateStyle.cs
+++ source/scripting_v3/GTA/Entities/Vehicles/LicensePlateStyle.cs
@@ -12,6 +12,13 @@ namespace GTA
         BlueOnWhite3 = 4,
         YellowOnBlack = 1,
         YellowOnBlue = 2,
-        NorthYankton = 5
+        NorthYankton = 5,
+        ECola = 7,
+        LasVenturas = 8,
+        LibertyCity = 9,
+        LSCarMeet = 10,
+        LSPanic = 11,
+        LSPounders = 12,
+        Sprunk = 13
     }
 }
