ba297ccc710c8f72236a797c7e9f03f8f9cd94ac
Jonas <jonasthebrinkmann@gmail.com>
2025-10-29 00:22:57 +0100

    feat(api-v3): add missing `LicensePlateStyle` entries (#1634)
    Reference:
    
    https://github.com/DurtyFree/gta-v-data-dumps/blob/master/vehicleColors.json#L1916-L2076
    
    (also private testing)

--- source/scripting_v3/GTA/Entities/Vehicles/LicensePlateStyle.cs
+++ source/scripting_v3/GTA/Entities/Vehicles/LicensePlateStyle.cs
@@ -12,6 +12,13 @@ namespace GTA
         BlueOnWhite3 = 4,
         YellowOnBlack = 1,
         YellowOnBlue = 2,
-        NorthYankton = 5
+        NorthYankton = 5,
+        ECola = 6,
+        LasVenturas = 7,
+        LibertyCity = 8,
+        LSCarMeet = 9,
+        LSPanic = 10,
+        LSPounders = 11,
+        Sprunk = 12
     }
 }
