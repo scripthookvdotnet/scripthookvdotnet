61f59b0d1ce16149fd9df6ac5c3f4dd477a41ba6
TiverT <jonasthebrinkmann@gmail.com>
2025-11-15 20:26:27 +0100

    refactor(api-v3): move `VehicleModType` code to new helper and extension classes

--- source/scripting_v3/GTA/Entities/Vehicles/VehicleModType.cs
+++ source/scripting_v3/GTA/Entities/Vehicles/VehicleModType.cs
@@ -54,4 +54,58 @@ namespace GTA
         Windows,
         Livery = 48
     }
+
+    internal static class VehicleModTypeExtensions
+    {
+        /// <summary>
+        /// Returns the value of this <see cref="VehicleModType"/> aligned to the game build.
+        /// </summary>
+        /// <param name="value">The <see cref="VehicleModType"/> to correct.</param>
+        /// <returns>The corrected integer value based on game version.</returns>
+        internal static int ToCorrectedValue(this VehicleModType value)
+        {
+            // This kind of correction was introduced in b393 so return the same value if the game version is earlier than b393
+            if (Game.FileVersion < VersionConstsForGameVersion.v1_0_393_2)
+            {
+                return (int)value;
+            }
+
+            int valueAsInt = (int)value;
+
+            if (valueAsInt > 24)
+            {
+                return valueAsInt - 14;
+            }
+
+            if (valueAsInt > 10)
+            {
+                return valueAsInt + 25;
+            }
+
+            return valueAsInt;
+        }
+    }
+
+    internal static class VehicleModTypeHelpers
+    {
+        internal static VehicleModType FromIndex(int value)
+        {
+            if (Game.FileVersion < VersionConstsForGameVersion.v1_0_393_2)
+            {
+                return (VehicleModType)value;
+            }
+
+            if (value is > 35 and < 50)
+            {
+                return (VehicleModType)(value - 25);
+            }
+
+            if (value is > 10 and < 36)
+            {
+                return (VehicleModType)(value + 14);
+            }
+
+            return (VehicleModType)value;
+        }
+    }
 }
