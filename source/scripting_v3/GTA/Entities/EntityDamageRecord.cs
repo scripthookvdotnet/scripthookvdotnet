scripthookvdotnet/scripthookvdotnet

commit f4b13df7d6f5ef7c3cdbb034523ecf4a3e02202a

Add EntityDamageRecord and collection class (#1009)
* Add EntityDamageRecord struct and collection class

source/scripting_v3/GTA/Entities/EntityDamageRecord.cs
@@ -1,0 +1,126 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+using GTA.Math;
+using GTA.Native;
+
+namespace GTA
+{
+	public readonly struct EntityDamageRecord
+	{
+		internal EntityDamageRecord(Entity victim, Entity attacker, WeaponHash weaponHash, int gameTime)
+		{
+			Victim = victim;
+			Attacker = attacker;
+			WeaponHash = weaponHash;
+			GameTime = gameTime;
+		}
+
+		/// <summary>
+		/// Gets the victim <see cref="Entity" />.
+		/// </summary>
+		public Entity Victim
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets the attacker <see cref="Entity" />. Can be <c>null</c>.
+		/// </summary>
+		public Entity Attacker
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets the game time when the <see cref="Victim" /> took damage.
+		/// </summary>
+		public int GameTime
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets the weapon hash what the <see cref="Victim" /> took damage with.
+		/// </summary>
+		public WeaponHash WeaponHash
+		{
+			get;
+		}
+
+		public void Deconstruct(out Entity attacker, out WeaponHash weaponHash, out int gameTime)
+		{
+			attacker = Attacker;
+			weaponHash = WeaponHash;
+			gameTime = GameTime;
+		}
+
+		public void Deconstruct(out Entity victim, out Entity attacker, out WeaponHash weaponHash, out int gameTime)
+		{
+			victim = Victim;
+			attacker = Attacker;
+			weaponHash = WeaponHash;
+			gameTime = GameTime;
+		}
+
+		/// <summary>
+		/// Determines if <paramref name="entityDamageRecord"/> has the same properties as this <see cref="EntityDamageRecord"/>.
+		/// </summary>
+		/// <param name="entityDamageRecord">The <see cref="object"/> to check.</param>
+		/// <returns><c>true</c> if the <paramref name="entityDamageRecord"/> has the same properties as this <see cref="EntityDamageRecord"/>; otherwise, <c>false</c>.</returns>
+		public bool Equals(EntityDamageRecord entityDamageRecord)
+		{
+			return Victim == entityDamageRecord.Victim &&
+				Attacker == entityDamageRecord.Attacker &&
+				WeaponHash == entityDamageRecord.WeaponHash &&
+				GameTime == entityDamageRecord.GameTime;
+		}
+		/// <summary>
+		/// Determines if an <see cref="object"/> is an <see cref="EntityDamageRecord"/> and has the same properties as this <see cref="EntityDamageRecord"/>.
+		/// </summary>
+		/// <param name="obj">The <see cref="object"/> to check.</param>
+		/// <returns><c>true</c> if the <paramref name="obj"/> is an <see cref="EntityDamageRecord"/> and has the same properties as this <see cref="EntityDamageRecord"/>; otherwise, <c>false</c>.</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is EntityDamageRecord entityDamageRecord)
+			{
+				Equals(entityDamageRecord);
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Determines if two <see cref="EntityDamageRecord"/>s have the same properties.
+		/// </summary>
+		/// <param name="left">The left <see cref="Entity"/>.</param>
+		/// <param name="right">The right <see cref="Entity"/>.</param>
+		/// <returns><c>true</c> if <paramref name="left"/> has the same properties as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
+		public static bool operator ==(EntityDamageRecord left, EntityDamageRecord right)
+		{
+			return left.Equals(right);
+		}
+		/// <summary>
+		/// Determines if two <see cref="Entity"/>s do not have the same properties.
+		/// </summary>
+		/// <param name="left">The left <see cref="Entity"/>.</param>
+		/// <param name="right">The right <see cref="Entity"/>.</param>
+		/// <returns><c>true</c> if <paramref name="left"/> does not have the same properties as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
+		public static bool operator !=(EntityDamageRecord left, EntityDamageRecord right)
+		{
+			return !left.Equals(right);
+		}
+
+		public override int GetHashCode()
+		{
+			int hash = 17;
+			hash = Victim != null ? hash * 5039 + Victim.Handle.GetHashCode() : hash;
+			hash = Attacker != null ? hash * 883 + Attacker.Handle.GetHashCode() : hash;
+			hash = hash * 9719 + ((int)WeaponHash).GetHashCode();
+			hash = hash * 317 + GameTime.GetHashCode();
+			return hash;
+		}
+	}
+}

source/core/NativeMemory.cs
@@ -253,6 +253,21 @@ namespace SHVDN
 				waypointInfoArrayEndAddress = (ulong*)(*(int*)(address + 3) + address + 7);
 			}
 
+
+			address = FindPattern("\x48\x8B\x89\x00\x00\x00\x00\x33\xC0\x44\x8B\xC2\x48\x85\xC9\x74\x20", "xxx????xxxxxxxxxx");
+			cAttackerArrayOfEntityOffset = *(uint*)(address + 3); // the correct name is unknown
+			if (address != null)
+			{
+				startAddressToSearch = new IntPtr(address);
+				address = FindPattern("\x48\x63\x51\x00\x48\x85\xD2", "xxx?xxx", startAddressToSearch);
+				elementCountOfCAttackerArrayOfEntityOffset = (uint)(*(sbyte*)(address + 3));
+
+				startAddressToSearch = new IntPtr(address);
+				address = FindPattern("\x48\x83\xC1\x00\x48\x3B\xC2\x7C\xEF", "xxx?xxxxx", startAddressToSearch);
+				// the element size might be 0x10 in older builds (the size is 0x18 at least in b1604 and b2372)
+				elementSizeOfCAttackerArrayOfEntity = (uint)(*(sbyte*)(address + 3));
+			}
+
 			address = FindPattern("\x48\x8B\x0B\x33\xD2\xE8\x00\x00\x00\x00\x89\x03", "xxxxxx????xx");
 			GetHashKeyFunc = GetDelegateForFunctionPointer<GetHashKeyDelegate>(new IntPtr(*(int*)(address + 6) + address + 10));
 
@@ -1197,6 +1212,100 @@ namespace SHVDN
 			}
 
 			return IntPtr.Zero;
+		}
+
+		#endregion
+
+		#region -- Entity Offsets --
+
+		public static uint cAttackerArrayOfEntityOffset { get; }
+		public static uint elementCountOfCAttackerArrayOfEntityOffset { get; }
+		public static uint elementSizeOfCAttackerArrayOfEntity { get; }
+
+		#endregion
+
+		#region -- Entity Data --
+
+		// the size is at least 0x10 in all game versions
+		[StructLayout(LayoutKind.Explicit, Size = 0x10)]
+		struct CAttacker
+		{
+			[FieldOffset(0x0)]
+			internal ulong attackerEntityAddress;
+			[FieldOffset(0x8)]
+			internal int weaponHash;
+			[FieldOffset(0xC)]
+			internal int gameTime;
+		}
+
+		public static bool IsIndexOfEntityDamageRecordValid(IntPtr entityAddress, uint index)
+		{
+			if (cAttackerArrayOfEntityOffset == 0 ||
+				elementCountOfCAttackerArrayOfEntityOffset == 0 ||
+				elementSizeOfCAttackerArrayOfEntity == 0)
+				return false;
+
+			ulong entityCAttackerArrayAddress = *(ulong*)(entityAddress + (int)cAttackerArrayOfEntityOffset).ToPointer();
+
+			if (entityCAttackerArrayAddress == 0)
+				return false;
+
+			var returnEntrySize = *(int*)(entityCAttackerArrayAddress + elementCountOfCAttackerArrayOfEntityOffset);
+
+			return index < returnEntrySize;
+		}
+		static (int attackerHandle, int weaponHash, int gameTime) GetEntityDamageRecordEntryAtIndex(ulong cAttackerArrayAddress, uint index)
+		{
+			var cAttacker = (CAttacker*)(cAttackerArrayAddress + index * elementSizeOfCAttackerArrayOfEntity);
+
+			var attackerEntityAddress = cAttacker->attackerEntityAddress;
+			var weaponHash = cAttacker->weaponHash;
+			var gameTime = cAttacker->gameTime;
+			var attackerHandle = attackerEntityAddress != 0 ? GetEntityHandleFromAddress(new IntPtr((long)attackerEntityAddress)) : 0;
+
+			return (attackerHandle, weaponHash, gameTime);
+		}
+		public static (int attackerHandle, int weaponHash, int gameTime) GetEntityDamageRecordEntryAtIndex(IntPtr entityAddress, uint index)
+		{
+			if (cAttackerArrayOfEntityOffset == 0 ||
+				elementCountOfCAttackerArrayOfEntityOffset == 0 ||
+				elementSizeOfCAttackerArrayOfEntity == 0)
+				return default((int attackerHandle, int weaponHash, int gameTime));
+
+			ulong entityCAttackerArrayAddress = *(ulong*)(entityAddress + (int)cAttackerArrayOfEntityOffset).ToPointer();
+
+			if (entityCAttackerArrayAddress == 0)
+				return default((int attackerHandle, int weaponHash, int gameTime));
+
+			var returnEntrySize = *(int*)(entityCAttackerArrayAddress + elementCountOfCAttackerArrayOfEntityOffset);
+
+			if (index >= returnEntrySize)
+				return default((int attackerHandle, int weaponHash, int gameTime));
+
+			return GetEntityDamageRecordEntryAtIndex(entityCAttackerArrayAddress, index);
+		}
+
+		public static (int attackerHandle, int weaponHash, int gameTime)[] GetEntityDamageRecordEntries(IntPtr entityAddress)
+		{
+			if (cAttackerArrayOfEntityOffset == 0 ||
+				elementCountOfCAttackerArrayOfEntityOffset == 0 ||
+				elementSizeOfCAttackerArrayOfEntity == 0)
+					return Array.Empty<(int handle, int weaponHash, int gameTime)>();
+
+			ulong entityCAttackerArrayAddress = *(ulong*)(entityAddress + (int)cAttackerArrayOfEntityOffset).ToPointer();
+
+			if (entityCAttackerArrayAddress == 0)
+				return Array.Empty<(int attackerHandle, int weaponHash, int gameTime)>();
+
+			var returnEntrySize = *(int*)(entityCAttackerArrayAddress + elementCountOfCAttackerArrayOfEntityOffset);
+			var returnEntries = returnEntrySize != 0 ? new (int attackerHandle, int weaponHash, int gameTime)[returnEntrySize] : Array.Empty<(int attackerHandle, int weaponHash, int gameTime)>();
+
+			for (uint i = 0; i < returnEntries.Length; i++)
+            {
+				returnEntries[i] = GetEntityDamageRecordEntryAtIndex(entityCAttackerArrayAddress, i);
+			}
+
+			return returnEntries;
 		}
 
 		#endregion

source/scripting_v3/GTA/Entities/Entity.cs
@@ -14,6 +14,7 @@ namespace GTA
 	{
 		#region Fields
 		EntityBoneCollection _bones;
+		EntityDamageRecordCollection _damageRecords;
 		#endregion
 
 		internal Entity(int handle) : base(handle)
@@ -530,6 +531,11 @@ namespace GTA
 		#endregion
 
 		#region Damaging
+
+		/// <summary>
+		/// Gets a collection of the <see cref="EntityDamageRecord"/>s in this <see cref="Entity"/>.
+		/// </summary>
+		public EntityDamageRecordCollection DamageRecords => _damageRecords ?? (_damageRecords = new EntityDamageRecordCollection(this));
 
 		/// <summary>
 		/// Determines whether this <see cref="Entity"/> has been damaged by a specified <see cref="Entity"/>.

source/scripting_v3/GTA/Entities/EntityDamageRecordCollection.cs
@@ -1,0 +1,67 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GTA
+{
+	public class EntityDamageRecordCollection : IEnumerable<EntityDamageRecord>, IEnumerable
+	{
+		#region Fields
+		protected readonly Entity _owner;
+		#endregion
+
+		internal EntityDamageRecordCollection(Entity owner)
+		{
+			_owner = owner;
+		}
+
+		public IEnumerator<EntityDamageRecord> GetEnumerator()
+		{
+			// No more than 3 damage records
+			for (uint i = 0; i < 3; i++)
+			{
+				var memoryAddress = _owner.MemoryAddress;
+
+				if (memoryAddress == IntPtr.Zero || SHVDN.NativeMemory.IsIndexOfEntityDamageRecordValid(memoryAddress, i))
+					yield break;
+
+				var returnDamageRecord = SHVDN.NativeMemory.GetEntityDamageRecordEntryAtIndex(_owner.MemoryAddress, i);
+
+				(int attackerHandle, int weaponHash, int gameTime) = returnDamageRecord;
+				var attackerEntity = attackerHandle != 0 ? Entity.FromHandle(attackerHandle) : null;
+				yield return new EntityDamageRecord(_owner, attackerEntity, (WeaponHash)weaponHash, gameTime);
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		/// <summary>
+		/// Gets all the <see cref="EntityDamageRecord" /> at the moment.
+		/// The return array can contain up to 3 <see cref="EntityDamageRecord" />s.
+		/// </summary>
+		public EntityDamageRecord[] GetAllDamageRecords()
+		{
+			var memoryAddress = _owner.MemoryAddress;
+
+			if (memoryAddress == IntPtr.Zero)
+				return new EntityDamageRecord[0];
+
+			var damageEntries = SHVDN.NativeMemory.GetEntityDamageRecordEntries(memoryAddress);
+			var returnDamageRecords = new EntityDamageRecord[damageEntries.Length];
+
+			for (int i = 0; i < returnDamageRecords.Length; i++)
+			{
+				(int attackerHandle, int weaponHash, int gameTime) = damageEntries[i];
+				var attackerEntity = attackerHandle != 0 ? Entity.FromHandle(attackerHandle) : null;
+				returnDamageRecords[i] = new EntityDamageRecord(_owner, attackerEntity, (WeaponHash)weaponHash, gameTime);
+			}
+
+			return returnDamageRecords;
+		}
+	}
+}

source/scripting_v3/ScriptHookVDotNet_APIv3.csproj
@@ -98,6 +98,7 @@
     <Compile Include="GTA\Entities\Entity.cs" />
     <Compile Include="GTA\Entities\EntityBone.cs" />
     <Compile Include="GTA\Entities\EntityBoneCollection.cs" />
+    <Compile Include="GTA\Entities\EntityDamageRecord.cs" />
     <Compile Include="GTA\Entities\EntityType.cs" />
     <Compile Include="GTA\Entities\Model.cs" />
     <Compile Include="GTA\Entities\Peds\AnimationFlags.cs" />
@@ -179,6 +180,7 @@
     <Compile Include="GTA\Player.cs" />
     <Compile Include="GTA\PoolObject.cs" />
     <Compile Include="GTA\RadioStation.cs" />
+    <Compile Include="GTA\Entities\EntityDamageRecordCollection.cs" />
     <Compile Include="GTA\RaycastResult.cs" />
     <Compile Include="GTA\GameVersionNotSupportedException.cs" />
     <Compile Include="GTA\RequireScript.cs" />

commit e6a2bbd422fe03be5e9c37ac2d796565081825e0

fix(api-v3): `EntityDamageRecord.Equals(object)` always returning false
Return true when the passed object is a `EntityDamageRecord` and what
the both caller and the passed record have are the same as each other.

The bug is introduced in f4b13df7d6f5ef7c3cdbb034523ecf4a3e02202a,
where `EntityDamageRecord` is added. Note that `EntityDamageRecord` is
included in every stable version since v3.2.0.

Fix #1707.

source/scripting_v3/GTA/Entities/EntityDamageRecord.cs
@@ -83,7 +83,7 @@ namespace GTA
         {
             if (obj is EntityDamageRecord entityDamageRecord)
             {
-                Equals(entityDamageRecord);
+                return Equals(entityDamageRecord);
             }
 
             return false;