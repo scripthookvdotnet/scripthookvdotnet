scripthookvdotnet/scripthookvdotnet

commit 061914f9e7dec96e97d57b8acc05f4fc7c20ba02

Consider player ped on Entity.MaxHealthFloat and add notes for max health of player ped

source/scripting_v3/GTA/Entities/Entity.cs
@@ -248,6 +248,7 @@ namespace GTA
 		/// </value>
 		/// <remarks>
 		/// Use <see cref="HealthFloat"/> instead if you need to get or set the value precisely, since a health value of a <see cref="Entity"/> are stored as a <see cref="float"/>.
+		/// You should note <see cref="HealthFloat"/> does not care about the 16-bit unsigned integer value for the max health of the player ped(s) on <c>CPlayerInfo</c> even if the <see cref="Entity"/> is the player <see cref="Ped"/>s, however.
 		/// </remarks>
 		/// <seealso cref="HealthFloat"/>
 		public int Health
@@ -305,6 +306,17 @@ namespace GTA
 		/// <value>
 		/// The maximum health as a <see cref="float"/>.
 		/// </value>
+		/// <remarks>
+		/// <para>
+		/// This method also set value to the 16-bit unsigned integer field for the max health of the player ped(s) on <c>CPlayerInfo</c> to prevent them from having inconsistent max health values.
+		/// while the base implementation of <see cref="MaxHealth"/> does not care about it at all since there is <see cref="Ped.MaxHealth"/>, where the max health of the player ped(s) on <c>CPlayerInfo</c> is considered.
+		/// </para>
+		/// <para>
+		/// You should not set a value larger than <c>65535.0</c> or a negative value for the player ped(s) as the game uses the 16-bit unsigned integer value for the max health of the player ped(s) on <c>CPlayerInfo</c>
+		/// and it is used when respawning and in <c>SET_ENTITY_MAX_HEALTH</c> as the max limit.
+		/// Setting a value larger than <c>65535.0</c> will result in the overflow of the 16-bit unsigned integer value for the max health of <c>CPlayerInfo</c>.
+		/// </para>
+		/// </remarks>
 		public float MaxHealthFloat
 		{
 			get
@@ -326,6 +338,38 @@ namespace GTA
 				}
 
 				SHVDN.NativeMemory.WriteFloat(address + SHVDN.NativeMemory.EntityMaxHealthOffset, value);
+
+				// Only needed for setter as GET_ENTITY_HEALTH doesn't use the the uint16_t max health value of CPlayerInfo in any cases
+				// SET_ENTITY_HEALTH ignores the float max health value of CPhysical and instead uses the max health value of CPlayerInfo for the player ped
+				// SET_ENTITY_MAX_HEALTH sets both values for player peds as thw health display on HUD uses the float max health value of CPhysical
+				#region Special Max Health Treatment for Player Ped
+				if (SHVDN.NativeMemory.UnkCPedStateOffset == 0 || SHVDN.NativeMemory.PedPlayerInfoOffset == 0)
+				{
+					return;
+				}
+
+				var entityType = (EntityTypeInternal)SHVDN.NativeMemory.ReadByte(address + 0x28);
+				if (entityType != EntityTypeInternal.Ped)
+				{
+					return;
+				}
+
+				var unkPedState = SHVDN.NativeMemory.ReadByte(address + SHVDN.NativeMemory.UnkCPedStateOffset);
+				if (unkPedState != 2)
+				{
+					return;
+				}
+
+
+				var cPlayerInfo = SHVDN.NativeMemory.ReadAddress(address + SHVDN.NativeMemory.PedPlayerInfoOffset);
+				if (cPlayerInfo == IntPtr.Zero)
+				{
+					return;
+				}
+
+				// SET_ENTITY_MAX_HEALTH doesn't care about overflow either
+				SHVDN.NativeMemory.WriteUInt16(address + SHVDN.NativeMemory.CPlayerInfoMaxHealthOffset, (ushort)System.Math.Ceiling(value));
+				#endregion
 			}
 		}
 

source/core/NativeMemory.cs
@@ -804,6 +804,11 @@ namespace SHVDN
 				}
 			}
 
+			address = FindPatternBmh("\x66\x0F\x6E\xF0\x0F\x5B\xF6\xE8\x00\x00\x00\x00\x0F\x28\xCE\x41\xB1\x01\x45\x33\xC0\x48\x8B\xC8\xE8", "xxxxxxxx????xxxxxxxxxxxxx");
+			if (address != null)
+			{
+				CPlayerInfoMaxHealthOffset = *(int*)(address - 4);
+			}
 			// None of variables for player targeting and wanted info are not accessed with direct offsets from CPlayerInfo instances in the game code
 			address = FindPatternBmh("\x48\x85\xFF\x74\x23\x48\x85\xDB\x74\x26", "xxxxxxxxxx");
 			if (address != null)
@@ -867,6 +872,7 @@ namespace SHVDN
 
 				PedPlayerInfoOffset = PedIntelligenceOffset + 0x8;
 				CPedIntentoryOfCPedOffset = PedIntelligenceOffset + 0x10;
+				UnkCPedStateOffset = PedIntelligenceOffset - 0x10;
 			}
 
 			address = FindPatternNaive("\x48\x85\xC0\x74\x7F\xF6\x80\x00\x00\x00\x00\x02\x75\x76", "xxxxxxx????xxx");
@@ -1229,6 +1235,15 @@ namespace SHVDN
 			return *(short*)address.ToPointer();
 		}
 		/// <summary>
+		/// Reads an unsigned single 16-bit value from the specified <paramref name="address"/>.
+		/// </summary>
+		/// <param name="address">The memory address to access.</param>
+		/// <returns>The value at the address.</returns>
+		public static ushort ReadUInt16(IntPtr address)
+		{
+			return *(ushort*)address.ToPointer();
+		}
+		/// <summary>
 		/// Reads a single 32-bit value from the specified <paramref name="address"/>.
 		/// </summary>
 		/// <param name="address">The memory address to access.</param>
@@ -1302,6 +1317,16 @@ namespace SHVDN
 		public static void WriteInt16(IntPtr address, short value)
 		{
 			var data = (short*)address.ToPointer();
+			*data = value;
+		}
+		/// <summary>
+		/// Writes an unsigned single 16-bit value to the specified <paramref name="address"/>.
+		/// </summary>
+		/// <param name="address">The memory address to access.</param>
+		/// <param name="value">The value to write.</param>
+		public static void WriteUInt16(IntPtr address, ushort value)
+		{
+			var data = (ushort*)address.ToPointer();
 			*data = value;
 		}
 		/// <summary>
@@ -2390,6 +2415,11 @@ namespace SHVDN
 		#region -- Ped Offsets --
 
 		public static int SweatOffset { get; }
+
+		/// <summary>
+		/// The value at this offset should be 2 if the ped is a player ped.
+		/// </summary>
+		public static int UnkCPedStateOffset { get; }
 
 		public static int CPedIntentoryOfCPedOffset { get; }
 
@@ -3613,6 +3643,11 @@ namespace SHVDN
 		static delegate* unmanaged[Stdcall]<int, ulong> GetPlayerPedAddressFunc;
 
 		static bool* isGameMultiplayerAddr;
+
+		/// <summary>
+		/// The offset for max health of CPlayerInfo, which is stored as an uint16_t.
+		/// </summary>
+		public static int CPlayerInfoMaxHealthOffset { get; }
 
 		public static int PedPlayerInfoOffset { get; }
 		public static int CWantedOffset { get; }

source/scripting_v3/GTA/Entities/Peds/Ped.cs
@@ -294,6 +294,11 @@ namespace GTA
 		/// <value>
 		/// The maximum health as an <see cref="int"/>.
 		/// </value>
+		/// <remarks>
+		/// You should not set a value larger than <c>65535</c> or a negative value for the player ped(s) as the game uses the 16-bit unsigned integer value for the max health of the player ped(s) on <c>CPlayerInfo</c>
+		/// and it is used when respawning and in <c>SET_ENTITY_MAX_HEALTH</c> as the max limit.
+		/// Setting a value larger than <c>65535</c> will result in the overflow of the 16-bit unsigned integer value for the max health of <c>CPlayerInfo</c>.
+		/// </remarks>
 		public override int MaxHealth
 		{
 			get => Function.Call<int>(Hash.GET_PED_MAX_HEALTH, Handle);

commit ca0b7688f6c1c7881ba7ec2b195314abb3ff6f97

fix(api-v3): `Entity.MaxHealthFloat` writing at incorrect location
Make the setter of `Entity.MaxHealthFloat` a calculated max health of
`CPlayerInfo` at the correct location. The bug is introduced in
061914f9e7dec96e97d57b8acc05f4fc7c20ba02, which is the first commit
where the special logic for a player `Ped` is added.

Fix #1706.

source/scripting_v3/GTA/Entities/Entity.cs
@@ -529,7 +529,7 @@ namespace GTA
                 }
 
                 // SET_ENTITY_MAX_HEALTH doesn't care about overflow either
-                SHVDN.MemDataMarshal.WriteUInt16(address + SHVDN.NativeMemory.CPlayerInfoMaxHealthOffset, (ushort)System.Math.Ceiling(value));
+                SHVDN.MemDataMarshal.WriteUInt16(cPlayerInfo + SHVDN.NativeMemory.CPlayerInfoMaxHealthOffset, (ushort)System.Math.Ceiling(value));
                 #endregion
             }
         }